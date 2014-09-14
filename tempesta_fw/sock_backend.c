1e3f22f0bba72a5dfe52cb6ffa41c2b1bd0c4618
Dmitry Vasilyanov <vdmit11@gmail.com>
2014-09-15 00:27:36 +0400

    reorder functions in bottom-to-top manner

--- tempesta_fw/sock_backend.c
+++ tempesta_fw/sock_backend.c
@@ -65,107 +65,70 @@ static struct task_struct *breconnd_task = NULL;
 #define TFW_BRECONND_INTERVAL 1000
 
 
-/* Forward declarations for local functions. */
-static void start_breconnd_thread(void);
-static void stop_breconnd_thread(void);
-static int breconnd_main_loop(void *data);
-static void open_new_backend_sockets(void);
-static void reopen_dead_backend_sockets(void);
-static void close_all_backend_sockets(void);
-static bool all_backend_sockets_are_closed(void);
-static void copy_new_backend_addresses_from_cfg(void);
-static void free_backends_mem(void);
-static int tfw_backend_connect(struct socket **sock, void *addr);
 
-
-/*
- * Apply the new backends configuration.
- *
- * This function should be called when you change the backend configuration
- * (the addresses list in tfw_cfg). It closes all active connections to backends
- * and then opens new ones within the new configuration.
- * Also it restarts the thread that re-connects to the backends in background.
+/**
+ * Connect to the back-end server.
  */
-void
-tfw_apply_new_backends_cfg(void)
-{
-	stop_breconnd_thread();
-	close_all_backend_sockets();
-	copy_new_backend_addresses_from_cfg();
-
-	if (backend_socks_n > 0) {
-		open_new_backend_sockets();
-		start_breconnd_thread();
-	}
-}
-
-
-/*
- * The clean-up routine: it closes all backend connections, terminates
- * background threads and releases allocated memory.
- */
-void
-tfw_release_backend_sockets(void)
-{
-	stop_breconnd_thread();
-	close_all_backend_sockets();
-	free_backends_mem();
-}
-
-
-
-static void
-start_breconnd_thread(void)
-{
-	struct task_struct *t;
-
-	BUG_ON(breconnd_task);
-	TFW_DBG("Starting thread: %s\n", TFW_BRECONND_NAME);
-
-	t = kthread_run(
-		breconnd_main_loop,
-		NULL,
-		TFW_BRECONND_NAME
-	);
-
-	if (IS_ERR_OR_NULL(t)) {
-		TFW_ERR("Can't create thread: %s (%ld)",
-			TFW_BRECONND_NAME,
-			PTR_ERR(t));
-	} else {
-		breconnd_task = t;
-	}
-}
-
-
-static void
-stop_breconnd_thread(void)
-{
-	/* FIXME:
-	 * The function will block while the thread sleeps because the
-	 * kthread_stop() waits until the thread exits.
-	 * We need to somehow notify the thread that it should exit and then
-	 * interrupt its sleep (perhaps by sending a signal).
-	 */
-	if (breconnd_task) {
-		TFW_DBG("Stopping thread: %s\n", TFW_BRECONND_NAME);
-		kthread_stop(breconnd_task);
-		breconnd_task = NULL;
-	}
-}
-
-
 static int
-breconnd_main_loop(void *data)
+tfw_backend_connect(struct socket **sock, void *addr)
 {
-	while (true) {
-		msleep_interruptible(TFW_BRECONND_INTERVAL);
-		reopen_dead_backend_sockets();
+	static struct {
+		SsProto	_placeholder;
+		int	type;
+	} dummy_proto = {
+		.type = TFW_FSM_HTTP,
+	};
 
-		if (kthread_should_stop()) {
-			return 0;
-		}
+	TfwServer *srv;
+	struct sock *sk;
+	unsigned short family = *(unsigned short *)addr;
+	unsigned short sza = family == AF_INET
+			     ? sizeof(struct sockaddr_in)
+			     : sizeof(struct sockaddr_in6);
+	int r;
+
+	r = sock_create_kern(family, SOCK_STREAM, IPPROTO_TCP, sock);
+	if (r) {
+		TFW_ERR("Can't create back-end connections socket (%d)\n", r);
+		return r;
 	}
+
+	r = kernel_connect(*sock, (struct sockaddr *)addr, sza, 0);
+	if (r) {
+		goto err_sock_destroy;
+	}
+	sk = (*sock)->sk;
+
+	/*
+	 * TODO Set up socket callbacks.
+	 * Do we need any?
+	 */
+
+	/*
+	 * TODO only one server connection is established now.
+	 * Create N connections to each server for redundancy,
+	 * so we shuldn't allocate a new server for each connection.
+	 */
+	srv = tfw_create_server(sk);
+	if (!srv) {
+		char buf[MAX_ADDR_LEN];
+		tfw_inet_ntop(addr, buf);
+		TFW_ERR("Can't create server descriptor for %s\n", buf);
+		goto err_sock_destroy;
+	}
+
+	sk->sk_user_data = &dummy_proto;
+	r = tfw_connection_new(sk, Conn_Srv, srv, tfw_destroy_server);
+	if (r)
+		goto err_conn_create;
+
+	return 0;
+err_conn_create:
+	tfw_destroy_server(sk);
+err_sock_destroy:
+	sock_release(*sock);
+	*sock = NULL;
+	return r;
 }
 
 
@@ -269,6 +232,19 @@ all_backend_sockets_are_closed(void)
 }
 
 
+static void
+free_backends_mem(void)
+{
+	mutex_lock(&backend_socks_mtx);
+
+	kfree(backend_socks);
+	backend_socks = NULL;
+	backend_socks_n = 0;
+
+	mutex_unlock(&backend_socks_mtx);
+}
+
+
 void
 copy_new_backend_addresses_from_cfg(void)
 {
@@ -311,81 +287,91 @@ copy_new_backend_addresses_from_cfg(void)
 }
 
 
-static void
-free_backends_mem()
+static int
+breconnd_main_loop(void *data)
 {
-	mutex_lock(&backend_socks_mtx);
+	while (true) {
+		msleep_interruptible(TFW_BRECONND_INTERVAL);
+		reopen_dead_backend_sockets();
 
-	kfree(backend_socks);
-	backend_socks = NULL;
-	backend_socks_n = 0;
-
-	mutex_unlock(&backend_socks_mtx);
+		if (kthread_should_stop()) {
+			return 0;
+		}
+	}
 }
 
 
-
-/**
- * Connect to the back-end server.
- */
-static int
-tfw_backend_connect(struct socket **sock, void *addr)
+static void
+start_breconnd_thread(void)
 {
-	static struct {
-		SsProto	_placeholder;
-		int	type;
-	} dummy_proto = {
-		.type = TFW_FSM_HTTP,
-	};
+	struct task_struct *t;
 
-	TfwServer *srv;
-	struct sock *sk;
-	unsigned short family = *(unsigned short *)addr;
-	unsigned short sza = family == AF_INET
-			     ? sizeof(struct sockaddr_in)
-			     : sizeof(struct sockaddr_in6);
-	int r;
+	BUG_ON(breconnd_task);
+	TFW_DBG("Starting thread: %s\n", TFW_BRECONND_NAME);
 
-	r = sock_create_kern(family, SOCK_STREAM, IPPROTO_TCP, sock);
-	if (r) {
-		TFW_ERR("Can't create back-end connections socket (%d)\n", r);
-		return r;
+	t = kthread_run(
+		breconnd_main_loop,
+		NULL,
+		TFW_BRECONND_NAME
+	);
+
+	if (IS_ERR_OR_NULL(t)) {
+		TFW_ERR("Can't create thread: %s (%ld)",
+			TFW_BRECONND_NAME,
+			PTR_ERR(t));
+	} else {
+		breconnd_task = t;
 	}
+}
 
-	r = kernel_connect(*sock, (struct sockaddr *)addr, sza, 0);
-	if (r) {
-		goto err_sock_destroy;
-	}
-	sk = (*sock)->sk;
 
-	/*
-	 * TODO Set up socket callbacks.
-	 * Do we need any?
+static void
+stop_breconnd_thread(void)
+{
+	/* FIXME:
+	 * The function will block while the thread sleeps because the
+	 * kthread_stop() waits until the thread exits.
+	 * We need to somehow notify the thread that it should exit and then
+	 * interrupt its sleep (perhaps by sending a signal).
 	 */
-
-	/*
-	 * TODO only one server connection is established now.
-	 * Create N connections to each server for redundancy,
-	 * so we shuldn't allocate a new server for each connection.
-	 */
-	srv = tfw_create_server(sk);
-	if (!srv) {
-		char buf[MAX_ADDR_LEN];
-		tfw_inet_ntop(addr, buf);
-		TFW_ERR("Can't create server descriptor for %s\n", buf);
-		goto err_sock_destroy;
+	if (breconnd_task) {
+		TFW_DBG("Stopping thread: %s\n", TFW_BRECONND_NAME);
+		kthread_stop(breconnd_task);
+		breconnd_task = NULL;
 	}
+}
 
-	sk->sk_user_data = &dummy_proto;
-	r = tfw_connection_new(sk, Conn_Srv, srv, tfw_destroy_server);
-	if (r)
-		goto err_conn_create;
 
-	return 0;
-err_conn_create:
-	tfw_destroy_server(sk);
-err_sock_destroy:
-	sock_release(*sock);
-	*sock = NULL;
-	return r;
+/*
+ * Apply the new backends configuration.
+ *
+ * This function should be called when you change the backend configuration
+ * (the addresses list in tfw_cfg). It closes all active connections to backends
+ * and then opens new ones within the new configuration.
+ * Also it restarts the thread that re-connects to the backends in background.
+ */
+void
+tfw_apply_new_backends_cfg(void)
+{
+	stop_breconnd_thread();
+	close_all_backend_sockets();
+	copy_new_backend_addresses_from_cfg();
+
+	if (backend_socks_n > 0) {
+		open_new_backend_sockets();
+		start_breconnd_thread();
+	}
+}
+
+
+/*
+ * The clean-up routine: it closes all backend connections, terminates
+ * background threads and releases allocated memory.
+ */
+void
+tfw_release_backend_sockets(void)
+{
+	stop_breconnd_thread();
+	close_all_backend_sockets();
+	free_backends_mem();
 }
