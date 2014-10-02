tempesta-tech/tempesta

commit 1cd52cf8f0052c00125e9f08d50518e1c49dc5e8

fix: #19 - A Connection could not be closed before it is scheduled
The commit fixes closing a connection at a point when no TfwSession is
yet established.

Changes:

1. tfw_connection_free() now releases a TfwSession and a peer
TfwConnection only if they exist.

2. tfw_connection_send_srv() doesn't close the connection by itself.
Instead, it returns an error code which is propagated up to Synchronous
Sockets, and the connection is closed from ss_do_close().
That is done to avoid a condition where the connection is referenced by
SS after it is closed (and free()'d) in tfw_connection_send_srv().

3. tfw_sess_conn() and tfw_connection_peer() now check for existence of
session and connection objects and return NULL if they don't exist.

4. tfw_http_conn_destruct() frees a HTTP request attached to the
message only if it exists. That is done to avoid another NULL pointer
dereference that occurs because the requests are pipelined now, and
this "current" request may be already pipelined and set to NULL when
the destructor is called.

5. tfw_http_req_process() doesn't free a HTTP request anymore.
It is done from a destructor called by ss_do_close() when
tfw_http_req_process() returns TFW_BLOCK.

6. tfw_session_free() now releases all pipelined HTTP requests stored
in the session's queue.

tempesta_fw/connection.h
@@ -83,16 +83,23 @@ typedef struct {
 static inline TfwConnection *
 tfw_sess_conn(TfwSession *sess, int type)
 {
-	if (type & Conn_Clnt)
+	if ((type & Conn_Clnt) && sess->cli)
 		return sess->cli->sock->sk_user_data;
-	return sess->srv->sock->sk_user_data;
+	if ((type & Conn_Srv) && sess->srv)
+		return sess->srv->sock->sk_user_data;
+
+	return NULL;
 }
 
 static inline TfwConnection *
 tfw_connection_peer(TfwConnection *c)
 {
+	if (!c->sess)
+		return NULL;
+
 	if (c->type & Conn_Clnt)
 		return tfw_sess_conn(c->sess, Conn_Srv);
+
 	return tfw_sess_conn(c->sess, Conn_Clnt);
 }
 
@@ -100,7 +107,7 @@ tfw_connection_peer(TfwConnection *c)
 int tfw_connection_new(struct sock *sk, int type, void *handler,
 		       void (*destructor)(struct sock *s));
 void tfw_connection_send_cli(TfwSession *sess, TfwMsg *msg);
-void tfw_connection_send_srv(TfwSession *sess, TfwMsg *msg);
+int tfw_connection_send_srv(TfwSession *sess, TfwMsg *msg);
 
 void tfw_connection_hooks_register(TfwConnHooks *hooks, int type);
 

tempesta_fw/http.c
@@ -57,7 +57,8 @@ tfw_http_conn_init(TfwConnection *conn)
 static void
 tfw_http_conn_destruct(TfwConnection *conn)
 {
-	tfw_http_msg_free((TfwHttpMsg *)conn->msg);
+	if (conn->msg)
+		tfw_http_msg_free((TfwHttpMsg *)conn->msg);
 }
 
 /**
@@ -685,8 +686,11 @@ tfw_http_req_process(TfwConnection *conn, unsigned char *data, size_t len)
 		} else {
 			if (tfw_http_adjust_req(req))
 				goto block;
+
 			/* Send the request to appropriate server. */
-			tfw_connection_send_srv(sess, (TfwMsg *)req);
+			if (tfw_connection_send_srv(sess, (TfwMsg *)req)) {
+				goto block;
+			}
 		}
 
 		if (!req->parser.data_off || req->parser.data_off == len)
@@ -704,7 +708,6 @@ tfw_http_req_process(TfwConnection *conn, unsigned char *data, size_t len)
 
 	return r;
 block:
-	tfw_http_msg_free((TfwHttpMsg *)req);
 	return TFW_BLOCK;
 }
 

tempesta_fw/http_msg.c
@@ -55,6 +55,8 @@ tfw_http_msg_alloc(int type)
 void
 tfw_http_msg_free(TfwHttpMsg *m)
 {
+	TFW_DBG("Free msg: %p", m);
+
 	/*
 	 * FIXME do we need to synchronize this?
 	 * If a connection can be processed from different CPUs, then we do.

tempesta_fw/session.c
@@ -19,6 +19,10 @@
  * this program; if not, write to the Free Software Foundation, Inc., 59
  * Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+
+#include <linux/list.h>
+
+#include "http_msg.h"
 #include "log.h"
 #include "sched.h"
 #include "session.h"
@@ -56,6 +60,16 @@ tfw_session_create(TfwClient *cli)
 void
 tfw_session_free(TfwSession *s)
 {
+	TfwHttpReq *req, *tmp;
+
+	TFW_DBG("Free session: %p\n", s);
+
+	/* Release all pipelined HTTP requests. */
+	list_for_each_entry_safe(req, tmp, &s->req_list, list) {
+		list_del(&req->list);
+		tfw_http_msg_free((TfwHttpMsg *)req);
+	}
+
 	kmem_cache_free(sess_cache, s);
 }
 

tempesta_fw/connection.c
@@ -55,15 +55,21 @@ tfw_connection_alloc(int type, void *handler)
 static void
 tfw_connection_free(TfwConnection *c)
 {
-	TfwConnection *peer_conn = tfw_connection_peer(c);
+	TFW_DBG("Free connection: %p\n", c);
 
-	/*
-	 * FIXME do we need to synchronize this?
-	 * If a connection can be processed from different CPUs, then we do.
-	 */
-	peer_conn->sess = NULL;
+	if (c->sess) {
+		/*
+		 * FIXME do we need to synchronize this?
+		 * If a connection can be processed from different CPUs, then we do.
+		 */
+		TfwConnection *peer_conn = tfw_connection_peer(c);
+		if (peer_conn) {
+			TFW_DBG("Detach from peer: %p\n", peer_conn);
+			peer_conn->sess = NULL;
+		}
+		tfw_session_free(c->sess);
+	}
 
-	tfw_session_free(c->sess);
 	kmem_cache_free(conn_cache, c);
 }
 
@@ -134,7 +140,7 @@ tfw_connection_send_cli(TfwSession *sess, TfwMsg *msg)
 	ss_send(sess->cli->sock, &msg->skb_list);
 }
 
-void
+int
 tfw_connection_send_srv(TfwSession *sess, TfwMsg *msg)
 {
 	TfwConnection *srv_conn;
@@ -152,8 +158,7 @@ tfw_connection_send_srv(TfwSession *sess, TfwMsg *msg)
 	if (tfw_session_sched_msg(sess, msg)) {
 		TFW_ERR("Cannot schedule message, msg=%p clnt=%p\n",
 			msg, sess->cli);
-		tfw_connection_close(sess->cli->sock);
-		return;
+		return -1;
 	}
 
 	/* Bind the server connection with the session. */
@@ -167,6 +172,8 @@ tfw_connection_send_srv(TfwSession *sess, TfwMsg *msg)
 	srv_conn->sess = sess;
 
 	ss_send(sess->srv->sock, &msg->skb_list);
+
+	return 0;
 }
 
 /*

commit 2d8fcfc2b4d18557b5d8f0115c8237516667941f

#23 - fix defects introduced by commit 1cd52cf
- Any client connection must have a session associated with it, so
  corresponding BUG()s aredded to tfw_sess_conn() and
  tfw_connection_peer() for servers.

- tfw_http_msg_free() is NULL safe now.

- tfw_http_msg_free() is moved to TfwMsg->destructor to let TfwSession
  code work with generic TfwMsg instead of specific TfwHttpReq.
  Also the pipeline list_head is moved from TfwHttpReq to TfwMsg for 
  the same reason.

tempesta_fw/connection.h
@@ -84,24 +84,37 @@ typedef struct {
 static inline TfwConnection *
 tfw_sess_conn(TfwSession *sess, int type)
 {
-	if ((type & Conn_Clnt) && sess->cli)
+	if (type & Conn_Clnt) {
+		BUG_ON(!sess->cli);
 		return sess->cli->sock->sk_user_data;
-	if ((type & Conn_Srv) && sess->srv)
-		return sess->srv->sock->sk_user_data;
+	}
 
+	if (type & Conn_Srv) {
+		if (!sess->srv)
+			return NULL;
+		return sess->srv->sock->sk_user_data;
+	}
+
+	BUG();
 	return NULL;
 }
 
 static inline TfwConnection *
 tfw_connection_peer(TfwConnection *c)
 {
-	if (!c->sess)
-		return NULL;
-
-	if (TFW_CONN_TYPE(c) & Conn_Clnt)
+	if (TFW_CONN_TYPE(c) & Conn_Clnt) {
+		BUG_ON(!c->sess);
 		return tfw_sess_conn(c->sess, Conn_Srv);
+	}
 
-	return tfw_sess_conn(c->sess, Conn_Clnt);
+	if (TFW_CONN_TYPE(c) & Conn_Srv) {
+		if (!c->sess)
+			return NULL;
+		return tfw_sess_conn(c->sess, Conn_Clnt);
+	}
+
+	BUG();
+	return NULL;
 }
 
 /* Connection downcalls. */

tempesta_fw/http.c
@@ -57,8 +57,7 @@ tfw_http_conn_init(TfwConnection *conn)
 static void
 tfw_http_conn_destruct(TfwConnection *conn)
 {
-	if (conn->msg)
-		tfw_http_msg_free((TfwHttpMsg *)conn->msg);
+	tfw_http_msg_free((TfwHttpMsg *)conn->msg);
 }
 
 /**
@@ -662,7 +661,7 @@ tfw_http_req_process(TfwConnection *conn, unsigned char *data, size_t len)
 			return TFW_POSTPONE;
 		case TFW_PASS:
 			/* Request is fully parsed, add it to the connection. */
-			list_add_tail(&req->list, &sess->req_list);
+			list_add_tail(&req->msg.pl_list, &sess->req_list);
 			conn->msg = NULL;
 
 			tfw_http_establish_skb_hdrs((TfwHttpMsg *)req);
@@ -774,8 +773,8 @@ tfw_http_resp_process(TfwConnection *conn, unsigned char *data, size_t len)
 			TFW_WARN("Response w/o request\n");
 			goto block;
 		}
-		req = list_first_entry(&sess->req_list, TfwHttpReq, list);
-		list_del(&req->list);
+		req = list_first_entry(&sess->req_list, TfwHttpReq, msg.pl_list);
+		list_del(&req->msg.pl_list);
 		tfw_cache_add(resp, req);
 
 		/* Now we don't need the request anymore. */

tempesta_fw/http.h
@@ -131,7 +131,6 @@ typedef struct {
 
 typedef struct {
 	TFW_HTTP_MSG_COMMON;
-	struct list_head list;
 	unsigned char	method;
 	TfwStr		host; /* host in URI, may differ from Host header */
 	TfwStr		uri;

tempesta_fw/http_msg.c
@@ -19,12 +19,7 @@
  */
 #include "gfsm.h"
 #include "http.h"
-
-static void
-__tfw_http_msg_init_req(TfwHttpReq *req)
-{
-	INIT_LIST_HEAD(&req->list);
-}
+#include "http_msg.h"
 
 TfwHttpMsg *
 tfw_http_msg_alloc(int type)
@@ -43,8 +38,9 @@ tfw_http_msg_alloc(int type)
 	hm->h_tbl->off = 0;
 	memset(hm->h_tbl->tbl, 0, __HHTBL_SZ(1) * sizeof(TfwHttpHdr));
 
-	if (type & Conn_Clnt)
-		__tfw_http_msg_init_req((TfwHttpReq *)hm);
+	INIT_LIST_HEAD(&hm->msg.pl_list);
+
+	hm->msg.destructor = (tfw_msg_destructor_t)tfw_http_msg_free;
 
 	return hm;
 }
@@ -56,6 +52,10 @@ void
 tfw_http_msg_free(TfwHttpMsg *m)
 {
 	TFW_DBG("Free msg: %p", m);
+
+	/* Allow passing a NULL pointer as the argument (similar to kfree()). */
+	if (!m)
+		return;
 
 	/*
 	 * FIXME do we need to synchronize this?

tempesta_fw/msg.h
@@ -28,11 +28,29 @@
 
 #include "gfsm.h"
 
+typedef struct tfw_msg TfwMsg;
+typedef void (*tfw_msg_destructor_t)(TfwMsg *msg);
+
 typedef struct tfw_msg {
 	struct tfw_msg	*prev;		/* sibling messages */
 	TfwGState	state;		/* message processing state. */
 	SsSkbList	skb_list;	/* list of sk_buff's belonging
 					   to the message. */
+	struct list_head pl_list;	/* Element of a pipeline list. */
+	tfw_msg_destructor_t destructor;
 } TfwMsg;
+
+
+/**
+ * Invoke TfwMsg destructor.
+ * Also the macro sets given @msg_ptr to NULL because the destructor is supposed
+ * to free some allocated memory, so the pointer becomes invalid after the call.
+ */
+#define tfw_msg_destruct(msg_ptr) 	\
+do { 					\
+	BUG_ON(!msg_ptr->destructor); 	\
+	msg_ptr->destructor(msg_ptr);	\
+	msg_ptr = NULL;			\
+} while (0)
 
 #endif /* __TFW_MSG_H__ */

tempesta_fw/session.c
@@ -22,7 +22,6 @@
 
 #include <linux/list.h>
 
-#include "http_msg.h"
 #include "log.h"
 #include "sched.h"
 #include "session.h"
@@ -60,14 +59,14 @@ tfw_session_create(TfwClient *cli)
 void
 tfw_session_free(TfwSession *s)
 {
-	TfwHttpReq *req, *tmp;
+	TfwMsg *msg, *tmp;
 
 	TFW_DBG("Free session: %p\n", s);
 
 	/* Release all pipelined HTTP requests. */
-	list_for_each_entry_safe(req, tmp, &s->req_list, list) {
-		list_del(&req->list);
-		tfw_http_msg_free((TfwHttpMsg *)req);
+	list_for_each_entry_safe(msg, tmp, &s->req_list, pl_list) {
+		list_del(&msg->pl_list);
+		tfw_msg_destruct(msg);
 	}
 
 	kmem_cache_free(sess_cache, s);