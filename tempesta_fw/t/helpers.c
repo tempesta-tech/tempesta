0e5e890f38a1f9246a89edb6ac6d7449ebaa1f67
Dmitry Vasilyanov <vdmit11@gmail.com>
2014-12-04 22:06:05 +0300

    add basic tests for headers parsing code

---
+++ tempesta_fw/t/helpers.c
@@ -0,0 +1,60 @@
+/**
+ *		Tempesta FW
+ *
+ * This file contains utils that help to test certain Tempesta FW modules.
+ * They implement things like stubbing, mocking, generating data for testing.
+ *
+ * Actually things contained in this file are wrong a bit.
+ * Good code tends to have most of the logic in pure stateless loosely-coupled
+ * well-isolated functions that may be tested without faking any state.
+ * But this is not reachable most of the time, especially when performance is
+ * a goal and you have to build the architecture keeping it in mind.
+ * So over time, we expect to see a decent amount of helpers here.
+ *
+ * These things are specific to Tempesta FW, so they are located here,
+ * and generic testing functions/macros are located in test.c/test.h
+ *
+ *
+ * Copyright (C) 2012-2014 NatSys Lab. (info@natsys-lab.com).
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "http_msg.h"
+
+TfwHttpReq *
+test_req_alloc(void)
+{
+	TfwHttpReq *req;
+
+	/* Actually there were more code here, mostly it was copy-paste from
+	 * tfw_http_msg_alloc(). It is removed because we need to test how it
+	 * initializes the message and we would not like to test the copy-paste.
+	 */
+	req = (TfwHttpReq *)tfw_http_msg_alloc(Conn_HttpClnt);
+	BUG_ON(!req);
+
+	return req;
+}
+
+void
+test_req_free(TfwHttpReq *req)
+{
+	/* In tests we are stricter: we don't allow to free a NULL pointer
+	 * to be sure exactly what we are free'ing and to catch bugs early. */
+	BUG_ON(!req);
+
+	tfw_http_msg_free((TfwHttpMsg *)req);
+}
---
+++ tempesta_fw/t/helpers.h
@@ -0,0 +1,28 @@
+/**
+ *		Tempesta FW
+ *
+ * Copyright (C) 2012-2014 NatSys Lab. (info@natsys-lab.com).
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+#ifndef __TFW_TEST_HELPER_H__
+#define __TFW_TEST_HELPER_H__
+
+/* These functions help to create fake HTTP messages for testing without
+ * involving complicated stuff like sk_buff manipulations. */
+TfwHttpReq *test_req_alloc(void);
+void test_req_free(TfwHttpReq *req);
+
+#endif /* __TFW_TEST_HELPER_H__ */
