aba321781910c8cf25194b9ab4c4a353c4fbab95
Dmitry Vasilyanov <vdmit11@gmail.com>
2014-12-05 06:15:54 +0300

    add infrastructure for running functional test in addition to unit tests

---
+++ tempesta_fw/t/functional/run_all_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# 2012-2014. Written by NatSys Lab. (info@natsys-lab.com).
+
+function run() {
+	echo run: $1
+	python3 $(dirname $0)/$1	
+}
+
+echo
+echo ------------------------------------------------------------------
+echo Running functional tests...
+echo ------------------------------------------------------------------
+ 
+echo nothing here yet
