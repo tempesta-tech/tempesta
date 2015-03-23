1a7e5368ba287dc962ffad915e40cf3cd8960ccd
Dmitry Vasilyanov <vdmit11@gmail.com>
2015-03-23 04:39:28 +0300

    frang: switch from sysctl to tempesta cfg framework

--- tempesta_fw/classifier/frang.c
+++ tempesta_fw/classifier/frang.c
@@ -15,12 +15,6 @@
  * 	3. HTTP method and Content-Type restrictions (check that the value is
  * 	   in a set of allowed values defined by the user).
  *
- * The module exports appropriate configuration options in
- * /proc/net/tempesta/frang directory.
- * 	- options with names *_rate define requests/connections rate per second.
- * 	- *_burst are temporal burst for 1/FRANG_FREQ of second.
- * 	- http_* are static limits for contents of a HTTP request.
- *
  * Copyright (C) 2012-2014 NatSys Lab. (info@natsys-lab.com).
  * Copyright (C) 2015 Tempesta Technologies, Inc.
  *
@@ -90,29 +84,32 @@ FrangHashBucket frang_hash[1 << FRANG_HASH_BITS] = {
 
 static struct kmem_cache *frang_mem_cache;
 
-/* Limits (zero means unlimited). */
-static unsigned int frang_req_rate = 0;
-static unsigned int frang_req_burst = 0;
-static unsigned int frang_conn_rate = 0;
-static unsigned int frang_conn_burst = 0;
-static unsigned int frang_conn_max = 0;
-
-/* Limits for HTTP request contents: uri, headers, body, etc. */
-static unsigned int frang_http_uri_len = 0;
-static unsigned int frang_http_field_len = 0;
-static unsigned int frang_http_body_len = 0;
-static unsigned long frang_http_methods_mask = 0;
-static bool frang_http_ct_is_required = false;
-static bool frang_http_host_is_required = false;
-
-/* The list of allowed Content-Type values. */
-
 typedef struct {
 	char   *str;
 	size_t len;	/* The pre-computed strlen(@str). */
 } FrangCtVal;
 
-static FrangCtVal *frang_http_ct_vals __rcu;
+typedef struct {
+	/* Limits (zero means unlimited). */
+	unsigned int 	req_rate;
+	unsigned int 	req_burst;
+	unsigned int 	conn_rate;
+	unsigned int 	conn_burst;
+	unsigned int 	conn_max;
+
+	/* Limits for HTTP request contents: uri, headers, body, etc. */
+	unsigned int 	http_uri_len;
+	unsigned int 	http_field_len;
+	unsigned int 	http_body_len;
+	bool 		http_ct_is_required;
+	bool 		http_host_is_required;
+	/* The bitmask of allowed HTTP Method values. */
+	unsigned long 	http_methods_mask;
+	/* The list of allowed Content-Type values. */
+	FrangCtVal	*http_ct_vals;
+} FrangCfg;
+
+static FrangCfg frang_cfg __read_mostly;
 
 static void
 frang_get_ipv6addr(struct sock *sk, struct in6_addr *addr)
@@ -203,16 +200,16 @@ frang_conn_limit(FrangAcc *ra, struct sock *unused)
 	ra->history[i].conn_new++;
 	ra->conn_curr++;
 
-	if (frang_conn_max && ra->conn_curr > frang_conn_max)
+	if (frang_cfg.conn_max && ra->conn_curr > frang_cfg.conn_max)
 		return TFW_BLOCK;
-	if (frang_req_burst && ra->history[i].req > frang_req_burst)
+	if (frang_cfg.req_burst && ra->history[i].req > frang_cfg.req_burst)
 		return TFW_BLOCK;
 
 	/* Collect new connections sum. */
 	for (i = 0; i < FRANG_FREQ; i++)
 		if (ra->history[i].ts + FRANG_FREQ >= ts)
 			csum += ra->history[i].conn_new;
-	if (frang_conn_rate && csum > frang_conn_rate)
+	if (frang_cfg.conn_rate && csum > frang_cfg.conn_rate)
 		return TFW_BLOCK;
 
 	return TFW_PASS;
@@ -257,14 +254,14 @@ frang_req_limit(FrangAcc *ra, struct sock *sk)
 	}
 	ra->history[i].req++;
 
-	if (frang_req_burst && ra->history[i].req > frang_req_burst)
+	if (frang_cfg.req_burst && ra->history[i].req > frang_cfg.req_burst)
 		goto block;
 
 	/* Collect current request sum. */
 	for (i = 0; i < FRANG_FREQ; i++)
 		if (ra->history[i].ts + FRANG_FREQ >= ts)
 			rsum += ra->history[i].req;
-	if (frang_req_rate && rsum > frang_req_rate)
+	if (frang_cfg.req_rate && rsum > frang_cfg.req_rate)
 		goto block;
 
 	return TFW_PASS;
@@ -284,7 +281,8 @@ frang_http_uri_len_limit(const TfwHttpReq *req)
 {
 	/* FIXME: tfw_str_len() iterates over chunks to calculate the length.
 	 * This is too slow. The value must be stored in a TfwStr field. */
-	if (frang_http_uri_len && tfw_str_len(&req->uri) > frang_http_uri_len) {
+	if (frang_cfg.http_uri_len &&
+	    tfw_str_len(&req->uri) > frang_cfg.http_uri_len) {
 		TFW_DBG("frang: http_uri_len limit is reached\n");
 		return TFW_BLOCK;
 	}
@@ -297,11 +295,11 @@ frang_http_field_len_limit(const TfwHttpReq *req)
 {
 	const TfwStr *field, *end;
 
-	if (!frang_http_field_len)
+	if (!frang_cfg.http_field_len)
 		return TFW_PASS;
 
 	TFW_HTTP_FOR_EACH_HDR_FIELD(field, end, req) {
-		if (tfw_str_len(field) > frang_http_field_len) {
+		if (tfw_str_len(field) > frang_cfg.http_field_len) {
 			TFW_DBG("frang: http_field_len limit is reached\n");
 			return TFW_BLOCK;
 		}
@@ -313,8 +311,8 @@ frang_http_field_len_limit(const TfwHttpReq *req)
 static int
 frang_http_body_len_limit(const TfwHttpReq *req)
 {
-	if (frang_http_body_len &&
-	    tfw_str_len(&req->body) > frang_http_body_len) {
+	if (frang_cfg.http_body_len &&
+	    tfw_str_len(&req->body) > frang_cfg.http_body_len) {
 		TFW_DBG("frang: http_body_len limit is reached\n");
 		return TFW_BLOCK;
 	}
@@ -345,7 +343,7 @@ frang_http_methods_check(const TfwHttpReq *req)
 {
 	unsigned long m = (1 << req->method);
 
-	if (frang_http_methods_mask && (frang_http_methods_mask & m))
+	if (frang_cfg.http_methods_mask && (frang_cfg.http_methods_mask & m))
 		return TFW_PASS;
 
 	TFW_DBG("frang: forbidden method: %d (%#lx)\n", req->method, m);
@@ -360,7 +358,8 @@ frang_http_ct_check(const TfwHttpReq *req)
 	TfwStr *ct, *end;
 	FrangCtVal *curr;
 
-	if (!frang_http_ct_is_required || req->method != TFW_HTTP_METH_POST)
+	if (!frang_cfg.http_ct_is_required || !frang_cfg.http_ct_vals ||
+	    req->method != TFW_HTTP_METH_POST)
 		return TFW_PASS;
 
 	/* Find the Content-Type header.
@@ -390,13 +389,11 @@ frang_http_ct_check(const TfwHttpReq *req)
 	 * TODO: don't store field name in the TfwStr. Store only the header
 	 * value, and thus get rid of the nasty tfw_str_eq_kv().
 	 */
-	rcu_read_lock();
-	for (curr = rcu_dereference(frang_http_ct_vals); curr->str; ++curr) {
+	for (curr = frang_cfg.http_ct_vals; curr->str; ++curr) {
 		if (tfw_str_eq_kv(ct, _CT, _CTLEN, ':', curr->str, curr->len,
 				  TFW_STR_EQ_PREFIX_CASEI))
 			break;
 	}
-	rcu_read_unlock();
 
 	if (!curr->str) {
 		TFW_DBG("frang: forbidden Content-Type value\n");
@@ -413,7 +410,7 @@ frang_http_host_check(const TfwHttpReq *req)
 {
 	TfwStr *field;
 
-	if (!frang_http_host_is_required || req->method != TFW_HTTP_METH_POST)
+	if (!frang_cfg.http_host_is_required || req->method != TFW_HTTP_METH_POST)
 		return TFW_PASS;
 
 	field = &req->h_tbl->tbl[TFW_HTTP_HDR_HOST].field;
@@ -461,130 +458,52 @@ static TfwClassifier frang_class_ops = {
 	.classify_conn_close	= frang_conn_close,
 };
 
-static int
-frang_parse_int(char *tmp_buf, int *out_val)
-{
-	char *p;
-	int val = 0;
-
-	for (p = tmp_buf; *p; ++p) {
-		if (!isdigit(*p)) {
-			TFW_ERR("not a digit: '%c'\n", *p);
-			return -EINVAL;
-		}
-		val = val * 10 + *p - '0';
-	}
-
-	*out_val = val;
-	return 0;
-}
+static const TfwCfgEnum frang_http_methods_enum[] = {
+	{ "get", TFW_HTTP_METH_GET },
+	{ "post", TFW_HTTP_METH_POST },
+	{ "head", TFW_HTTP_METH_HEAD },
+	{}
+};
 
 static int
-frang_parse_bool(char *tmp_buf, bool *out_bool)
+frang_set_methods_mask(TfwCfgSpec *cs, TfwCfgEntry *ce)
 {
-	char c = *tmp_buf;
+	int i, r, method_id;
+	const char *method_str;
+	unsigned long methods_mask = 0;
 
-	/* XXX: should we support true/false/etc instead of 0/1 here? */
-	if (c != '0' && c != '1') {
-		TFW_ERR("invalid boolean value: %s\n", tmp_buf);
-		return -EINVAL;
-	}
-
-	*out_bool = c - '0';
-	return 0;
-}
-
-/* TODO: refactoring: get rid of these sysctl handlers,
- * replace them with Tempesta's cfg framework. */
-
-static int
-frang_find_idx_by_str(const char **str_vec, size_t vec_size, const char *str)
-{
-	int i;
-	const char *curr_str;
-
-	for (i = 0; i < vec_size; ++i) {
-		curr_str = str_vec[i];
-		BUG_ON(!curr_str);
-		if (!strcasecmp(curr_str, str))
-			return i;
-	}
-
-	return -1;
-}
-
-#define FRANG_TOKEN_SEPARATORS	" \r\n\t"
-
-static char *
-frang_tokenize(char **tmp_buf)
-{
-	char *token = strsep(tmp_buf, FRANG_TOKEN_SEPARATORS);
-
-	/* Unlike strsep(), don't return empty tokens. */
-	if (token && !*token)
-		token = NULL;
-
-	return token;
-}
-
-static int
-frang_count_tokens(char *str)
-{
-	int n = 0;
-
-	while (*str) {
-		str += strspn(str, FRANG_TOKEN_SEPARATORS);
-		if (*str)
-			++n;
-		str += strcspn(str, FRANG_TOKEN_SEPARATORS);
-	}
-
-	return n;
-}
-
-static int
-frang_parse_methods_mask(char *tmp_buf, unsigned long *out_mask)
-{
-	static const char *strs[_TFW_HTTP_METH_COUNT] = {
-		[TFW_HTTP_METH_GET] = "get",
-		[TFW_HTTP_METH_POST] = "post",
-		[TFW_HTTP_METH_HEAD] = "head",
-	};
-	char *token;
-	int idx;
-	unsigned long methods_mask;
-
-	token = tmp_buf;
-	methods_mask = 0;
-	while ((token = frang_tokenize(&tmp_buf))) {
-		idx = frang_find_idx_by_str(strs, ARRAY_SIZE(strs), token);
-		if (idx < 0) {
-			TFW_ERR("frang: invalid method: '%s'\n", token);
+	TFW_CFG_ENTRY_FOR_EACH_VAL(ce, i, method_str) {
+		r = tfw_cfg_map_enum(frang_http_methods_enum, method_str,
+				     &method_id);
+		if (r) {
+			TFW_ERR("frang: invalid method: '%s'\n", method_str);
 			return -EINVAL;
 		}
 
-		TFW_DBG("frang: parsed method: %s => %d\n", token, idx);
-		methods_mask |= (1 << idx);
+		TFW_DBG("frang: parsed method: %s => %d\n",
+			method_str, method_id);
+		methods_mask |= (1 << method_id);
 	}
 
 	TFW_DBG("parsed methods_mask: %#lx\n", methods_mask);
-	*out_mask = methods_mask;
+	frang_cfg.http_methods_mask = methods_mask;
 	return 0;
 }
 
+static void
+frang_clear_methods_mask(TfwCfgSpec *cs)
+{
+	frang_cfg.http_methods_mask = 0;
+}
+
 static int
-frang_parse_ct_vals(char *tmp_buf, void *unused)
+frang_set_ct_vals(TfwCfgSpec *cs, TfwCfgEntry *ce)
 {
 	void *mem;
-	char *token, *strs, *strs_pos;
-	size_t tokens_n, vals_size, strs_size;
-	FrangCtVal *vals, *vals_pos, *old_vals;
-
-	tokens_n = frang_count_tokens(tmp_buf);
-	if (!tokens_n) {
-		TFW_ERR("the frang_http_ct_vals is empty\n");
-		return -EINVAL;
-	}
+	const char *in_str;
+	char *strs, *strs_pos;
+	FrangCtVal *vals, *vals_pos;
+	size_t i, strs_size, vals_n, vals_size;
 
 	/* Allocate a single chunk of memory which is suitable to hold the
 	 * variable-sized list of variable-sized strings.
@@ -599,221 +518,130 @@ frang_parse_ct_vals(char *tmp_buf, void *unused)
 	 *                               |                           |
 	 *                               +---------------------------+
 	 */
-	strs_size = strlen(tmp_buf) + 1;
-	vals_size = sizeof(FrangCtVal) * (tokens_n + 1);
+	vals_n = ce->val_n;
+	vals_size = sizeof(FrangCtVal) * (vals_n + 1);
+	strs_size = 0;
+	TFW_CFG_ENTRY_FOR_EACH_VAL(ce, i, in_str) {
+		strs_size += strlen(in_str) + 1;
+	}
 	mem = kzalloc(vals_size + strs_size, GFP_KERNEL);
+	if (!mem)
+		return -ENOMEM;
 	vals = mem;
 	strs = mem + vals_size;
 
-	/* Copy tokens from tmp_buf to the vals/strs list. */
+	/* Copy tokens to the new vals/strs list. */
 	/* TODO: validate tokens, they should look like: "text/plain". */
 	vals_pos = vals;
 	strs_pos = strs;
-	while ((token = frang_tokenize(&tmp_buf))) {
-		size_t len = strlen(token) + 1;
-		BUG_ON(!len);
+	TFW_CFG_ENTRY_FOR_EACH_VAL(ce, i, in_str) {
+		size_t len = strlen(in_str) + 1;
 
-		memcpy(strs_pos, token, len);
+		memcpy(strs_pos, in_str, len);
 		vals_pos->str = strs_pos;
 		vals_pos->len = (len - 1);
-		TFW_DBG("parsed Content-Type value: '%s'\n", strs_pos);
+
+		TFW_DBG("parsed Content-Type value: '%s'\n", in_str);
 
 		vals_pos++;
 		strs_pos += len;
 	}
-	BUG_ON(vals_pos != (vals + tokens_n));
-	BUG_ON(strs_pos > (strs + strs_size));
+	BUG_ON(vals_pos != (vals + vals_n));
+	BUG_ON(strs_pos != (strs + strs_size));
 
-	/* Replace the old list of allowed Content-Type values. */
-	/* TODO: sort values to make binary search possible. */
-	old_vals = frang_http_ct_vals;
-	rcu_assign_pointer(frang_http_ct_vals, vals);
-	synchronize_rcu();
-	kfree(old_vals);
+	frang_cfg.http_ct_vals = vals;
 	return 0;
 }
 
-static int
-frang_sysctl_handle(ctl_table *ctl, int write,
-		  void __user *buffer, size_t *lenp, loff_t *ppos)
+static void
+frang_free_ct_vals(TfwCfgSpec *cs)
 {
-	int r, len;
-	char *tmp_buf, *trimmed_buf;
-	void *parse_dest;
-	int (*parse_fn)(const char *tmp_buf, void *dest);
-
-	tmp_buf = NULL;
-	parse_fn = ctl->extra1;
-	parse_dest = ctl->extra2;
-	BUG_ON(!parse_fn);
-
-	if (write) {
-		tmp_buf = kzalloc(ctl->maxlen + 1, GFP_KERNEL);
-		if (!tmp_buf) {
-			TFW_ERR("frang: can't allocate temporary buffer\n");
-			r = -ENOMEM;
-			goto out;
-		}
-
-		len =  min(ctl->maxlen, (int)*lenp);
-		if (copy_from_user(tmp_buf, buffer, len)) {
-			TFW_ERR("frang: can't copy data from userspace\n");
-			r = -EFAULT;
-			goto out;
-		}
-
-		trimmed_buf = strim(tmp_buf);
-		if (parse_fn(trimmed_buf, parse_dest)) {
-			TFW_ERR("frang: can't parse input data\n");
-			r = -EINVAL;
-			goto out;
-		}
-	}
-
-	r = proc_dostring(ctl, write, buffer, lenp, ppos);
-	if (r)
-		TFW_ERR("frang: sysctl error\n");
-out:
-	if (r)
-		TFW_ERR("frang: can't read/write parameter: %s\n",
-			ctl->procname);
-	kfree(tmp_buf);
-	return r;
+	kfree(frang_cfg.http_ct_vals);
+	frang_cfg.http_ct_vals = NULL;
 }
 
-#define FRANG_INT_LEN		10
-#define FRANG_STR_LEN		255
-#define FRANG_LONG_STR_LEN	1024
-
-char frang_req_rate_str[FRANG_INT_LEN];
-char frang_req_burst_str[FRANG_INT_LEN];
-char frang_conn_rate_str[FRANG_INT_LEN];
-char frang_conn_burst_str[FRANG_INT_LEN];
-char frang_conn_max_str[FRANG_INT_LEN];
-char frang_http_uri_len_str[FRANG_INT_LEN];
-char frang_http_field_len_str[FRANG_INT_LEN];
-char frang_http_body_len_str[FRANG_INT_LEN];
-char frang_http_methods_str[FRANG_STR_LEN];
-char frang_http_ct_is_required_str[FRANG_INT_LEN];
-char frang_http_content_types_str[FRANG_LONG_STR_LEN];
-char frang_http_host_is_required_str[FRANG_INT_LEN];
-
-static ctl_table frang_ctl_table[] = {
+static TfwCfgSpec frang_cfg_section_specs[] = {
 	{
-		.procname	= "request_rate",
-		.data		= frang_req_rate_str,
-		.maxlen		= sizeof(frang_req_rate_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_int,
-		.extra2		= &frang_req_rate,
+		"request_rate", "0",
+		tfw_cfg_set_int,
+		&frang_cfg.req_rate,
 	},
 	{
-		.procname	= "request_burst",
-		.data		= frang_req_burst_str,
-		.maxlen		= sizeof(frang_req_burst_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_int,
-		.extra2		= &frang_req_burst,
+		"request_burst", "0",
+		tfw_cfg_set_int,
+		&frang_cfg.req_burst,
 	},
 	{
-		.procname	= "new_connection_rate",
-		.data		= frang_conn_rate_str,
-		.maxlen		= sizeof(frang_conn_rate_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_int,
-		.extra2		= &frang_conn_rate,
+		"new_connection_rate", "0",
+		tfw_cfg_set_int,
+		&frang_cfg.conn_rate,
 	},
 	{
-		.procname	= "new_connection_burst",
-		.data		= frang_conn_burst_str,
-		.maxlen		= sizeof(frang_conn_burst_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_int,
-		.extra2		= &frang_conn_burst,
+		"new_connection_burst", "0",
+		tfw_cfg_set_int,
+		&frang_cfg.conn_burst,
 	},
 	{
-		.procname	= "concurrent_connections",
-		.data		= frang_conn_max_str,
-		.maxlen		= sizeof(frang_conn_max_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_int,
-		.extra2		= &frang_conn_max,
+		"concurrent_connections", "0",
+		tfw_cfg_set_int,
+		&frang_cfg.conn_max,
 	},
 	{
-		.procname 	= "http_uri_len",
-		.data		= frang_http_uri_len_str,
-		.maxlen		= sizeof(frang_http_uri_len_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_int,
-		.extra2		= &frang_http_uri_len,
+		"http_uri_len", "0",
+		tfw_cfg_set_int,
+		&frang_cfg.http_uri_len,
 	},
 	{
-		.procname 	= "http_field_len",
-		.data		= frang_http_field_len_str,
-		.maxlen		= sizeof(frang_http_field_len_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_int,
-		.extra2		= &frang_http_field_len,
+		"http_field_len", "0",
+		tfw_cfg_set_int,
+		&frang_cfg.http_field_len,
 	},
 	{
-		.procname 	= "http_body_len",
-		.data		= frang_http_body_len_str,
-		.maxlen		= sizeof(frang_http_body_len_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_int,
-		.extra2		= &frang_http_body_len,
+		"http_body_len", "0",
+		tfw_cfg_set_int,
+		&frang_cfg.http_body_len,
 	},
 	{
-		.procname	= "http_methods",
-		.data		= frang_http_methods_str,
-		.maxlen		= sizeof(frang_http_methods_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_methods_mask,
-		.extra2		= &frang_http_methods_mask,
+		"http_host_is_required", "false",
+		tfw_cfg_set_bool,
+		&frang_cfg.http_host_is_required,
 	},
 	{
-		.procname 	= "http_ct_is_required",
-		.data		= frang_http_ct_is_required_str,
-		.maxlen		= sizeof(frang_http_ct_is_required_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_bool,
-		.extra2		= &frang_http_ct_is_required,
+		"http_ct_is_required", "false",
+		tfw_cfg_set_bool,
+		&frang_cfg.http_ct_is_required,
 	},
 	{
-		.procname	= "http_ct_vals",
-		.data		= frang_http_content_types_str,
-		.maxlen		= sizeof(frang_http_content_types_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_ct_vals,
+		"http_methods", NULL,
+		frang_set_methods_mask,
+		.cleanup = frang_clear_methods_mask,
 	},
 	{
-		.procname	= "http_host_is_required",
-		.data		= frang_http_host_is_required_str,
-		.maxlen		= sizeof(frang_http_host_is_required_str),
-		.mode		= 0644,
-		.proc_handler	= frang_sysctl_handle,
-		.extra1		= frang_parse_bool,
-		.extra2		= &frang_http_host_is_required
+		"http_ct_vals", NULL,
+		frang_set_ct_vals,
+		.cleanup = frang_free_ct_vals
 	},
 	{}
+};
+
+static TfwCfgSpec frang_cfg_toplevel_specs[] = {
+	{
+		"frang_limits", NULL,
+		tfw_cfg_handle_children,
+		&frang_cfg_section_specs
+	},
+	{}
+};
+
+static TfwCfgMod frang_cfg_mod = {
+	.name = "frang",
+	.specs = frang_cfg_toplevel_specs
 };
 
 static int __init
 frang_init(void)
 {
 	int r;
-	struct ctl_table_header *frang_ctl;
 
 	frang_mem_cache = KMEM_CACHE(frang_account_t, 0);
 	if (!frang_mem_cache) {
@@ -821,12 +649,10 @@ frang_init(void)
 		return -EINVAL;
 	}
 
-	frang_ctl = register_net_sysctl(&init_net, "net/tempesta/frang",
-				      frang_ctl_table);
-	if (!frang_ctl) {
-		TFW_ERR("frang: can't register sysctl table\n");
-		r = -1;
-		goto err_sysctl;
+	r = tfw_cfg_mod_register(&frang_cfg_mod);
+	if (r) {
+		TFW_ERR("frang: can't register as a configuration module\n");
+		goto err_cfg;
 	}
 
 	r = tfw_classifier_register(&frang_class_ops);
@@ -862,8 +688,8 @@ err_hook:
 err_fsm:
 	tfw_classifier_unregister();
 err_class:
-	unregister_sysctl_table(frang_ctl);
-err_sysctl:
+	tfw_cfg_mod_unregister(&frang_cfg_mod);
+err_cfg:
 	kmem_cache_destroy(frang_mem_cache);
 	return r;
 }
