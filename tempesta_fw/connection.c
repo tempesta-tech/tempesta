6ba6773ec063ddc221d6c69b8f1d540254fc6f6c
Alexander K <ak@natsys-lab.com>
2015-05-04 22:04:13 +0300

    Some cleanups

--- tempesta_fw/connection.c
+++ tempesta_fw/connection.c
@@ -20,7 +20,6 @@
  * this program; if not, write to the Free Software Foundation, Inc., 59
  * Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-
 #include "connection.h"
 #include "gfsm.h"
 #include "lib.h"
@@ -29,47 +28,28 @@
 
 #define TFW_CONN_MAX_PROTOS	TFW_GFSM_FSM_N
 
-static TfwConnHooks *tfw_conn_hooks[TFW_CONN_MAX_PROTOS];
+static TfwConnHooks *conn_hooks[TFW_CONN_MAX_PROTOS];
 
 #define TFW_CONN_HOOK_CALL(conn, hook_name) \
-	tfw_conn_hooks[TFW_CONN_TYPE2IDX(TFW_CONN_TYPE(conn))]->hook_name(conn)
+	conn_hooks[TFW_CONN_TYPE2IDX(TFW_CONN_TYPE(conn))]->hook_name(conn)
 
 void
 tfw_connection_hooks_register(TfwConnHooks *hooks, int type)
 {
 	unsigned hid = TFW_CONN_TYPE2IDX(type);
 
-	BUG_ON(hid >= TFW_CONN_MAX_PROTOS || tfw_conn_hooks[hid]);
+	BUG_ON(hid >= TFW_CONN_MAX_PROTOS || conn_hooks[hid]);
 
-	tfw_conn_hooks[hid] = hooks;
+	conn_hooks[hid] = hooks;
 }
 
-/**
- * The TfwConnection constructor.
- * It initializes @conn fields, but doesn't allocate the TfwConnection object.
- */
 void
-tfw_connection_construct(TfwConnection *conn)
+tfw_connection_init(TfwConnection *conn)
 {
 	memset(conn, 0, sizeof(*conn));
+
 	INIT_LIST_HEAD(&conn->list);
 	INIT_LIST_HEAD(&conn->msg_queue);
-}
-
-/**
- * Check that TfwConnection resources are cleaned up properly.
- */
-void
-tfw_connection_validate_cleanup(TfwConnection *conn)
-{
-	IF_DEBUG {
-		BUG_ON(!conn);
-		BUG_ON(!list_empty(&conn->list));
-		BUG_ON(!list_empty(&conn->msg_queue));
-		BUG_ON(conn->msg);
-		BUG_ON(conn->peer);
-		BUG_ON(conn->sk);
-	}
 }
 
 void
@@ -108,7 +88,7 @@ tfw_connection_unlink_peer(TfwConnection *conn)
  * Publish the "connection is established" event via TfwConnHooks.
  */
 int
-tfw_connection_init(TfwConnection *conn)
+tfw_connection_new(TfwConnection *conn)
 {
 	int r = TFW_CONN_HOOK_CALL(conn, conn_init);
 	if (r)
@@ -134,25 +114,6 @@ tfw_connection_send(TfwConnection *conn, TfwMsg *msg)
 	ss_send(conn->sk, &msg->skb_list);
 }
 
-/**
- * TODO/FIXME
- * Things which happen in the function are wrong. We have to choose backend
- * server when the request is [fully?] parsed to be able to route static and
- * dynamic requests to different server (so we need to know at least base part
- * of URI to choose a server).
- *
- * Probably, following scheme is most suitable:
- * 1. schedulers which are going to route request depending on URI must register
- *    hook TFW_HTTP_HOOK_REQ_STATUS and adjust server information;
- * 2. schedulers which schedule request depending on server stress on
- *    round-robin actully should do their work as early as possible to reduce
- *    message latency and accelerator memory consumption (that parts of
- *    the request can be sent to server immediately) and choose the server
- *    in this function.
- *
- * So at least we need scheduler interface which can register its callbacks in
- * different places.
- */
 int
 tfw_connection_recv(struct sock *sk, unsigned char *data, size_t len)
 {
@@ -192,4 +153,3 @@ tfw_connection_postpone_skb(SsProto *proto, struct sk_buff *skb)
 
 	return 0;
 }
-
