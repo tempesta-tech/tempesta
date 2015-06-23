b1ee296d74ce20eb3519ad407f5a8e2f0fe8adb0
Alexander K <ak@natsys-lab.com>
2015-06-24 01:18:03 +0300

    small cleanups

--- tempesta_fw/cache.c
+++ tempesta_fw/cache.c
@@ -13,7 +13,7 @@
  * 2. Purge cache by individual entities (e.g. curl -X PURGE <URL>)
  *
  * Copyright (C) 2012-2014 NatSys Lab. (info@natsys-lab.com).
- * Copyright (C) 2014 Tempesta Technologies, Inc.
+ * Copyright (C) 2015 Tempesta Technologies, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
