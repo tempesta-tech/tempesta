e2de1d9a765c059146f997e631876efb7ebee91b
Stefan Strogin <ss@natsys-lab.com>
2015-09-04 17:07:03 +0300

    tfw_fuzzer: reworked memory usage
    No reallocations, no kmallocs, use a single 2MB buffer for the request.

--- tempesta_fw/t/unit/tfw_fuzzer.c
+++ tempesta_fw/t/unit/tfw_fuzzer.c
@@ -4,13 +4,6 @@
 
 #include "tfw_fuzzer.h"
 
-#define ADD(_d, _s)			\
-{					\
-	char *_tmp = _s;		\
-	_d = addstr(_d, _tmp);		\
-	kfree(_tmp);			\
-}
-
 static char *methods[] = {"GET", "HEAD", "POST", NULL};
 static char *versions[] = {"0.9", "1.0", "1.1", NULL};
 static char *conn_val[] = {"keep-alive", "close", "upgrade", NULL};
@@ -73,286 +66,222 @@ static int gen_vector_move(int i)
 
 static char *addch(char *s1, char ch)
 {
-	size_t len1 = strlen(s1);
-	char *result;
+	size_t len;
 
 	if (!ch)
 		return s1;
-	result = kmalloc(len1 + 2, GFP_KERNEL);
-	strncpy(result, s1, len1);
-	result[len1] = ch;
-	result[len1 + 1] = '\0';
-	kfree(s1);
+	len = strlen(s1);
+	s1[len] = ch;
+	s1[len + 1] = '\0';
 
-	return result;
+	return s1;
 }
 
-static char *addstr(char *s1, char *s2)
+static char *add_string(char *s1, int i)
 {
-	char *result;
-
-	result = kmalloc(strlen(s1) + strlen(s2) + 1, GFP_KERNEL);
-	strcpy(result, s1);
-	strcat(result, s2);
-	kfree(s1);
-
-	return result;
-}
-
-/* kfree `result` after use */
-static char *string(int i)
-{
-	char *str = kmalloc(1, GFP_KERNEL);
 	char *seed = "aNx0\03\r\0234fddf";
 	int j;
 
-	*str = '\0';
-	for (j = 0; j < i; ++j) {
-		str = addch(str, seed[((j + 333) ^ seed[j]) % sizeof(seed)]);
-	}
+	for (j = 0; j < i; ++j)
+		addch(s1, seed[((j + 333) ^ seed[j]) % sizeof(seed)]);
 
-	return str;
+	return s1;
 }
 
-static char *spaces(void)
+static char *add_spaces(char *s1)
 {
-	char *str = kmalloc(2, GFP_KERNEL);
 	int i;
 
-	*str = '\0';
-
-	for (i = 0; i <= gen_vector[SPACES].i; ++i) {
+	for (i = 0; i < gen_vector[SPACES].i; ++i) {
 		if (i % 2)
-			str = addch(str, '\t');
+			addch(s1, '\t');
 		else if (i % 3)
-			str = addch(str, '\r');
-		else if (i % 4)
-			str = addch(str, '\n');
+			addch(s1, '\r');
+		else if (i % 5)
+			addch(s1, '\n');
 		else
-			str = addch(str, ' ');
+			addch(s1, ' ');
 	}
 
-	return str;
-}
-
-/* kfree `v` after use */
-static char *__addhdr(char *s1, char *v)
-{
-	char *result = kmalloc(strlen(s1) + strlen(v) + 1, GFP_KERNEL);
-
-	strcpy(result, s1);
-	strcat(result, v);
-	kfree(s1);
-
-	return result;
+	return s1;
 }
 
 static char *add_method(char *s1)
 {
-	char *result, *v = methods[gen_vector[METHOD].i];
+	char *v = methods[gen_vector[METHOD].i];
 
-	if (v)
-		result = __addhdr(s1, v);
-	else {
-		v = string(gen_vector[METHOD].i);
-		result = __addhdr(s1, v);
-		kfree(v);
-	}
+	if (v) {
+		strcat(s1, v);
+		return 0;
+	} else
+		add_string(s1, gen_vector[METHOD].i);
 
-	return result;
+	return 0;
 }
 
 static char *add_http_ver(char *s1)
 {
-	char *result, *v = versions[gen_vector[HTTP_VER].i];
+	char *v = versions[gen_vector[HTTP_VER].i];
 
 	if (v)
-		result = __addhdr(s1, v);
-	else {
-		v = string(gen_vector[HTTP_VER].i);
-		result = __addhdr(s1, v);
-		kfree(v);
-	}
+		strcat(s1, v);
+	else
+		add_string(s1, gen_vector[HTTP_VER].i);
 
-	return result;
+	return s1;
 }
 
 static char *add_connection(char *s1)
 {
-	char *result, *v = conn_val[gen_vector[CONNECTION].i];
+	char *v = conn_val[gen_vector[CONNECTION].i];
 
 	if (v)
-		result = __addhdr(s1, v);
-	else {
-		v = string(gen_vector[CONNECTION].i);
-		result = __addhdr(s1, v);
-		kfree(v);
-	}
+		strcat(s1, v);
+	else
+		add_string(s1, gen_vector[CONNECTION].i);
 
-	return result;
+	return s1;
 }
 
 static char *add_user_agent(char *s1)
 {
-	char *result, *v = ua_val[gen_vector[USER_AGENT].i];
+	char *v = ua_val[gen_vector[USER_AGENT].i];
 
 	if (v)
-		result = __addhdr(s1, v);
-	else {
-		v = string(gen_vector[USER_AGENT].i);
-		result = __addhdr(s1, v);
-		kfree(v);
-	}
+		strcat(s1, v);
+	else
+		add_string(s1, gen_vector[USER_AGENT].i);
 
-	return result;
+	return s1;
 }
 
 static char *add_host(char *s1)
 {
-	char *result, *v = hosts[gen_vector[HOST].i];
+	char *v = hosts[gen_vector[HOST].i];
 
 	if (v)
-		result = __addhdr(s1, v);
-	else {
-		v = string(gen_vector[HOST].i);
-		result = __addhdr(s1, v);
-		kfree(v);
-	}
+		strcat(s1, v);
+	else
+		add_string(s1, gen_vector[HOST].i);
 
-	return result;
+	return s1;
 }
 
 static char *add_x_forwarded_for(char *s1)
 {
-	char *result, *v = hosts[gen_vector[X_FORWARDED_FOR].i];
+	char *v = hosts[gen_vector[X_FORWARDED_FOR].i];
 
 	if (v)
-		result = __addhdr(s1, v);
-	else {
-		v = string(gen_vector[X_FORWARDED_FOR].i);
-		result = __addhdr(s1, v);
-		kfree(v);
-	}
+		strcat(s1, v);
+	else
+		add_string(s1, gen_vector[X_FORWARDED_FOR].i);
 
-	return result;
+	return s1;
 }
 
 static char *add_content_type(char *s1)
 {
-	char *result, *v = content_type[gen_vector[CONTENT_TYPE].i];
+	char *v = content_type[gen_vector[CONTENT_TYPE].i];
 
 	if (v)
-		result = __addhdr(s1, v);
-	else {
-		v = string(gen_vector[CONTENT_TYPE].i);
-		result = __addhdr(s1, v);
-		kfree(v);
-	}
+		strcat(s1, v);
+	else
+		add_string(s1, gen_vector[CONTENT_TYPE].i);
 
-	return result;
+	return s1;
 }
 
 static char *add_content_length(char *s1)
 {
-	char *result, *v = content_length[gen_vector[CONTENT_LENGTH].i];
+	char *v = content_length[gen_vector[CONTENT_LENGTH].i];
 
 	if (v)
-		result = __addhdr(s1, v);
-	else {
-		v = string(gen_vector[CONTENT_LENGTH].i);
-		result = __addhdr(s1, v);
-		kfree(v);
-	}
+		strcat(s1, v);
+	else
+		add_string(s1, gen_vector[CONTENT_LENGTH].i);
 
-	return result;
+	return s1;
 }
 
-static char *add_transfer_encoding(char *s1, int i)
+static char *add_transfer_encoding(char *s1, int n)
 {
-	char *result, *v = transfer_encoding[gen_vector[i].i];
+	char *v = transfer_encoding[n];
 
 	if (v)
-		result = __addhdr(s1, v);
-	else {
-		v = string(gen_vector[i].i);
-		result = __addhdr(s1, v);
-		kfree(v);
-	}
+		strcat(s1, v);
+	else
+		add_string(s1, n);
 
-	return result;
+	return s1;
 }
 
-char *fuzz_req(void)
+/* Returns 1 if str is a valid request, -1 if it's invalid.
+ * 0 if the request sequence is over. */
+int fuzz_req(char *str)
 {
-	char *str, *tmp;
 	int i;
 
-	str = kmalloc(1, GFP_KERNEL);
+	BUG_ON(!str);
 	*str = '\0';
-	str = add_method(str);
-
-	ADD(str, spaces());
+	add_method(str);
+	add_spaces(str);
 
 	str = addch(str, gen_vector[URI_PATH_START].i ? '/' : 0);
 
 	for (i = 0; i < gen_vector[URI_PATH_DEPTH].i; ++i) {
 		str = addch(str, '/');
-		ADD(str, spaces());
-		tmp = string(gen_vector[URI_FILE].i);
-		str = addstr(str, tmp);
-		kfree(tmp);
+		add_spaces(str);
+		add_string(str, gen_vector[URI_FILE].i);
 	}
 
-	ADD(str, spaces());
-	str = addstr(str, "HTTP/");
+	add_spaces(str);
+	str = strcat(str, "HTTP/");
 	str = add_http_ver(str);
-	str = addstr(str, "\r\n");
+	str = strcat(str, "\r\n");
 
-	str = addstr(str, "Connection:");
-	ADD(str, spaces());
+	str = strcat(str, "Connection:");
+	add_spaces(str);
 	str = add_connection(str);
-	str = addstr(str, "\r\n");
+	str = strcat(str, "\r\n");
 
-	str = addstr(str, "User-agent:");
-	ADD(str, spaces());
+	str = strcat(str, "User-agent:");
+	add_spaces(str);
 	str = add_user_agent(str);
-	str = addstr(str, "\r\n");
+	str = strcat(str, "\r\n");
 
-	str = addstr(str, "Host:");
-	ADD(str, spaces());
+	str = strcat(str, "Host:");
+	add_spaces(str);
 	str = add_host(str);
-	str = addstr(str, "\r\n");
+	str = strcat(str, "\r\n");
 
-	str = addstr(str, "X-Forwarded-For:");
-	ADD(str, spaces());
+	str = strcat(str, "X-Forwarded-For:");
+	add_spaces(str);
 	str = add_x_forwarded_for(str);
-	str = addstr(str, "\r\n");
+	str = strcat(str, "\r\n");
 
-	str = addstr(str, "Content-Type:");
-	ADD(str, spaces());
+	str = strcat(str, "Content-Type:");
+	add_spaces(str);
 	str = add_content_type(str);
-	str = addstr(str, "\r\n");
+	str = strcat(str, "\r\n");
 
-	str = addstr(str, "Content-Length:");
-	ADD(str, spaces());
+	str = strcat(str, "Content-Length:");
+	add_spaces(str);
 	str = add_content_length(str);
-	str = addstr(str, "\r\n");
+	str = strcat(str, "\r\n");
 
-	str = addstr(str, "Transfer-Encoding:");
-	ADD(str, spaces());
-	str = add_transfer_encoding(str, TRANSFER_ENCODING);
-	for (i = 0; gen_vector[TRANSFER_ENCODING_NUM].i; ++i) {
+	str = strcat(str, "Transfer-Encoding:");
+	add_spaces(str);
+	str = add_transfer_encoding(str, gen_vector[TRANSFER_ENCODING].i);
+	for (i = 0; i < gen_vector[TRANSFER_ENCODING_NUM].i; ++i) {
 		str = addch(str, ',');
-		ADD(str, spaces());
+		add_spaces(str);
 		add_transfer_encoding(str,
-				gen_vector[CONTENT_LENGTH].i %
+				(i + gen_vector[CONTENT_LENGTH].i) %
 				gen_vector[TRANSFER_ENCODING].max);
 	}
 
-	str = addstr(str, "\r\n\r\n");
+	strcat(str, "\r\n\r\n");
 
-	if (!gen_vector_move(N_FIELDS))
-		return NULL;
-	return str;
+	return gen_vector_move(N_FIELDS - 1);
 }
 EXPORT_SYMBOL(fuzz_req);
