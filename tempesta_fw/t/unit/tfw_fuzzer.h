dfc03d7489276fddadd294c287205b3997d81524
Stefan Strogin <ss@natsys-lab.com>
2015-09-08 18:43:42 +0300

    test_http_parser, tfw_fuzzer: add support for response messages

--- tempesta_fw/t/unit/tfw_fuzzer.h
+++ tempesta_fw/t/unit/tfw_fuzzer.h
@@ -1,12 +1,17 @@
 #ifndef __TFW_FUZZER_H__
 #define __TFW_FUZZER_H__
 
-int fuzz_gen(char *str, int move);
+int fuzz_gen(char *str, int move, int type);
 
 enum {
 	FUZZ_VALID,
 	FUZZ_INVALID,
 	FUZZ_END
+};
+
+enum {
+	FUZZ_REQ,
+	FUZZ_RESP
 };
 
 #endif /* __TFW_FUZZER_H__ */
