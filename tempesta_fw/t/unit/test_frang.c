6569f0a3c6892e48643db3a9b71959753826c80c
Sergey Lvov <sl@natsys-lab.com>
2015-11-11 16:59:22 +0300

    includem10

--- tempesta_fw/t/unit/test_frang.c
+++ tempesta_fw/t/unit/test_frang.c
@@ -170,7 +170,6 @@ TEST(frang, max_conn)
 	frang_cfg.conn_max = 0;
 	frang_cfg.conn_rate = 0;
 	frang_cfg.conn_burst = 5;
-//((FrangAcc*)mocksock.sk.sk_security)->history[i].conn_new = 5;
 
 	res = req_handler(mockreq);
 	/*conn_burst*/
