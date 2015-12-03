7c3bf4f015f997cb2510829ac2343f3956d5b3d0
Sergey Lvov <sl@natsys-lab.com>
2015-12-03 17:23:54 +0300

    sched http4

--- tempesta_fw/t/unit/test.c
+++ tempesta_fw/t/unit/test.c
@@ -92,7 +92,6 @@ TEST_SUITE(cfg);
 TEST_SUITE(sched_rr);
 TEST_SUITE(sched_hash);
 TEST_SUITE(sched_http);
-TEST_SUITE(frang);
 
 int
 test_run_all(void)
@@ -109,7 +108,6 @@ test_run_all(void)
 	TEST_SUITE_RUN(sched_rr);
 	TEST_SUITE_RUN(sched_hash);
 	TEST_SUITE_RUN(sched_http);
-	TEST_SUITE_RUN(frang);
 
 	return test_fail_counter;
 }
