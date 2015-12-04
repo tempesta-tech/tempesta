4edb03ccfdbdf9b789e6869194041b4d52f6458d
Sergey Lvov <sl@natsys-lab.com>
2015-12-04 12:07:04 +0300

    edit01

--- tempesta_fw/t/unit/sched_helper.c
+++ tempesta_fw/t/unit/sched_helper.c
@@ -18,7 +18,6 @@
  * this program; if not, write to the Free Software Foundation, Inc., 59
  * Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-//#include "sock_srv.c" 
 
 #include "connection.h"
 #include "sched_helper.h"
