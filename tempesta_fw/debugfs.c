9ca3a38156544518ec9c9222efe626d60ecc295a
Alexander K <ak@tempesta-tech.com>
2015-12-09 22:53:38 +0300

    Remove support of linux 3.10.10 kernel

--- tempesta_fw/debugfs.c
+++ tempesta_fw/debugfs.c
@@ -30,11 +30,9 @@
 
 #ifdef DEBUG
 
-#if LINUX_VERSION_CODE > KERNEL_VERSION(3,10,10)
 extern int
 vfs_path_lookup(struct dentry *dentry, struct vfsmount *mnt, const char *name,
 		unsigned int flags, struct path *path);
-#endif
 
 /* The root directory for the Tempesta debugfs module.
  * All paths are referenced relative to this root. */
