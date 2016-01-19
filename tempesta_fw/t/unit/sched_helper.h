9daea2839cd931ecc431924037b0267166150b2b
Sergey Lvov <sl@natsys-lab.com>
2016-01-19 16:18:46 +0300

    Fix #344

--- tempesta_fw/t/unit/sched_helper.h
+++ tempesta_fw/t/unit/sched_helper.h
@@ -22,23 +22,22 @@
 #ifndef __TFW_SCHED_HELPER_H__
 #define __TFW_SCHED_HELPER_H__
 
-#include "server.h"
+#include "addr.h"
+#include "cfg.h"
+#include "connection.h"
 
-typedef struct {
-	TfwConnection		conn;
-	struct timer_list	retry_timer;
-	unsigned long		timeout;
-	unsigned int		attempts;
-} TfwSrvConnection;
-
+int tfw_server_init(void);
+int tfw_sched_rr_init(void);
 void sched_helper_init(void);
 
+void test_spec_cleanup(TfwCfgSpec specs[]);
 TfwSrvGroup *test_create_sg(const char *name, const char *sched_name);
 void test_sg_release_all(void);
 
 TfwServer *test_create_srv(const char *in_addr, TfwSrvGroup *sg);
 
 TfwSrvConnection *test_create_conn(TfwPeer *peer);
+
 void test_conn_release_all(TfwSrvGroup *sg);
 
 #endif /* __TFW_SCHED_HELPER_H__ */
