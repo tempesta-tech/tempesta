a0860498d2a9b641bfa41d4b7f10a1e1143e7fee
Sergey Lvov <sl@natsys-lab.com>
2016-02-29 17:59:51 +0300

    fix #396 review fix

--- tempesta_fw/t/functional/fragmented_requests.py
+++ tempesta_fw/t/functional/fragmented_requests.py
@@ -11,7 +11,7 @@ GET http://github.com/natsys/tempesta HTTP/1.1\r\n
 Host: github.com\r\n
 User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 \
 Firefox/31.0 Iceweasel/31.2.0\r\n
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r \
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n
 Accept-Language: en-US,en;q=0.5\r\n
 Accept-Encoding: gzip, deflate\r\n
 DNT: 1\r\n
