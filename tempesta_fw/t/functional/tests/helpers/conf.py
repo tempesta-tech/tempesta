d33c2fba8cb97e1a22570965564465bb241dbb45
Sergey Lvov <sl@natsys-lab.com>
2016-03-31 10:08:48 +0300

    fix #396

---
+++ tempesta_fw/t/functional/tests/helpers/conf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+__author__ = 'Tempesta Technologies Inc.'
+__copyright__ = 'Copyright (C) 2016 Tempesta Technologies Inc. (info@natsys-lab.com).'
+__license__ = 'GPL2'
+
+import os
+import fileinput
+class Config:
+	name = '/etc/tempesta_fw.conf'
+	tmpname = 'etc/temp.conf'
+
+	def __init__(self, name):
+		self.name = name
+		open(self.name, "w")
+		return
+
+	def add_option(self, option, value):
+		with open(self.name, "a+") as conf:
+			conf.write(option + ' ' + value + ';\n')
+
+	def del_option(self, option):
+		temp = open(self.tmpname, 'a+')
+		for line in fileinput.input(self.name):
+			if not option in line:
+				temp.write(line)
+
+		os.rename(self.tmpname, self.name)
