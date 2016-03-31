00fe90d26fbf13af1e643aeb0ebcaec3abb758b4
Sergey Lvov <sl@natsys-lab.com>
2016-03-31 10:08:48 +0300

    fix #396

--- tempesta_fw/t/functional/tests/helpers/tfw.py
+++ tempesta_fw/t/functional/tests/helpers/tfw.py
@@ -1,40 +1,44 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
-Helpers for interacting with Tempesta FW (start/stop, configure, etc). 
+Helpers for interacting with Tempesta FW (start/stop, configure, etc).
 """
 
 import os
 import subprocess
 import sys
 
-from . import teardown
+import teardown
 
 __author__ = 'NatSys Lab'
 __copyright__ = 'Copyright (C) 2014 NatSys Lab. (info@natsys-lab.com).'
 __license__ = 'GPL2'
 
 _functest_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
-_tempesta_dir = os.path.normpath(os.path.join(_functest_dir, '../../../'))
-
+_tempesta_dir = os.path.realpath('.')
 def start():
-    _sh("SYNC_SOCKET=./sync_socket TDB=./tempesta_db ./tempesta.sh start")
+	_sh(_tempesta_dir + '/scripts/tempesta.sh --start')
 
 def stop():
-    _sh("./tempesta.sh stop")
+	_sh("./scripts/tempesta.sh --stop")
 
 def _sh(command):
-    return subprocess.check_output(command, shell=True, cwd=_tempesta_dir)
+	return subprocess.check_output(command, shell=True, cwd=_tempesta_dir)
 
 def _is_started():
-    return (0 == subprocess.call("lsmod | grep -q tempesta", shell=True))
+	return (0 == subprocess.call("lsmod | grep -q tempesta", shell=True))
 
 def _stop_if_started():
-    if (_is_started()):
-        stop()
-
+	if (_is_started()):
+		stop()
 # Ensure we start and stop in a pristine environment.
-assert (not _is_started())
+	assert (not _is_started())
+
+def start_bomber():
+	_sh("./scripts/tfw_bomber.sh --start")
+
+def stop_bomber():
+	_sh("./scripts/tfw_bomber.sh --stop")
 # The teardown line is commented-out because we have the issue:
 #   #10 -Oops on shutdown
 # At this point it is not solved and Tempesta FW simply can't be stopped.
