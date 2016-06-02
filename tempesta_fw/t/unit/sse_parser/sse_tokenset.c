38753ad425888439a4eb887696da1080ad4170c3
Dmitry Leontiev <dm.leontiev7@gmail.com>
2016-06-02 17:45:54 +0400

    more tests in sse_tokenset

--- tempesta_fw/t/unit/sse_parser/sse_tokenset.c
+++ tempesta_fw/t/unit/sse_parser/sse_tokenset.c
@@ -5,39 +5,7 @@
 #include <stdlib.h>
 #include "sse_parser.h"
 
-static int my_malloc(unsigned long len, void** out, void * unused) {
-    void * ptr = malloc(len);
-    if (!ptr) return ENOMEM;
-    *out = ptr;
-    return 0;
-}
-
-
-int main()
-{
-    sse_init_constants();
-
-    TokenSet * ts = NULL;
-    int        ts_len;
-
-    const char * tokens[] = {
-        "GET", "PUT", "POST", "COPY", "MOVE", "LOCK", "HEAD", "PATCH",
-        "TRACK", "DELETE", "UNLOCK", "MKCOL", "OPTIONS", "PROPFIND", "PROPPATCH",
-        NULL
-    };
-
-    ts_len = tokenSetLength(tokens);
-    void * ts_buffer = NULL;
-    if (posix_memalign(&ts_buffer, 16, ts_len+640)) {
-        printf("malloc error\n");
-        return 1;
-    }
-    ts = initTokenSet(tokens, ts_buffer, ts_len);
-    if (!ts) {
-        printf("token set lenght mismatch(%p)\n", ts);
-        return 1;
-    }
-
+int dumpTokenSet(TokenSet * ts, const char ** tokens) {
     //dump tokenset structure:
     printf("Token set lenght: %d\n", ts->iterations);
     printf("|  0 |  1 |  2 |  3 |  4 |  5 |  6 |  7 "
@@ -69,8 +37,63 @@ int main()
             printf("SUCCESS\n");
         }
     }
+    return fail;
+}
+
+int main()
+{
+    sse_init_constants();
+
+    TokenSet * ts = NULL;
+    int        ts_len;
+    int        fail = 0;
+
+    const char * tokens[] = {
+        "GET", "PUT", "POST", "COPY", "MOVE", "LOCK", "HEAD", "PATCH",
+        "TRACK", "DELETE", "UNLOCK", "MKCOL", "OPTIONS", "PROPFIND", "PROPPATCH",
+        NULL
+    };
+    ts_len = tokenSetLength(tokens);
+    void * ts_buffer = NULL;
+    if (posix_memalign(&ts_buffer, 16, ts_len)) {
+        printf("malloc error\n");
+        return 1;
+    }
+    ts = initTokenSet(tokens, ts_buffer, ts_len);
+    if (!ts) {
+        printf("token set lenght mismatch(%p)\n", ts);
+        return 1;
+    }
+    fail += dumpTokenSet(ts, tokens);
+    free(ts);
+
+
+    static const char * tokens_sch[] = {
+        "https://", "http://",
+        NULL
+    };
+    ts_len = tokenSetLength(tokens_sch);
+    if (posix_memalign(&ts_buffer, 16, ts_len)) {
+        printf("malloc error\n");
+        return 1;
+    }
+    ts = initTokenSet(tokens_sch, ts_buffer, ts_len);
+    if (!ts) {
+        printf("token set lenght mismatch(%p)\n", ts);
+        return 1;
+    }
+    fail += dumpTokenSet(ts, tokens_sch);
+
+    printf("**************************************************\n");
+    int r = matchTokenSet(ts, strToVec("http://yandex.ru"));
+    printf("parse str: %04x\n", r);
+    if (!r) {
+        printf("failed to parse host\n");
+        ++fail;
+    }
 
     free(ts);
+
     if (fail) return 1;
     return 0;
 }
