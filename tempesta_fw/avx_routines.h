01b353d555529b3b381856e87a2e67b7c23804ea
Dmitry Leontiev <dm.leontiev7@gmail.com>
2016-06-22 20:17:07 +0400

    avx_routines: fixed header detection

--- tempesta_fw/avx_routines.h
+++ tempesta_fw/avx_routines.h
@@ -15,7 +15,7 @@ extern const unsigned char __c_avx_header_names[];
 
 #define __header_charset _mm256_loadu2_m128i((const __m128i*)__c_avx_data, (const __m128i*)__c_avx_data)
 #define __constants _mm256_loadu2_m128i((const __m128i*)__c_avx_data+16, (const __m128i*)__c_avx_data+16)
-#define _mm256_loadu(p) _mm256_loadu_si256((const __m256*)p)
+#define _mm256_loadu(p) _mm256_lddqu_si256((const __m256*)p)
 #define _mm256_load(p)  _mm256_load_si256((const __m256*)p)
 
 #define __DEFINE_AVX_VARIABLES \
@@ -69,43 +69,62 @@ enum {
 //==================================================
 //DATA#  0low 0hi  1low 1hi  2low 2hi  3low 3hi
 //DWORD#
-// 0     Host Tran X-Fo Cont Cont User Conn Cook
-// 1     Cach sfer rwar ent- ent- -age ecti ie
-// 2     e-Co -Enc ded- Leng Type nt   on
-// 3     ntro odin For  th
-// 4     l    g
+// 0     Host Tran X-Fo Cont Cach Cook Conn User
+// 1          sfer rwar ent- e-Co ie   ecti -age
+// 2     Type -Enc ded- Leng ntro g    on   nt
+// 3          odin For  th   l
 //==================================================
 
 #define AVX_MATCH_HEADER_NAME(result, text) do {\
     /*mask 1 = rows 0-1, columns 0low-1hi*/\
     __m256i mask1 = _mm256_cmpeq_epi64(\
-        _mm256_and_si256(_mm256_permute4x64_epi64(text, 0x00), _mm256_load(__c_avx_header_names+0)),\
+        _mm256_and_si256(_mm256_permute4x64_epi64(text, 0x00),\
+                         _mm256_load(__c_avx_header_names)),\
         _mm256_load((__c_avx_header_names+32)));\
     /*mask 2 = rows 0-1, columns 2low-3hi*/\
     __m256i mask2 = _mm256_cmpeq_epi64(\
-        _mm256_and_si256(_mm256_permute4x64_epi64(text, 0x00), _mm256_load(__c_avx_header_names+64)),\
+        _mm256_and_si256(_mm256_permute4x64_epi64(text, 0x00),\
+                         _mm256_load(__c_avx_header_names+64)),\
         _mm256_load((__c_avx_header_names+96)));\
     /*mask 3 = rows 2-3, columns 0low-1hi*/\
     __m256i mask3 = _mm256_cmpeq_epi64(\
-        _mm256_and_si256(_mm256_permute4x64_epi64(text, 0x55), _mm256_load(__c_avx_header_names+128)),\
+        _mm256_and_si256(_mm256_permute4x64_epi64(text, 0x55),\
+                         _mm256_load(__c_avx_header_names+128)),\
         _mm256_load((__c_avx_header_names+160)));\
     /*mask 4 = rows 2-3, columns 2low-3hi*/\
     __m256i mask4 = _mm256_cmpeq_epi64(\
-        _mm256_and_si256(_mm256_permute4x64_epi64(text, 0x55), _mm256_load(__c_avx_header_names+192)),\
+        _mm256_and_si256(_mm256_permute4x64_epi64(text, 0x95),\
+                         _mm256_load(__c_avx_header_names+192)),\
         _mm256_load((__c_avx_header_names+224)));\
-    /*mask5 = row 4*/\
-    __m256i mask5 = _mm256_cmpeq_epi64(\
-        _mm256_and_si256(_mm256_permute4x64_epi64(text, 0xAA), _mm256_load(__c_avx_header_names+256)),\
-        _mm256_load((__c_avx_header_names+288)));\
-    mask1 = _mm256_and_si256(_mm256_and_si256(mask1, mask3), mask5);\
-    mask2 = _mm256_and_si256(mask2, mask4);\
-    int mask = _mm256_movemask_epi8(_mm256_packus_epi32(mask1, mask2));\
-    mask &= 0x87654321;\
-    mask = mask | (mask>>16);\
-    mask = mask | (mask>>8);\
-    mask = mask | (mask>>4);\
-    result = 0xFF & mask;} while(0);
+    /*extract mask for host*/\
+    int bm = 0x1 & _mm256_movemask_epi8(mask1);\
+    /*combine mask1 and mask3*/\
+    mask1 = _mm256_and_si256(_mm256_permute4x64_epi64(mask1, 0xe7),\
+                             mask3);\
+    mask1 = _mm256_and_si256(mask1, _mm256_blend_epi32(mask4, 0x0c);\
+    /*combine mask2 and mask4*/\
+    mask2 = _mm256_and_si256(mask2, _mm256_blend_epi32(mask4, 0xf3);\
+    bm |= 0x05040302 & _mm256_movemask_epi8(mask1);\
+    bm |= 0x09080706 & _mm256_movemask_epi8(mask2);\
+    bm = bm | (bm>>16);\
+    bm = bm | (bm>>8);\
+    result = 0xFF & bm;} while(0);
 
+#define AVX_STRID_1(n) do {\
+    static const int ids[9] = {\
+        RGen_HdrOther,
+        Req_HdrCache_ControlV,
+        Req_HdrTransfer_EncodingV,
+        Req_HdrTransfer_EncodingV,
+        Req_HdrX_Forwarded_ForV,
+        Req_HdrContent_LengthV,
+        Req_HdrContent_TypeV,
+        Req_HdrUser_AgentV,
+        Req_HdrConnectionV,
+        Req_HdrCookieV,
+    };\
+
+}
 
 #endif // AVX_ROUTINES_H
 
