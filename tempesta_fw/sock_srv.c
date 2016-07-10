7f2922778eff79adfd593e77f2e5000bc3b13d6d
Alexander K <ak@natsys-lab.com>
2016-07-10 22:07:32 +0300

    Remove unnecessary and dangerous socket unlink: socket is freed already and nobody can use it, so the freeing is useless and memory corruption prone

--- tempesta_fw/sock_srv.c
+++ tempesta_fw/sock_srv.c
@@ -169,7 +169,6 @@ tfw_sock_srv_connect_try(TfwSrvConnection *srv_conn)
 	if (r) {
 		TFW_ERR("Unable to initiate a connect to server: %d\n", r);
 		ss_close_sync(sk);
-		tfw_connection_unlink_from_sk(sk);
 		return r;
 	}
 
