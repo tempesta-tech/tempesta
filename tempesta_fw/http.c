c998f48fbb1d474ad5afd5b9cec09ccde653ec36
Sergey Lvov <sl@natsys-lab.com>
2016-08-03 17:58:00 +0300

    fix #1

--- tempesta_fw/http.c
+++ tempesta_fw/http.c
@@ -752,9 +752,9 @@ tfw_http_adjust_resp(TfwHttpResp *resp, TfwHttpReq *req)
 
 	r = tfw_http_add_hdr_via(hm);
 	if (resp->flags & TFW_HTTP_RESP_STALE) {
-#define S_WARN_110 "Warning: 110 - Response is stale\r\n"
+#define S_WARN_110 "Warning: 110 - Response is stale"
 		/* TODO: ajust for #215 */
-		TfwStr wh = {.ptr = S_WARN_110, .len = SLEN(S_WARN_110)};
+		TfwStr wh = {.ptr = S_WARN_110, .len = SLEN(S_WARN_110),.eolen = 2};
 		r = tfw_http_msg_hdr_add(hm, &wh);
 		if (r)
 			return r;
