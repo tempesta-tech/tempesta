8a8ba1bd1fdee6be65b8cbbff52d9e5e25666542
Sergey Lvov <sl@natsys-lab.com>
2016-08-23 12:30:26 +0300

    review

--- tempesta_fw/http_msg.c
+++ tempesta_fw/http_msg.c
@@ -96,7 +96,7 @@ __http_msg_hdr_val(TfwStr *hdr, unsigned id, TfwStr *val, bool client)
 			val->len -= c->len;
 		}
 		else {
-			val->chunks = (struct TfwStr *)c;
+			val->chunks = c;
 			return;
 		}
 		BUG_ON(TFW_STR_CHUNKN(val) < 1);
@@ -119,7 +119,8 @@ __hdr_is_singular(const TfwStr *hdr)
 {
 	int i, fc;
 	static const TfwStr hdr_singular[] __read_mostly = {
-#define TfwStr_string(v) { .data = (v), .skb = NULL, .len = sizeof(v) - 1, 0 }
+#define TfwStr_string(v) { .data = (v), .skb = NULL, .len = sizeof(v) - 1,\
+			   .flags = 0 }
 		TfwStr_string("authorization:"),
 		TfwStr_string("from:"),
 		TfwStr_string("if-modified-since:"),
