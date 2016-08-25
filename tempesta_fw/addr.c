1d98c7f95e640463be1c94dd37864925c30035c2
Sergey Lvov <sl@natsys-lab.com>
2016-08-25 11:32:03 +0300

    review

--- tempesta_fw/addr.c
+++ tempesta_fw/addr.c
@@ -522,13 +522,13 @@ tfw_put_ipv6_digit_group(u16 group, char *out_buf)
 
 	switch (digits_n) {
 	case 4:
-		out_buf[-4] = hex_asc[(group >> 12)];
+	out_buf[-4] = hex_asc[(group >> 12)];
 	case 3:
-		out_buf[-3] = hex_asc[(group >> 8)  & 0xF];
+	out_buf[-3] = hex_asc[(group >> 8)  & 0xF];
 	case 2:
-		out_buf[-2] = hex_asc[(group >> 4)  & 0xF];
+	out_buf[-2] = hex_asc[(group >> 4)  & 0xF];
 	case 1:
-		out_buf[-1] = hex_asc[group        & 0xF];
+	out_buf[-1] = hex_asc[group        & 0xF];
 	}
 
 	return out_buf;
