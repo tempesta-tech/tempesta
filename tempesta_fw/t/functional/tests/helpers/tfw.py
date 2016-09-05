acbf73fe72745b19e2bd2a86f26d51ec01a43eed
Sergey Lvov <sl@natsys-lab.com>
2016-09-05 12:26:33 +0300

    copyright

--- tempesta_fw/t/functional/tests/helpers/tfw.py
+++ tempesta_fw/t/functional/tests/helpers/tfw.py
@@ -11,7 +11,8 @@ import sys
 import teardown
 
 __author__ = 'NatSys Lab'
-__copyright__ = 'Copyright (C) 2014 NatSys Lab. (info@natsys-lab.com).'
+__copyright__ = 'Copyright (C) 2014-2016 Tempesta Technologies Inc.
+		 2014 NatSys Lab. (info@natsys-lab.com).'
 __license__ = 'GPL2'
 
 _functest_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
