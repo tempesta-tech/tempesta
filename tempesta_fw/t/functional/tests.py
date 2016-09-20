1a234a8f6c43a4c4a575958e78d3cca87c688a1a
Sergey Lvov <sl@natsys-lab.com>
2016-09-20 16:53:27 +0300

    lines

--- tempesta_fw/t/functional/tests.py
+++ tempesta_fw/t/functional/tests.py
@@ -4,14 +4,12 @@ __author__ = 'Tempesta Technologies Inc.'
 __copyright__ = 'Copyright (C) 2016 Tempesta Technologies Inc. (info@natsys-lab.com).'
 __license__ = 'GPL2'
 
-
 import pkgutil
 import tests
 import subprocess
 import sys
 from os.path import dirname, realpath, sep
 
-#sys.path.append('./tests/helpers')
 sys.path.append((dirname(realpath(__file__))+ sep + "tests" + sep + "helpers"))
 
 for loader, name, ispkg in pkgutil.iter_modules(path = tests.__path__, prefix = ''):
