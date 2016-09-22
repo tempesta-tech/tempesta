1cb29e9060a50270cd3b91a55bccbcbbf7df42f0
Sergey Lvov <sl@natsys-lab.com>
2016-09-22 16:40:52 +0300

    copyright move

--- tempesta_fw/t/functional/tests/helpers/apache.py
+++ tempesta_fw/t/functional/tests/helpers/apache.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-import subprocess
+
 __author__ = 'Tempesta Technologies Inc.'
 __copyright__ = 'Copyright (C) 2016 Tempesta Technologies. (info@natsys-lab.com).'
 __license__ = 'GPL2'
+
+import subprocess
 
 def start():
 	subprocess.call("service apache2 start", shell = True)
