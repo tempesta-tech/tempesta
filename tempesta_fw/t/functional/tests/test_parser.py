a1886cd7298cee08909b7410cfd411c71208b39f
Sergey Lvov <sl@natsys-lab.com>
2016-10-13 12:08:28 +0300

    content

--- tempesta_fw/t/functional/tests/test_parser.py
+++ tempesta_fw/t/functional/tests/test_parser.py
@@ -1,6 +1,37 @@
-#!/usr/src/env python
+#!/usr/bin/env python
 __author__ = 'Tempesta Technologies'
 __copyright__ = 'Copyright (C) 2016 Tempesta Technologies. (info@natsys-lab.com).'
 __license__ = 'GPL2'
+import sys
+from os.path import dirname, realpath, sep
 
+sys.path.append((dirname(realpath(__file__))+ sep + "helpers"))
 import conf
+import tfw
+import socket
+
+def run():
+	vs_get = b"POST /index.html HTTP/1.0\r\nHost: loc\r\n" +\
+	b"\r\nTransfer-Encoding: chunked\r\nConnection: Keep-Alive\r\n" +\
+b"\r\nContent-type: html\r\nContent-Length: 0" +\
+b"\r\n<html>content</html>\r\n\r\n"
+	c = conf.Config('etc/tempesta_fw.conf')
+	c.add_option('cache', '0')
+	c.add_option('listen', '8081')
+	c.add_option('server', '127.0.0.1:80')
+	tfw.start()
+	print("tfw start\n")
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(("127.0.0.1",8081))
+	s.sendall(vs_get)
+	data = s.recv(1024)
+	print("recv:", data)
+	s.close()
+	tfw.stop()
+	
+	
+run()   
+	
+	
+
+
