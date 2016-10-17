aef0eb5dbda1a3b2dffb4929bc6550b8a6754b1e
Sergey Lvov <sl@natsys-lab.com>
2016-10-17 10:52:02 +0300

    #319 - ok

--- tempesta_fw/t/functional/tests/test_parser.py
+++ tempesta_fw/t/functional/tests/test_parser.py
@@ -3,35 +3,31 @@ __author__ = 'Tempesta Technologies'
 __copyright__ = 'Copyright (C) 2016 Tempesta Technologies. (info@natsys-lab.com).'
 __license__ = 'GPL2'
 import sys
-from os.path import dirname, realpath, sep
 
-sys.path.append((dirname(realpath(__file__))+ sep + "helpers"))
 import conf
 import tfw
 import socket
+class Test:
+	def __init__(self):
+		self.cfg = conf.Config('etc/tempesta_fw.conf')
+		self.cfg.add_option('cache', '0')
+		self.cfg.add_option('listen', '8081')
+		self.cfg.add_option('server', '127.0.0.1:80')	
+	def run(self):
+		vs_get = b"POST /index.html HTTP/1.0\r\nHost: loc\r\n" +\
+		b"Transfer-Encoding: chunked\r\nConnection: Keep-Alive" +\
+		b"\r\n\r\n"+\
+		b"16\r\n<html>content</html>\r\n\r\n0\r\n\r\n"
 
-def run():
-	vs_get = b"POST /index.html HTTP/1.0\r\nHost: loc\r\n" +\
-	b"\r\nTransfer-Encoding: chunked\r\nConnection: Keep-Alive\r\n" +\
-b"\r\nContent-type: html\r\nContent-Length: 0" +\
-b"\r\n<html>content</html>\r\n\r\n"
-	c = conf.Config('etc/tempesta_fw.conf')
-	c.add_option('cache', '0')
-	c.add_option('listen', '8081')
-	c.add_option('server', '127.0.0.1:80')
-	tfw.start()
-	print("tfw start\n")
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(("127.0.0.1",8081))
-	s.sendall(vs_get)
-	data = s.recv(1024)
-	print("recv:", data)
-	s.close()
-	tfw.stop()
-	
-	
-run()   
-	
-	
+		tfw.start()
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect(("127.0.0.1",8081))
+		s.sendall(vs_get)
+		data = s.recv(1024)
+		if len(data) > 0:
+			print("res:{}".format(True))
+		s.close()
+		tfw.stop()
 
-
+	def get_name(self):
+		return 'test_chunked'	
