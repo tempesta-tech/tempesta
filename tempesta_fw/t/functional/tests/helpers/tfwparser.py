5f6732df3d1566407df77d79d8c8b310e1fda287
Sergey Lvov <sl@natsys-lab.com>
2016-10-18 16:35:33 +0300

    html parser

---
+++ tempesta_fw/t/functional/tests/helpers/tfwparser.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+__author__ = 'Tempesta Technologies Inc.'
+__copyright__ = 'Copyright (C) 2016 Tempesta Technologies Inc. (info@natsys-lab.com).'
+__license__ = 'GPL2'
+
+from HTMLParser import HTMLParser
+import re
+
+class TFWParser(HTMLParser):
+	status = ""
+	def set_status(self, data):
+		status = re.findall("\d{3}\s\w+\s?\w+", data)
+		if len(status) > 0:
+			self.status = status[0]
+
+	def get_status(self):
+		return self.status	
+
+	def handle_data(self, data):
+		if data[0] == 'H':
+			self.set_status(data)
+
