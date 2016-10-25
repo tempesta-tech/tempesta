f5fcbcf8f1842fa6850d1cab9d534c8653aa51eb
Sergey Lvov <sl@natsys-lab.com>
2016-10-25 18:09:45 +0300

    ret 5

--- tempesta_fw/t/functional/tests/helpers/be.py
+++ tempesta_fw/t/functional/tests/helpers/be.py
@@ -8,17 +8,44 @@ A primitive back-end HTTP server implementation suitable for testing purposes.
 """
 import http.server 
 import threading
+import os
+import sys
+import signal
 
+process_id = 0
+server = None
+def start():
+	httpd = BackendHTTPServer(host='127.0.0.1', port=8080,
+				  handler=BackendHTTPRequestHandler)
+	server = httpd
+	pid = os.fork()
+	if pid > 0:
+		process_id = os.getpid()
+		print('is parent')
+	else:
+		process_id = os.getpid()	
+		print("fork - pid:{}".format(process_id))
+		server.run()
+
+def stop():
+	if server != None:
+		server.server_close()
+		server.keep = False
+	os.kill(process_id, signal.SIGTERM)
+	sys.exit(0)
+	
 class BackendHTTPServer(http.server.HTTPServer):
 # A simple backend http server.	
-	def __init__(self, address, port=8080):
+	def __init__(self, host, port, handler):
 	# Initialize HTTP server, bind/listen/etc.
 		self.keep = True
-		super(BackendHTTPServer, self).__init__((address, port), BackendHTTPRequestHandler)
+		super(BackendHTTPServer, self).__init__((host, port), handler)
 
 	def run(self):
-		thread = threading.Thread(target=self.handle_request())
-		thread.start()
+		self.close_connection = 0
+		while self.keep:
+			self.handle_request()
+			
 
 	def stop(self):
 		self.keep = False
@@ -26,16 +53,18 @@ class BackendHTTPServer(http.server.HTTPServer):
 		self.httpd.socket.close()
 		self.kill_received = True
 		
-#	def handle_one_request():
-#		print("be:", "handle_one_request") 
+#	def handle_request(self):
+#		print("be:", "handle")
+#		super()
 
 class BackendHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
 
 	def __init__(self, req, client_address, server):
+		print('be: handler_init:', self.raw_requestline)
 		super(BackendHTTPRequestHandler, self).__init__(req,
 							client_address,	server)
 
-	protocol_version = 'HTTP/1.1'
+	protocol_version = 'HTTP/1.0'
 
 	def do_GET(self):
 		print("be:", "do_GET")
