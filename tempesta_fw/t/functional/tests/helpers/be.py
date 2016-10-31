470786e3ca02c77d4785e5cbd67b7f16658ce7a5
Sergey Lvov <sl@natsys-lab.com>
2016-10-31 18:56:28 +0300

    resp

--- tempesta_fw/t/functional/tests/helpers/be.py
+++ tempesta_fw/t/functional/tests/helpers/be.py
@@ -6,18 +6,25 @@ __license__ = 'GPL2'
 """
 A primitive back-end HTTP server implementation suitable for testing purposes.
 """
-import http.server 
+#import http.server 
 import threading
 import os
 import sys
 import signal
+import SimpleHTTPServer
+import BaseHTTPServer
+from  SimpleHTTPServer import SimpleHTTPRequestHandler
+import socket
+import SocketServer
 
 process_id = 0
 server = None
 def start():
-	httpd = BackendHTTPServer(host='127.0.0.1', port=8080,
-				  handler=BackendHTTPRequestHandler)
+#	httpd = BackendHTTPServer(host='127.0.0.1', port=8080,
+#				  handler=BackendHTTPRequestHandler)
+	httpd = Server(('127.0.0.1', 8080), BackendHTTPRequestHandler)
 	server = httpd
+	server.socket.setblocking(0)
 	pid = os.fork()
 	if pid > 0:
 		process_id = os.getpid()
@@ -25,7 +32,7 @@ def start():
 	else:
 		process_id = os.getpid()	
 		print("fork - pid:{}".format(process_id))
-		server.run()
+		server.serve_forever()
 
 def stop():
 	if server != None:
@@ -33,42 +40,48 @@ def stop():
 		server.keep = False
 	os.kill(process_id, signal.SIGTERM)
 	sys.exit(0)
+
+class Server(BaseHTTPServer.HTTPServer):
+	pass
+
+class BackendHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	
-class BackendHTTPServer(http.server.HTTPServer):
-# A simple backend http server.	
-	def __init__(self, host, port, handler):
-	# Initialize HTTP server, bind/listen/etc.
-		self.keep = True
-		super(BackendHTTPServer, self).__init__((host, port), handler)
-
-	def run(self):
-		self.close_connection = 0
-		while self.keep:
-			self.handle_request()
-			
-
-	def stop(self):
-		self.keep = False
-		self.shutdown()
-		self.httpd.socket.close()
-		self.kill_received = True
-		
-#	def handle_request(self):
-#		print("be:", "handle")
-#		super()
-
-class BackendHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-
 	def __init__(self, req, client_address, server):
-		print('be: handler_init:', self.raw_requestline)
-		super(BackendHTTPRequestHandler, self).__init__(req,
-							client_address,	server)
+		self. protocol_version = b'HTTP/1.0'
+		SimpleHTTPRequestHandler.__init__(self, req, client_address,
+						  server)
+		print('be: handler_init:')
+#		super(BackendHTTPRequestHandler, self).__init__(self,req,
+#								('127.0.0.1', 
+#								 8080), server)
 
-	protocol_version = 'HTTP/1.0'
+	
+
+#	def handle_one_request(self):
+#		print('me:', 'handle one')
+
+	def handle(self):
+		self.close_connection = 0
+		try:
+			self.handle_one_request()
+		except socket.error, e:
+			if e[0] != errno.CONNRESET:
+				raise
+
+	def handle_one_request(self):
+		print("me:handle_one:block")
+		self.rfile._sock.setblocking(0)
+		self.rfile._sock.settimeout(5)
+		SimpleHTTPServer.SimpleHTTPRequestHandler.handle_one_request(self)
+#		super(self)
 
 	def do_GET(self):
+#		self.send_response(200)
+		resp = self.protocol_version + b' 200 - OK\r\n' +\
+b'Date: Mon, 31 Oct 2016 06:41:19 GMT\r\n' +\
+b'Server: python be\r\n\r\n'
 		print("be:", "do_GET")
-		self.wfile.write(b"Ok\n")
+		self.wfile.write(resp)
 		self.wfile.flush()
-		return super()
+#		return super()
 
