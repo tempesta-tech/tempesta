b48b5696da3d22e8aab4bc163dd16db9cb080916
Alexander K <ak@natsys-lab.com>
2016-11-28 02:14:56 +0300

    #439: switch off RPS on RSS devices; properly figure out and distribyte device queues; some debugging

--- scripts/tfw_lib.sh
+++ scripts/tfw_lib.sh
@@ -26,23 +26,86 @@ popd > /dev/null
 declare -r TFW_NAME=`basename $0` # program name (comm name in ps)
 declare -r TFW_NETDEV_PATH="/sys/class/net/"
 declare -r TFW_SCRIPTS="$TFW_ROOT/scripts"
+declare -r CPUS_N=$(grep -c processor /proc/cpuinfo)
 
-declare TFW_DEVS=$(ls $TFW_NETDEV_PATH)
+declare TFW_DEVS=$(ifconfig | grep -o '^[a-z0-9]\+')
+
+calc()
+{
+	echo "$1" | bc -iq | tail -1
+}
+
+distribute_queues()
+{
+	dev=$1
+	RXQ_MAX=$(ethtool -l $dev 2>/dev/null \
+		  | grep -m 1 RX | sed -e 's/RX\:\s*//')
+
+	echo "...distribute $dev queues"
+
+	if [ "$RXQ_MAX" ]; then
+		echo "...configuring rx queues, please wait..."
+		if [ $RXQ_MAX -gt $CPUS_N ]; then
+			ethtool -L $dev rx $CPUS_N >/dev/null 2>&1
+		else
+			ethtool -L $dev rx $RXQ_MAX >/dev/null 2>&1
+		fi
+		# Wait for the interface reconfiguration.
+		opstate="/sys/class/net/$dev/operstate"
+		while [ "$(cat $opstate)" = "down" ]; do
+			sleep 1
+		done
+		# Sleep bit more - it seems NIC sometimes
+		# drops packets even after reporint UP state.
+		# TODO AK_DBG fix server connection reistablishing
+		sleep 5
+	fi
+
+	irqs=($(grep $dev /proc/interrupts \
+		| sed -e 's/\s*\|:.*//g'))
+	irq0=${irqs[0]}
+	for i in ${irqs[@]}; do
+		# Wrap around CPU mask if number of queues is
+		# larger than CPUS_N.
+		if [ $(calc "$i - $irq0") -gt $CPUS_N ]; then
+			irq0=$i;
+		fi
+		perl -le '
+			my $a = 1 << ('$i' - '$irq0');
+			if ($a <= 0x80000000) {
+				printf("%x\n", $a)
+			} else {
+				$a = $a / 0x100000000;
+				printf("%x,00000000\n", $a)
+			}
+		' > /proc/irq/$i/smp_affinity
+	done
+}
 
 # Enable RPS for specified, or all by default, networking interfaces.
 # This is required for loopback interface for proper local delivery,
 # but physical interfaces can have RSS.
-# TODO assign RSS queues as well.
-tfw_set_rps()
+tfw_set_net_queues()
 {
-	cpu_n=$(grep -c processor /proc/cpuinfo)
-	cpu_mask=$(perl -le 'printf("%x", (1 << '$cpu_n') - 1)')
+	min_queues=$(calc "$CPUS_N / 2")
+	cpu_mask=$(perl -le 'printf("%x", (1 << '$CPUS_N') - 1)')
 
 	for dev in $TFW_DEVS; do
-		echo "...setup interface $dev"
-		for rx in $TFW_NETDEV_PATH/$dev/queues/rx-*; do
-			echo $cpu_mask > $rx/rps_cpus
-		done
+		queues=$(ls -d /sys/class/net/$dev/queues/rx-*|wc -l)
+		if [ $queues -lt $min_queues ]; then
+			echo "...enable RPS on $dev"
+			for rx in $TFW_NETDEV_PATH/$dev/queues/rx-*; do
+				echo $cpu_mask > $rx/rps_cpus
+			done
+		else
+
+			# Switch off RPS for multi-queued interfaces.
+			for rx in $TFW_NETDEV_PATH/$dev/queues/rx-*; do
+				echo 0 > $rx/rps_cpus
+			done
+
+			distribute_queues $dev
+		fi
 	done
 }
 
