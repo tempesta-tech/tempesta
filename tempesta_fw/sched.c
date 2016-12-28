da8012b3e2af2a2a7a7e0db546e364c99ddf2045
Ivan Koveshnikov <ik@tempesta-tech.com>
2016-12-28 15:06:20 +0500

    sched: update warning if sticky session must be rescheduled

--- tempesta_fw/sched.c
+++ tempesta_fw/sched.c
@@ -102,9 +102,9 @@ sched_conn_sticky(TfwMsg *msg, TfwSrvGroup *sg)
 			char addr_str[TFW_ADDR_STR_BUF_SIZE] = { 0 };
 
 			tfw_addr_ntop(&srv->addr, addr_str, sizeof(addr_str));
-			TFW_WARN("sched: Unable to reschedule request to the "
-				 "same server %s, schedule from server group\n",
-				 addr_str);
+			TFW_WARN("sched %s: Unable to schedule new request in "
+				 "session to the same server %s in group %s\n",
+				 srv->sg->sched->name, addr_str, srv->sg->name);
 		}
 	}
 
