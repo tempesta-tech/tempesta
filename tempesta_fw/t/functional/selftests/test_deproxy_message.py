0c0c1c30d5700daa104ed05223afcbdaccd101b7
Ivan Koveshnikov <ik@tempesta-tech.com>
2017-03-09 15:20:12 +0500

    tests: deproxy add http message fabric

--- tempesta_fw/t/functional/selftests/test_deproxy_message.py
+++ tempesta_fw/t/functional/selftests/test_deproxy_message.py
@@ -151,8 +151,63 @@ class TestDeproxyMessage(unittest.TestCase):
                 print('Error happen when processed message\n%s' % message)
                 raise
 
-    def test_request_fabric(self):
-        pass
+    def test_request_plain(self):
+        request = deproxy.Request(
+            "GET / HTTP/1.1\r\n"
+            "Host: 10.0.10.2\r\n"
+            "User-Agent: curl/7.53.1\r\n"
+            "Accept: */*\r\n"
+            "\r\n")
+        headers = ['Host: 10.0.10.2', 'User-Agent: curl/7.53.1', 'Accept: */*']
+        created = deproxy.Request.create('GET', headers)
+        self.assertEqual(request, created)
 
-    def test_response_fabric(self):
-        pass
+    def test_request_body(self):
+        request = deproxy.Request(
+            "GET / HTTP/1.1\r\n"
+            "Host: 10.0.10.2\r\n"
+            "User-Agent: curl/7.53.1\r\n"
+            "Accept: */*\r\n"
+            "Content-Length: 6\r\n"
+            "\r\n"
+            "<html>"
+            "\r\n")
+        headers = ['Host: 10.0.10.2', 'User-Agent: curl/7.53.1', 'Accept: */*',
+                   'Content-Length: 6']
+        body = "<html>\r\n"
+        created = deproxy.Request.create('GET', headers, body=body)
+        self.assertEqual(request, created)
+
+    def test_response_plain(self):
+        response = deproxy.Response(
+            "HTTP/1.1 200 OK\r\n"
+            "Server: SimpleHTTP/0.6 Python/3.6.0\r\n"
+            "Content-type: text/html\r\n"
+            "Content-Length: 138\r\n"
+            "Last-Modified: Mon, 12 Dec 2016 13:59:39 GMT\r\n"
+            "\r\n"
+            "<html>\r\n"
+            "<head>\r\n"
+            "  <title>An Example Page</title>\r\n"
+            "</head>\r\n"
+            "<body>\r\n"
+            "  Hello World, this is a very simple HTML document.\r\n"
+            "</body>\r\n"
+            "</html>\r\n"
+        )
+        headers = [
+            'Server: SimpleHTTP/0.6 Python/3.6.0',
+            'Content-type: text/html',
+            'Content-Length: 138',
+            'Last-Modified: Mon, 12 Dec 2016 13:59:39 GMT']
+        body = (
+            "<html>\r\n"
+            "<head>\r\n"
+            "  <title>An Example Page</title>\r\n"
+            "</head>\r\n"
+            "<body>\r\n"
+            "  Hello World, this is a very simple HTML document.\r\n"
+            "</body>\r\n"
+            "</html>\r\n")
+        created = deproxy.Response.create(200, headers, body=body)
+        self.assertEqual(response, created)
