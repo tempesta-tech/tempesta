c4310e73ede1c6a0f5bce1d8a0e8ccbedc2df51b
Ivan Koveshnikov <ik@tempesta-tech.com>
2017-05-04 13:07:34 +0500

    pkg: update installer script

--- pkg/scripts/tempesta_installer.sh
+++ pkg/scripts/tempesta_installer.sh
@@ -29,10 +29,16 @@ declare -r GITHUB_REPO_LINUX="linux-4.8.15-tfw"
 #TODO: currently Debian 9 is the only supported distribution, other
 # distributions may have other names.
 # Don't install packages with debug symbols.
-declare -a FILES_LINUX=("linux-image-[\d.]+-tempesta-amd64-unsigned"
+declare -a FILES_LINUX=("linux-image-[\d.]+-tempesta(.bpo)?-amd64-unsigned"
                         "linux-kbuild-[\d.]+_[\d.]+"
-                        "linux-headers-[\d.]+-tempesta")
+                        "linux-headers-[\d.]+-tempesta"
+                        "linux-compiler")
 declare -a FILES_TEMPESTA=("tempesta-fw-dkms")
+
+if [ "$(id -u)" != "0" ]; then
+	echo "This script must be run as root" 2>&1
+	exit 1
+fi
 
 
 usage()
@@ -52,15 +58,23 @@ usage()
 tfw_download()
 {
 	repo="${1}"
-	uri="https://api.github.com/repos/$GITHUB_USER/$1/releases/latest"
+	tag=`curl -s https://api.github.com/repos/$GITHUB_USER/$repo/tags | grep -P 'name": "\d+' | cut -d '"' -f 4 | sort -V -r | head -n1`
+	if [[ ! "$tag" ]]; then
+		echo "Can't find latest release in repo: https://github.com/$GITHUB_USER/$repo"
+		#TODO: show next line only if received 403 status code.
+		echo "Or may be Github API rate limit exceeded."
+		exit 2
+	fi
 
-	links=`curl -s $uri | grep browser_download_url | grep -P $2 | cut -d '"' -f 4`
+	release_tag="$DISTRO/$tag"
+	uri="https://api.github.com/repos/$GITHUB_USER/$repo/releases/tags/${release_tag}"
 
+	links=`curl -s $uri | grep browser_download_url | grep -P "$2" | cut -d '"' -f 4`
 	if [[ ! "$links" ]]; then
-		# Github api rate limit exceded
-		echo "Github API rate limit exceded: more than 60 requests" \
-		     "during last hour. Try again later."
-
+		echo "Can't download file $2 from release ${release_tag} in repo:"
+		echo "https://github.com/$GITHUB_USER/$repo"
+		#TODO: show next line only if received 403 status code.
+		echo "Or may be Github API rate limit exceeded."
 		exit 2
 	fi
 
@@ -91,9 +105,37 @@ tfw_install_packages()
 
 tfw_install_deps()
 {
+	echo ""
 	echo "Install dependencies ..."
+	APT_OPTS=
+
+	case $DISTRO in
+	"debian-8")
+		echo ""
+		echo "Installation on Debian 8 requires updating system from"
+		echo "jessie-backports repository before installing TempestaFW."
+		tfw_confirm
+
+		echo "deb http://deb.debian.org/debian/ " \
+		        "jessie-backports main" >> /etc/apt/sources.list
+		apt-get update
+		apt-get -t jessie-backports dist-upgrade -y
+		APT_OPTS="-t jessie-backports"
+		;;
+	*)
+		;;
+	esac
+
+	echo ""
+	echo "Since TempestaFW is implemented as in-kernel module and it is"
+	echo "under development, issues in it can affect stability of the"
+	echo "kernel and may lead to a kernel crush. It is highly recommended"
+	echo "to enable 'kdump-tools' by default in the next dialogue."
+	read -n 1 -s -p "Press any key to continue..."
 	# curl and wget are required for the script itself.
-	apt-get install -y dkms libboost-dev libboost-program-options-dev kdump-tools curl wget
+	apt-get ${APT_OPTS} install -y dkms libboost-dev libboost-program-options-dev kdump-tools curl wget
+
+	echo ""
 }
 
 tfw_install()
@@ -139,31 +181,105 @@ tfw_remove()
 	apt-get $RM_METHOD $RM_PACKAGES
 }
 
-args=$(getopt -o "" -a -l "$LONG_OPTS" -- "$@")
+tfw_confirm()
+{
+	local response
+	local msg="${1:-Continue installing TempestaFW?} [y/N] "; shift
+
+	read -r $* -p "$msg" response || echo
+	case "$response" in
+	[yY][eE][sS]|[yY])
+		return 1
+		;;
+	*)
+		echo "Installation was canceled."
+		exit
+		;;
+	esac
+}
+
+tfw_try_distro()
+{
+	d_name=`cat /etc/os-release | grep PRETTY_NAME | cut -d '"' -f 2`
+
+	case $d_name in
+	"Debian GNU/Linux 8 (jessie)")
+		DISTRO="debian-8"
+		;;
+	"Debian GNU/Linux 9 (stretch)")
+		DISTRO="debian-9"
+		;;
+	*)
+		echo "Installer does not support $d_name distro!"
+		exit 2
+		;;
+	esac
+}
+
+tfw_set_grub_default()
+{
+	if [[ "$TFW_SKIP_GRUB_DFT" ]]; then
+		return
+	fi
+
+	entry=`grep menuentry /boot/grub/grub.cfg | grep tempesta | head -n1 | cut -d "'" -f 2`
+	if [[ ! "$entry" ]]; then
+		echo "Error: Can't find Tempesta patched kernel in /boot/grub/grub.cfg!"
+		return
+	fi
+
+	echo "GRUB_DEFAULT='Advanced options for Debian GNU/Linux>$entry'" >> /etc/default/grub
+	update-grub
+}
+
+
+tfw_try_distro
+
+args=$(getopt -o ":" -a -l "$LONG_OPTS" -- "$@")
 eval set -- "${args}"
-while :; do
-	case "$1" in
-		# Selectors for internal usage.
-		--install)
-			tfw_install
-			exit
-			;;
-		--remove)
-			tfw_remove remove
-			exit
-			;;
-		--purge)
-			tfw_remove purge
-			exit
-			;;
-		--help)
-			usage
-			exit
-			;;
-		*)
-			echo "Bad command line argument: $opt"
-			usage
-			exit 2
-			;;
+for opt in ${args}
+do
+	case "$opt" in
+	--install)
+		tfw_install
+		;;
+	--remove)
+		tfw_remove remove
+		exit
+		;;
+	--purge)
+		tfw_remove purge
+		exit
+		;;
+	--help)
+		usage
+		exit
+		;;
+	--no-grub-default)
+		TFW_SKIP_GRUB_DFT=true
+		;;
+	"--") ;;
+	*)
+		echo "Bad command line argument: $opt"
+		usage
+		exit 2
+		;;
 	esac
 done
+
+# At the end of install set default kernel for GRUB.
+tfw_set_grub_default
+
+echo ""
+echo "Installation is completed!"
+if [[ ! "$TFW_SKIP_GRUB_DFT" ]]; then
+	echo "Reboot to finish installation of TempestaFW. The patched kernel"
+	echo "is set as default."
+else
+	echo "Reboot to Tempesta patched kernel to run TempestaFW."
+fi
+echo ""
+echo "More information about TempestaFW configuration and troubleshooting"
+echo "can be found in readme and wiki on project page:"
+echo "https://github.com/tempesta-tech/tempesta"
+
