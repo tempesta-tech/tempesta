aa2c97aa25f319b15ec1d8c963de50ceca27efcd
Ivan Koveshnikov <ik@tempesta-tech.com>
2017-08-16 16:40:39 +0500

    parser: add conditional headers parser
    Added: ETag, If-None-Match, If-Modified-Since, Last-Modified

--- tempesta_fw/str.h
+++ tempesta_fw/str.h
@@ -179,6 +179,8 @@ size_t tfw_ultoa(unsigned long ai, char *buf, unsigned int len);
 #define TFW_STR_VALUE		0x08
 /* The string represents hop-by-hop header, not end-to-end one */
 #define TFW_STR_HBH_HDR		0x10
+/* Weak identifier was set for Etag value. */
+#define TFW_STR_ETAG_WEAK	0x20
 
 /*
  * @ptr		- pointer to string data or array of nested strings;
