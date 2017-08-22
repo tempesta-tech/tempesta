7b0461a5c98c8c7511409db40347d508df370326
Ivan Shapovalov <intelfx@intelfx.name>
2017-08-22 21:09:53 +0300

    run_tests.py: implement manual and automatic test resumption
    Fixes #795.

--- tempesta_fw/t/functional/run_tests.py
+++ tempesta_fw/t/functional/run_tests.py
@@ -30,20 +30,30 @@ key, not password. `ssh-copy-id` can be used for that.
                                     and exit.
 -t, --duration <seconds>          - Duration of every single test.
 -f, --failfast                    - Stop tests after first error.
+-r, --resume <id>                 - Continue execution from first test matching
+                                    this ID prefix
+-a, --resume-after <id>           - Continue execution _after_ the first test
+                                    matching this ID prefix
+-n, --no-resume                   - Do not resume from state file
 
 Non-flag arguments may be used to include/exclude specific tests.
 Specify a dotted-style name or prefix to include every matching test:
 `cache.test_cache`, `flacky_net` (but not `sched.test_`).
 Prefix an argument with `-` to exclude every matching test: `-cache.test_purge`,
 `-flacky_net.test_sockets.CloseOnShutdown`.
+
+Testsuite execution is automatically resumed if it was interrupted, or it can
+be resumed manually from any given test.
 """)
 
 fail_fast = False
+test_resume = shell.TestResume()
 
 try:
-    options, remainder = getopt.getopt(sys.argv[1:], 'hvdt:f',
+    options, remainder = getopt.getopt(sys.argv[1:], 'hvdt:fr:a:n',
                                        ['help', 'verbose', 'defaults',
-                                        'duration=', 'failfast'])
+                                        'duration=', 'failfast', 'resume=',
+                                        'resume-after=', 'no-resume'])
 
 except getopt.GetoptError as e:
     print(e)
@@ -66,6 +76,12 @@ for opt, arg in options:
     elif opt in ('-h', '--help'):
         usage()
         sys.exit(0)
+    elif opt in ('-r', '--resume'):
+        test_resume.set(arg)
+    elif opt in ('-a', '--resume-after'):
+        test_resume.set(arg, after=True)
+    elif opt in ('-n', '--no-resume'):
+        test_resume.unlink_file()
 
 if os.geteuid() != 0:
     raise Exception("Tests must be run as root.")
@@ -93,15 +109,31 @@ for name in remainder:
     else:
         inclusions.append(name)
 
+# load resume state file, if needed
+test_resume.set_filters(inclusions, exclusions)
+if not test_resume:
+    test_resume.set_from_file()
+else:
+    tf_cfg.dbg(2, 'Not resuming from file: next test specified on command line')
+# if the file was not used, delete it
+if not test_resume.from_file:
+    test_resume.unlink_file()
+
 #
 # Discover tests, configure environment and run tests
 #
 
+addn_status = ""
+if test_resume:
+    if test_resume.last_completed:
+        addn_status = " (resuming from after %s)" % test_resume.last_id
+    else:
+        addn_status = " (resuming from %s)" % test_resume.last_id
 print("""
 ----------------------------------------------------------------------
-Running functional tests...
+Running functional tests%s...
 ----------------------------------------------------------------------
-""")
+""" % addn_status)
 
 # For the sake of simplicity, Unconditionally discover all tests and filter them
 # afterwards instead of importing individual tests by positive filters.
@@ -112,11 +144,14 @@ shell.testsuite_flatten(tests, loader.discover('.'))
 # Now that we initialized the loader, convert arguments to dotted form (if any).
 for lst in (inclusions, exclusions):
     lst[:] = [shell.test_id_parse(loader, t) for t in lst]
+test_resume.last_id = shell.test_id_parse(loader, test_resume.last_id)
 
 # filter testcases
+resume_filter = test_resume.filter()
 tests = [ t
           for t in tests
-          if (not inclusions or shell.testcase_in(t, inclusions))
+          if resume_filter(t)
+          and (not inclusions or shell.testcase_in(t, inclusions))
           and not shell.testcase_in(t, exclusions) ]
 
 #
@@ -126,7 +161,6 @@ tests = [ t
 # the default value of fs.nr_open
 nofile = 1048576
 resource.setrlimit(resource.RLIMIT_NOFILE, (nofile, nofile))
-
 remote.connect()
 
 #
@@ -136,8 +170,12 @@ remote.connect()
 testsuite = unittest.TestSuite(tests)
 testRunner = unittest.runner.TextTestRunner(verbosity=v_level,
                                             failfast=fail_fast,
-                                            descriptions=False)
+                                            descriptions=False,
+                                            resultclass=test_resume.resultclass())
 testRunner.run(testsuite)
 
+# check if we finished running the tests
+if not tests or (test_resume.last_id == tests[-1].id() and test_resume.last_completed):
+    test_resume.unlink_file()
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
