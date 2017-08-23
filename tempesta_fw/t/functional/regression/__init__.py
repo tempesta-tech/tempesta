da85a47d60c5bf3a9722efea1c9ad3f8b8d4ca84
Ivan Shapovalov <intelfx@intelfx.name>
2017-08-23 20:15:47 +0300

    func tests: add regression test for #798

--- tempesta_fw/t/functional/regression/__init__.py
+++ tempesta_fw/t/functional/regression/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ['test_shutdown', 'test_srv_failovering', 'test_stress_failovering']
+__all__ = ['test_shutdown', 'test_srv_failovering', 'test_stress_failovering',
+           'test_invalid']
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
