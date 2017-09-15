6444f1f106799ba93321e15e3cb46b17c6a7290f
Ivan Koveshnikov <ik@tempesta-tech.com>
2017-09-15 19:10:19 +0500

    fix code review comments by @krizhanovsky

--- tempesta_fw/t/fuzzer.c
+++ tempesta_fw/t/fuzzer.c
@@ -3,7 +3,7 @@
  *
  * Tempesta HTTP fuzzer.
  *
- * Copyright (C) 2015-2016 Tempesta Technologies, Inc.
+ * Copyright (C) 2015-2017 Tempesta Technologies, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
