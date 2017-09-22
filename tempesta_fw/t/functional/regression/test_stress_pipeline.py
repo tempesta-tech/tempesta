f2c896b08aae95f6408e0b65e05a2656bfc5dab4
Alexander Ostapenko <ao@tempesta-tech.com>
2017-09-23 02:55:53 +0700

    Add new stress tests.
    1. Add pipelined requests during stress testing.
    2. Add fault injection via SystemTap.

---
+++ tempesta_fw/t/functional/regression/test_stress_pipeline.py
@@ -0,0 +1,45 @@
+"""
+Pipeline stress testing.
+"""
+
+import sys
+import unittest
+from helpers import control, tempesta
+from testers import stress
+
+__author__ = 'Tempesta Technologies, Inc.'
+__copyright__ = 'Copyright (C) 2017 Tempesta Technologies, Inc.'
+__license__ = 'GPL2'
+
+class Pipeline(stress.StressTest):
+
+    def create_clients(self):
+        self.wrk = control.Wrk()
+        self.wrk.set_script("pipeline")
+        self.clients = [self.wrk]
+
+    def create_servers(self):
+        self.create_servers_helper(tempesta.servers_in_group())
+
+    def run_test(self, ka_reqs):
+        for s in self.servers:
+            s.config.set_ka(ka_reqs)
+        self.generic_test_routine('cache 0;\n')
+
+    def test_unlimited_ka(self):
+        self.run_test(sys.maxsize)
+
+    @unittest.expectedFailure
+    def test_low_ka(self):
+        """Low keep_alive value, make the server to close after the limit
+        is exhausted connection; thus Tempesta must generate 502 response.
+        """
+        self.run_test(50)
+
+class PipelineFaultInjection(Pipeline):
+
+    def create_tempesta(self):
+        self.tempesta = control.TempestaFI('resp_alloc_err', True)
+
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
