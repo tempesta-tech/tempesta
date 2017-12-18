02205320ee9830236fbfa256ae3d0de324b25a56
Ivan Koveshnikov <ik@tempesta-tech.com>
2017-12-18 17:34:50 +0500

    add_hdr directive: correct follow and override of global settings

--- tempesta_fw/t/functional/vhost/test_add_hdr.py
+++ tempesta_fw/t/functional/vhost/test_add_hdr.py
@@ -10,20 +10,26 @@ __license__ = 'GPL2'
 
 class TestAddHeader(functional.FunctionalTest):
 
-    def add_hdrs(self, hdrs, location=None):
-        self.config = ''
-        self.msg_chain = chains.proxy()
+    location = '/'
 
+    def config_append_directive(self, hdrs, location=None):
         if location is not None:
             self.config = self.config + ('location prefix "%s" {\n' % location)
-
         for (name, val) in hdrs:
             self.config = self.config + ('hdr_add %s "%s";\n' % (name, val))
+        if location is not None:
+            self.config = self.config + '}\n'
+
+    def make_chain(self, hdrs):
+        self.msg_chain = chains.proxy()
+        for (name, val) in hdrs:
             self.msg_chain.response.headers[name] = val
         self.msg_chain.response.update()
 
-        if location is not None:
-            self.config = self.config + '}\n'
+    def add_hdrs(self, hdrs, location=None):
+        self.config = ''
+        self.config_append_directive(hdrs, location)
+        self.make_chain(hdrs)
 
     def test_add_one_hdr(self):
         hdrs = [('X-My-Hdr', 'some text')]
@@ -39,5 +45,26 @@ class TestAddHeader(functional.FunctionalTest):
     def test_add_some_hdrs_custom_location(self):
         hdrs = [('X-My-Hdr', 'some text'),
                 ('X-My-Hdr-2', 'some other text')]
-        self.add_hdrs(hdrs, '/')
+        self.add_hdrs(hdrs, self.location)
         self.generic_test_routine(self.config, [self.msg_chain])
+
+    def test_add_hdrs_derive_config(self):
+        '''Derive general settings to custom location.'''
+        hdrs = [('X-My-Hdr', 'some text')]
+        self.config = ''
+        self.config_append_directive(hdrs)
+        self.config_append_directive([], self.location)
+        self.make_chain(hdrs)
+        self.generic_test_routine(self.config, [self.msg_chain])
+
+    def test_add_hdrs_override_config(self):
+        '''Override general settings to custom location.'''
+        hdrs = [('X-My-Hdr', 'some text')]
+        o_hdrs = [('X-My-Hdr-2', 'some other text')]
+        self.config = ''
+        self.config_append_directive(hdrs)
+        self.config_append_directive(o_hdrs, self.location)
+        self.make_chain(o_hdrs)
+        self.generic_test_routine(self.config, [self.msg_chain])
+
+# TODO: add tests for different vhosts, when vhosts will be implemented.
