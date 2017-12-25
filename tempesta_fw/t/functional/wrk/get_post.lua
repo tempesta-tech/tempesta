e0fc96035a8606b35c998e52ab14d7f0ad9c5c11
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2017-12-25 23:02:40 +0300

    fixes and real request test

--- tempesta_fw/t/functional/wrk/get_post.lua
+++ tempesta_fw/t/functional/wrk/get_post.lua
@@ -2,8 +2,8 @@ init = function(args)
     local r = {}
     r[1] = wrk.format("GET", "/")
     r[2] = wrk.format("GET", "/", {["Accept"] = "text/plain"})
-    r[3] = wrk.format("POST", "/")
-    r[4] = wrk.format("POST", "/", {["Content-Type"]="text/plain"})
+    r[3] = wrk.format("POST", "/", {["Content-Length"]="0"})
+    r[4] = wrk.format("POST", "/", {["Content-Type"]="text/plain", ["Content-Length"]="0"})
     r[5] = wrk.format("GET", "/", {["Host"] = ""})
     req = table.concat(r)
 end
