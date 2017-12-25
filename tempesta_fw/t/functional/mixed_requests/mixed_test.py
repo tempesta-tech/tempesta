79dcbe5a4b4610c972ab312d7228ff5b7981fa6e
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2017-12-25 23:02:40 +0300

    fixes and real request test

--- tempesta_fw/t/functional/mixed_requests/mixed_test.py
+++ tempesta_fw/t/functional/mixed_requests/mixed_test.py
@@ -5,8 +5,10 @@ class MixedRequests(stress.StressTest):
     """ Mixed requests test """
     config = 'cache 0;\n'
     script = None
+    clients_num = int(tf_cfg.cfg.get('General', 'concurrent_connections'))
+
     def create_clients(self):
-        self.wrk = control.Wrk(threads=8)
+        self.wrk = control.Wrk(threads=self.clients_num)
         self.wrk.set_script(self.script)
         self.clients = [self.wrk]
 
