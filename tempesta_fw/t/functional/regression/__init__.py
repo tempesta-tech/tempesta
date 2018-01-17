ac99b7f8877ae1b04ffa811ed6d00a05a78e8b29
Alexander Ostapenko <ao@tempesta-tech.com>
2018-01-17 19:43:16 +0700

    Add functional test for health monitor (PR#877).

--- tempesta_fw/t/functional/regression/__init__.py
+++ tempesta_fw/t/functional/regression/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ['test_shutdown', 'test_srv_failovering', 'test_stress_failovering',
-           'test_invalid', 'test_stress_pipeline']
+           'test_invalid', 'test_stress_pipeline', 'test_health_monitor']
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
