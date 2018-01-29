024bc2955467408aeef4ca62a1d020edd801ac29
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2018-01-29 20:08:05 +0300

    Request tests with wrong length
    expect 403 for wrong length
    
    Parsing multiple responses
    
    Expect exactly 1 parsing error
    
    check for garbage after response end
    
    Write special testers for wrong length

--- tempesta_fw/t/functional/helpers/wrk.py
+++ tempesta_fw/t/functional/helpers/wrk.py
@@ -4,7 +4,7 @@ __author__ = 'Tempesta Technologies, Inc.'
 __copyright__ = 'Copyright (C) 2017 Tempesta Technologies, Inc.'
 __license__ = 'GPL2'
 
-import os
+from . import remote
 
 class ScriptGenerator(object):
     """ Generate lua script """
@@ -12,8 +12,9 @@ class ScriptGenerator(object):
     uri = "/"
     headers = []
     body = ""
-    filename = None
+    config = ""
     def __luaencode(self, value):
+        # TODO: take care about escaping
         return value
 
     def set_request_type(self, request_type):
@@ -30,28 +31,14 @@ class ScriptGenerator(object):
 
     def make_config(self, filename):
         """ Generate config and write it to file """
-        self.filename = filename
-        config = open(filename, 'w')
-        config.write("local r = {\n")
-        config.write("    method = \"%s\",\n" % self.__luaencode(self.request_type))
-        config.write("    path = \"%s\",\n" % self.__luaencode(self.uri))
-        config.write("    headers = {\n")
+        config = ""
+        config += "wrk.method = \"%s\"\n" % self.request_type
+        config +="wrk.path = \"%s\",\n" % self.__luaencode(self.uri)
+        config += "wrk.headers = {\n"
         for header in self.headers:
-            config.write("        [\"%s\"] = \"%s\",\n" % (header[0], header[1]))
-        config.write("    },\n")
-        config.write("    body = \"%s\",\n" % self.body)
-        config.write("}\n")
-        config.write("local req\n")
-        config.write("init = function()\n")
-        config.write("    req = wrk.format(r.method, r.path, r.headers, r.body)\n")
-        config.write("end\n")
-        config.write("request = function()\n")
-        config.write("    return req\n")
-        config.write("end\n")
-        config.close()
-
-    def remove_config(self):
-        """ Remove previously created config """
-        if self.filename != None:
-            os.unlink(self.filename)
-            self.filename = None
+            name = self.__luaencode(header[0])
+            value = self.__luaencode(header[1])
+            config += "    [\"%s\"] = \"%s\",\n" % (name, value)
+        config += "},\n"
+        config += "wrk.body = \"%s\",\n" % self.__luaencode(self.body)
+        remote.client.copy_file(filename, config)
