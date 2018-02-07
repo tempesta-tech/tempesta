06c7c4b0f3c0c161367c7675400bb17f91af6659
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2018-02-07 18:15:02 +0300

    Specify pipeline

--- tempesta_fw/t/functional/mixed_requests/test_mixed.py
+++ tempesta_fw/t/functional/mixed_requests/test_mixed.py
@@ -10,39 +10,49 @@ __license__ = 'GPL2'
 class GetRequests(mixed_test.MixedRequests):
     """ HEAD, GET requests """
     script = "pipeline"
+    pipelined_req = 7
 
 class RealRequest(mixed_test.MixedRequests):
     """ Real GET request """
     script = "get_real"
+    pipelined_req = 1
 
 class RealRequest2(mixed_test.MixedRequests):
     """ Real GET request 2"""
     script = "get_real_2"
+    pipelined_req = 1
 
 class RealRequestPipeline(mixed_test.MixedRequests):
     """ Real pipelined GET request """
     script = "get_real_pipelined"
+    pipelined_req = 3
 
 class GetPostRequests(mixed_test.MixedRequests):
     """ HEAD, GET requests """
     script = "get_post"
+    pipelined_req = 5
 
 class HeadGetRequests(mixed_test.MixedRequests):
     """ HEAD, GET requests """
     script = "head_get"
+    pipelined_req = 2
 
 class EmptyPostRequests(mixed_test.MixedRequests):
     """ POST requests """
     script = "post_empty"
+    pipelined_req = 1
 
 class SmallPostRequests(mixed_test.MixedRequests):
     """ POST requests """
     script = "post_small"
+    pipelined_req = 1
 
 class BigPostRequests(mixed_test.MixedRequests):
     """ POST requests """
     script = "post_big"
+    pipelined_req = 1
 
 class RarelyUsedRequests(mixed_test.MixedRequests):
     """ Rarely used requests """
     script = "mixed"
+    pipelined_req = 9
