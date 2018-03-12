f854444730a0092a1c459fa7a02f418d00fb7052
Alexander Ostapenko <ao@tempesta-tech.com>
2018-03-12 17:50:06 +0700

    Correction according review comments (PR#947).

--- scripts/tempesta.sh
+++ scripts/tempesta.sh
@@ -19,8 +19,14 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59
 # Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-[ "${TEMPESTA_LCK}" != "$0" ] && \
-    exec env TEMPESTA_LCK="$0" flock -n "/tmp/tempesta-lock-file" "$0" "$@"
+if [ "${TEMPESTA_LCK}" != "$0" ]; then
+	env TEMPESTA_LCK="$0" flock -n -E 254 "/tmp/tempesta-lock-file" "$0" "$@"
+	if [ $? -eq 254 ]; then
+		echo "Cannot operate with Tempesta FW: locked by another process"
+		exit 3
+	fi
+	exit
+fi
 
 . "$(dirname $0)/tfw_lib.sh"
 
