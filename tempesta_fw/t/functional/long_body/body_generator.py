3ff7d64c767aed672388e9b2875e57770d8f9c7f
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2018-04-10 16:00:40 +0300

    Remove tests from this repository

--- tempesta_fw/t/functional/long_body/body_generator.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'Tempesta Technologies, Inc.'
-__copyright__ = 'Copyright (C) 2017-2018 Tempesta Technologies, Inc.'
-__license__ = 'GPL2'
-
-def generate_body(length):
-    """ Generate body of specified length """
-    return "x" * length
--- tempesta_fw/t/functional/selftests/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ['test_requests', 'test_responses', 'test_headercollection',
-           'test_deproxy']
-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
