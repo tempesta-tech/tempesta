cedaa8753438c507109b3ab9fe86354cf02d0d2b
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2018-04-10 16:00:40 +0300

    Remove tests from this repository

--- tempesta_fw/t/functional/cache/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ['test_cache', 'test_cache_methods', 'test_purge', 'test_conditional']
-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
--- tempesta_fw/t/functional/tcp_connection/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ['test_connection_close']
-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
