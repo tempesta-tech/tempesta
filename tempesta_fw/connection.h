3759b5478514605ce9bfe877cc6767cfebfd443c
Alexander K <ak@tempesta-tech.com>
2018-06-18 21:34:26 +0300

    Gigantic messy commit which is no way to review

--- tempesta_fw/connection.h
+++ tempesta_fw/connection.h
@@ -205,10 +205,10 @@ enum {
  */
 typedef struct {
 	TfwCliConn	cli_conn;
-	TfwTlsCtx	tls;
+	TfwTlsCtx	tc;
 } TfwTlsConn;
 
-#define tfw_tls_context(conn)	(TfwTlsCtx *)(&((TfwTlsConn *)conn)->tls)
+#define tfw_tls_context(p)	(TfwTlsCtx *)(&((TfwTlsConn *)p)->c)
 
 /* Callbacks used by l5-l7 protocols to operate on connection level. */
 typedef struct {
