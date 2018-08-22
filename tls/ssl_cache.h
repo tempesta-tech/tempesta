40d0072a9df8cea4b7e069ed7c9f8af5d349f7f9
Alexander K <ak@natsys-lab.com>
2018-08-23 01:50:36 +0300

    TODO comments for #488,#515,#1054

--- tls/ssl_cache.h
+++ tls/ssl_cache.h
@@ -1,49 +1,28 @@
 /**
- * \file ssl_cache.h
+ *		Tempesta TLS
  *
- * \brief SSL session cache implementation
- */
-/*
- *  Copyright (C) 2006-2015, ARM Limited, All Rights Reserved
- *  Copyright (C) 2015-2018 Tempesta Technologies, Inc.
- *  SPDX-License-Identifier: GPL-2.0
+ * Copyright (C) 2006-2015, ARM Limited, All Rights Reserved
+ * Copyright (C) 2015-2018 Tempesta Technologies, Inc.
+ * SPDX-License-Identifier: GPL-2.0
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- *  This file is part of mbed TLS (https://tls.mbed.org)
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 #ifndef TTLS_CACHE_H
 #define TTLS_CACHE_H
 
 #include "ttls.h"
-
-/**
- * \name SECTION: Module settings
- *
- * The configuration options you can set for this module are in this section.
- * Either change them in config.h or define them on the compiler command line.
- * \{
- */
-
-#if !defined(TTLS_CACHE_DEFAULT_TIMEOUT)
-#define TTLS_CACHE_DEFAULT_TIMEOUT	   86400   /*!< 1 day  */
-#endif
-
-#if !defined(TTLS_CACHE_DEFAULT_MAX_ENTRIES)
-#define TTLS_CACHE_DEFAULT_MAX_ENTRIES	  50   /*!< Maximum entries in cache */
-#endif
 
 typedef struct ttls_cache_context ttls_cache_context;
 typedef struct ttls_cache_entry ttls_cache_entry;
