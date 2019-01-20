d964ff491ba6a02713b8aa4002c143e5aa28ef26
Alexander K <ak@tempesta-tech.com>
2019-01-21 00:59:55 +0300

    Fix #984:
    1. accurately fix skb->truesize and TCP write memory in kernel
       by tcp_skb_unclone();
    
    2. __split_pgfrag_del() if we just move pointers, then we do not free
       TCP write memory, so do not change skb->truesize.
    
    3. ss_skb_unroll(): truesize and data_len/len are completely different counters,
       so do not mix them in ss_skb_adjust_data_len(). By the way, during the tests
       I saw crazy skb overheads - truesize can be larger than len in tens kilobytes.
       The explanation for such overheads is various fragments stoling (e.g. our
       __split_pgfrag_del) and cloning.
    
    4. cleanup: move ss_skb coalescing functions closer to their calls.

--- tempesta_fw/sock.c
+++ tempesta_fw/sock.c
@@ -382,9 +382,10 @@ ss_do_send(struct sock *sk, struct sk_buff **skb_head, int flags)
 		/* Propagate mark of message head skb.*/
 		skb->mark = mark;
 
-		TFW_DBG3("[%d]: %s: entail skb=%pK data_len=%u len=%u mark=%u"
-			 " tls_type=%x\n", smp_processor_id(), __func__,
-			 skb, skb->data_len, skb->len, skb->mark,
+		TFW_DBG3("[%d]: %s: entail sk=%pK skb=%pK data_len=%u len=%u"
+			 " truesize=%u mark=%u tls_type=%x\n",
+			 smp_processor_id(), __func__, sk,
+			 skb, skb->data_len, skb->len, skb->truesize, skb->mark,
 			 tempesta_tls_skb_type(skb));
 
 		skb_entail(sk, skb);
@@ -449,11 +450,7 @@ ss_send(struct sock *sk, struct sk_buff **skb_head, int flags)
 	 * or copy them if they're going to be used by Tempesta during
 	 * and after the transmission.
 	 */
-	/*
-	 * FIXME #984 the `true ||` statement at the below fixes the issue
-	 * (at least basic tests are passed now).
-	 */
-	if (/*true ||*/ flags & SS_F_KEEP_SKB) {
+	if (flags & SS_F_KEEP_SKB) {
 		skb = *skb_head;
 		do {
 			/* tcp_transmit_skb() will clone the skb. */
