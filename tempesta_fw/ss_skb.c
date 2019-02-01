be5be03733b380928cc030eee7218da1832ba638
Alexander Ostapenko <ao@tempesta-tech.com>
2019-02-01 15:02:42 +0700

    Add explanatory comment for behaviour of __extend_pgfrags().

--- tempesta_fw/ss_skb.c
+++ tempesta_fw/ss_skb.c
@@ -329,7 +329,14 @@ __extend_pgfrags(struct sk_buff *skb_head, struct sk_buff *skb, int from, int n)
 		if (!tail_frags)
 			return 0;
 
-		/* Shift @n_excess number of page fragments to new SKB. */
+		/*
+		 * Move @n_excess number of page fragments to new SKB. We
+		 * must move @n_excess fragments to next/new skb, except
+		 * those, which we are inserting (@n fragments) - so we
+		 * must move last @n_excess fragments: not more than
+		 * @tail_frags, and not more than @n_excess itself
+		 * (maximum @n_excess fragments can be moved).
+		 */
 		for (i = n_excess - 1; i >= max(n_excess - tail_frags, 0); --i) {
 			f = &si->frags[MAX_SKB_FRAGS - n + i];
 			skb_shinfo(nskb)->frags[i] = *f;
@@ -339,9 +346,12 @@ __extend_pgfrags(struct sk_buff *skb_head, struct sk_buff *skb, int from, int n)
 		ss_skb_adjust_data_len(nskb, e_size);
 	}
 	/*
-	 * Make room for @n page fragments in the SKB. Considering maximum @n
-	 * value must be not greater than 2, the minimum @n_shift value must
-	 * be not less than -1.
+	 * Make room for @n page fragments in current SKB. We must shift
+	 * @tail_frags fragments inside current skb, except those, which we
+	 * moved to next/new skb (above); in case of too small @tail_frags
+	 * and/or too big @n values, the value of @n_shift will be negative,
+	 * but considering maximum @n value must be not greater than 2, the
+	 * minimum @n_shift value must be not less than -1.
 	 */
 	n_shift = tail_frags - n_excess;
 	BUG_ON(n_shift + 1 < 0);
