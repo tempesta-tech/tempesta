450537201b23ec999e07e27d2556086cffba4982
Ivan Koveshnikov <ik@tempesta-tech.com>
2019-03-11 18:31:35 +0500

    fix #1189: correctly fill header part of skb when writing data into paged fragments
    ss_skb_frag_next() is always work with TfwMsgIter, so renamed and moved
    into msg.h, the whole TfwMsgIter became invalid if the function fails.

--- tempesta_fw/http_msg.c
+++ tempesta_fw/http_msg.c
@@ -889,42 +889,6 @@ tfw_http_msg_setup(TfwHttpMsg *hm, TfwMsgIter *it, size_t data_len)
 }
 EXPORT_SYMBOL(tfw_http_msg_setup);
 
-static int
-__http_msg_add_data_frag(TfwMsgIter *it, TfwHttpMsg *hm, TfwStr *field,
-			 const TfwStr *data, unsigned int off)
-{
-	char *p;
-	skb_frag_t *frag = &skb_shinfo(it->skb)->frags[it->frag];
-	unsigned int f_size, d_size, f_room, n_copy;
-
-next_frag:
-	if (!frag)
-		return -E2BIG;
-
-	d_size = data->len - off;
-	f_size = skb_frag_size(frag);
-	f_room = PAGE_SIZE - frag->page_offset - f_size;
-	n_copy = min(d_size, f_room);
-	if (!n_copy)
-		return 0;
-
-	p = (char *)skb_frag_address(frag) + f_size;
-	memcpy_fast(p, data->data + off, n_copy);
-	skb_frag_size_add(frag, n_copy);
-	ss_skb_adjust_data_len(it->skb, n_copy);
-
-	if (__tfw_http_msg_add_str_data(hm, field, p, n_copy, it->skb))
-		return -ENOMEM;
-
-	if (d_size > f_room) {
-		frag = ss_skb_frag_next(&it->skb, it->skb_head, &it->frag);
-		off += n_copy;
-		goto next_frag;
-	}
-
-	return 0;
-}
-
 /**
  * Similar to tfw_msg_write(), but properly maintain @hm header
  * fields, so that @hm can be used in regular transformations. However,
@@ -935,34 +899,48 @@ int
 tfw_http_msg_add_data(TfwMsgIter *it, TfwHttpMsg *hm, TfwStr *field,
 		      const TfwStr *data)
 {
-	char *p;
-	unsigned int n_copy;
+	unsigned int c_off = 0, c_size;
 
-	WARN_ON_ONCE(TFW_STR_DUP(data));
-	WARN_ON_ONCE(!TFW_STR_PLAIN(data));
+	if (WARN_ON_ONCE(TFW_STR_DUP(data) || !TFW_STR_PLAIN(data)))
+		return -EINVAL;
+	if (WARN_ON_ONCE(it->frag >= skb_shinfo(it->skb)->nr_frags))
+		return -E2BIG;
 
-	n_copy = min(data->len, (unsigned long)skb_tailroom(it->skb));
-	if (!n_copy)
-		return 0;
+	while ((c_size = data->len - c_off)) {
+		char *p;
+		unsigned int n_copy, f_room;
 
-	if (it->frag >= 0)
-		goto add_frag;
+		if (it->frag >= 0) {
+			unsigned int f_size;
+			skb_frag_t *frag = &skb_shinfo(it->skb)->frags[it->frag];
 
-	p = skb_put(it->skb, n_copy);
-	memcpy_fast(p, data->data, n_copy);
+			f_size = skb_frag_size(frag);
+			f_room = PAGE_SIZE - frag->page_offset - f_size;
+			p = (char *)skb_frag_address(frag) + f_size;
+			n_copy = min(c_size, f_room);
+			skb_frag_size_add(frag, n_copy);
+			ss_skb_adjust_data_len(it->skb, n_copy);
+		} else {
+			f_room = skb_tailroom(it->skb);
+			n_copy = min(c_size, f_room);
+			p = skb_put(it->skb, n_copy);
+		}
 
-	if (__tfw_http_msg_add_str_data(hm, field, p, n_copy, it->skb))
-		return -ENOMEM;
+		memcpy_fast(p, data->data, n_copy);
+		if (__tfw_http_msg_add_str_data(hm, field, p, n_copy, it->skb))
+			return -ENOMEM;
+		c_off += n_copy;
 
-	if (data->len == n_copy) {
-		if (!skb_tailroom(it->skb))
-			it->frag = 0;
-		return 0;
+		if (c_size >= f_room) {
+			if (WARN_ON_ONCE(tfw_msg_iter_next_data_frag(it)
+					 && (c_size != f_room)))
+			{
+				return -E2BIG;
+			}
+		}
 	}
 
-add_frag:
-	it->frag = 0;
-	return __http_msg_add_data_frag(it, hm, field, data, n_copy);
+	return 0;
 }
 
 void
