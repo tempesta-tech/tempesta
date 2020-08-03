d47564513816d8fb0aa66ec34b55c5700898d37a
Alexander K <ak@tempesta-tech.com>
2020-08-03 21:32:27 +0300

    Optimize the FIPS 186-3 D.2 modular reduction using ideas from "Low-Latency Elliptic Curve Scalar Multiplication" by J.W.Bos.
    More tests on the modular reduction.

--- tls/bignum_asm.h
+++ tls/bignum_asm.h
@@ -53,7 +53,7 @@ void mpi_mul_x86_64_4(unsigned long *x, const unsigned long *a,
 		      const unsigned long *b);
 void mpi_sqr_x86_64_4(unsigned long *x, const unsigned long *a);
 
-void ecp_mod_p256_x86_64(unsigned long *x, size_t x_len);
+void ecp_mod_p256_x86_64(unsigned long *x);
 
 #endif /* __BIGNUM_ASM_H__ */
 
