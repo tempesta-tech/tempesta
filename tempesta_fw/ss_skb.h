549a70a055d3b3369a69feace5f57e174872dca5
Artyom Belov <avb@tempesta-tech.com>
2021-02-15 16:15:58 +0300

    fix review comments

--- tempesta_fw/ss_skb.h
+++ tempesta_fw/ss_skb.h
@@ -211,7 +211,7 @@ struct sk_buff *ss_skb_split(struct sk_buff *skb, int len);
 int ss_skb_get_room(struct sk_buff *skb_head, struct sk_buff *skb,
 		    char *pspt, unsigned int len, TfwStr *it);
 int ss_skb_get_room_w_frag(struct sk_buff *skb_head, struct sk_buff *skb,
-                           char *pspt, unsigned int len, TfwStr *it, int *fragn);
+			   char *pspt, unsigned int len, TfwStr *it, int *fragn);
 int ss_skb_expand_head_tail(struct sk_buff *skb_head, struct sk_buff *skb,
 			    size_t head, size_t tail);
 int ss_skb_chop_head_tail(struct sk_buff *skb_head, struct sk_buff *skb,
