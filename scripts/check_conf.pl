e611d43439f866a2ce9697852d3d5a44ed30b4bc
Aleksey Mikhaylov <98801830+ttaym@users.noreply.github.com>
2022-02-04 11:49:06 +0300

    Add runtime check for not present adx, may be false negative (#1556)

--- scripts/check_conf.pl
+++ scripts/check_conf.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 #
-# Copyright (C) 2020-2021 Tempesta Technologies, Inc.
+# Copyright (C) 2020-2022 Tempesta Technologies, Inc.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
@@ -18,12 +18,39 @@
 use 5.16.0;
 use strict;
 use warnings;
+require File::Temp;
+use File::Temp ();
 
 `modprobe msr`;
 
+sub rtrim { my $s = shift; $s =~ s/\s+$//; return $s };
+
 sub test {
     print "'$_[0]' bit: ",
-    (hex(`rdmsr $_[1]`) & (1 << $_[2]) ? "" : "NOT "), "found\n";
+    (hex(rtrim(`rdmsr $_[1]`)) & (1 << $_[2]) ? "" : "NOT "), "found\n";
+}
+
+sub adx_test {
+    my $adx_test_src = q|#include <stdio.h>
+int main() {
+  unsigned long long op1 = 0x1100110022002200;
+  unsigned long long op2 = 0x00ff11ee22dd33cc;
+  __asm__ __volatile__("adcx %%rbx,%%rax" : "=a"(op1) : "a"(op1), "b"(op2));
+  printf("%s\n", "ADX supported");
+}|;
+    my $tmp_src = File::Temp->new(SUFFIX => '.c');
+    print $tmp_src $adx_test_src;
+    my $tmp_exe_filename = $tmp_src->filename;
+    $tmp_exe_filename =~ s{\.[^.]*(?:\.c)?$}{};
+    my $exit_status = system("gcc", $tmp_src->filename, "-o", $tmp_exe_filename);
+    my $test_output = `$tmp_exe_filename`;
+    print "'Intel ADX Instruction Extensions' support: ",
+        index($test_output, "ADX supported") == -1 || $exit_status != 0 ?
+        "NOT " : "", "found\n";
+    if ($exit_status != 0) {
+        return;
+    }
+    unlink($tmp_exe_filename);
 }
 
 test 'Activate secondary controls', 0x482, 63;
@@ -32,7 +59,9 @@ test 'APIC-register virtualization', 0x48b, 40;
 test 'Virtual-interrupt delivery', 0x48b, 41;
 
 print "'Process posted interrupts' bit: ",
-    (((hex(`rdmsr 0x480`) & (1 << 55))
-      && (hex(`rdmsr 0x48d`) & (1 << 39)))
-     || (hex(`rdmsr 0x481`) & (1 << 39)))
+    (((hex(rtrim(`rdmsr 0x480`)) & (1 << 55))
+      && (hex(rtrim(`rdmsr 0x48d`)) & (1 << 39)))
+     || (hex(rtrim(`rdmsr 0x481`)) & (1 << 39)))
     ? "" : "NOT ", "found\n";
+
+adx_test;
