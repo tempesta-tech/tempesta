dd893ececabb2cef84cc14915c44129985a97c36
Alexander K <ak@tempesta-tech.com>
2022-03-24 23:18:14 +0300

    Add "OID" to warning messages to be more user friendly

--- tls/pkparse.c
+++ tls/pkparse.c
@@ -93,7 +93,8 @@ pk_use_ecparams(const ttls_asn1_buf *params, const TlsEcpGrp **grp)
 	}
 	if (ttls_oid_get_ec_grp(params, &grp_id)) {
 		TTLS_WITH_OID_FMT(params, ec_str,
-			T_WARN("PK - Elliptic curve %s is unsupported\n", ec_str));
+			T_WARN("PK - Elliptic curve with OID %s is unsupported\n",
+			       ec_str));
 		return -EINVAL;
 	}
 
