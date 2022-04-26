5fc692ce932168b161e5e5f9ae3ba96750bd1de4
Aleksey Filomafitskiy <af@tempesta-tech.com>
2022-04-26 18:32:11 +0300

    Updated copyright year to 2015-2022 and the module version to 0.2.0 (#1297)

--- fw/t/fuzzer.c
+++ fw/t/fuzzer.c
@@ -3,11 +3,11 @@
  *
  * Tempesta HTTP fuzzer.
  *
- * Copyright (C) 2015-2017 Tempesta Technologies, Inc.
+ * Copyright (C) 2015-2022 Tempesta Technologies, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License,
+ * the Free Software Foundation; either version 0.2.0 of the License,
  * or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
