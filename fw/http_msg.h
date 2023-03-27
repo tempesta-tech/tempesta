0c8e64e13714880cbe6c1668518d25d68cfcc7c9
Constantine <kt@tempesta-tech.com>
2023-03-27 17:50:39 +0300

    tfw_http_msg_del_flagged_body renamed to tfw_http_msg_cutoff_body_chunks

--- fw/http_msg.h
+++ fw/http_msg.h
@@ -160,7 +160,7 @@ int tfw_http_msg_hdr_xfrm(TfwHttpMsg *hm, char *name, size_t n_len,
 
 int tfw_http_msg_del_str(TfwHttpMsg *hm, TfwStr *str);
 int tfw_http_msg_del_hbh_hdrs(TfwHttpMsg *hm);
-int tfw_http_msg_del_flagged_body(TfwHttpMsg *hm);
+int tfw_http_msg_cutoff_body_chunks(TfwHttpMsg *hm);
 
 int tfw_http_msg_setup(TfwHttpMsg *hm, TfwMsgIter *it, size_t data_len,
 		       unsigned int tx_flags);
