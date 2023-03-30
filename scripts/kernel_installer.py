ba742ea07ca7b9ca2c0795d4a5e9a2ad11d99aa8
pale-emperor <beloglazmv@gmail.com>
2023-03-30 13:18:13 +0700

    Replace hardcoded version to KERNEL_VERSION var

--- scripts/kernel_installer.py
+++ scripts/kernel_installer.py
@@ -2,6 +2,8 @@
 
 import subprocess, requests, tarfile, shutil, os, glob, argparse, sys
 
+KERNEL_VERSION = '5.10.35'
+
 parser = argparse.ArgumentParser(description='Kernel install script for tempesta-fw')
 parser.add_argument('--reboot', type=bool, default=False,
                     help='Reboot flag. Reboot at finish if present')
@@ -34,7 +36,7 @@ tempesta_path = os.path.dirname(script_path)
 
 # Prereq: Check is right kernel already installed
 p_kern_hash = subprocess.Popen(
-    ["git log -n 1 --pretty=format:%H -- linux-5.10.35.patch"],
+    [f"git log -n 1 --pretty=format:%H -- linux-{KERNEL_VERSION}.patch"],
     stdout=subprocess.PIPE,
     shell=True,
     cwd=tempesta_path
@@ -59,7 +61,7 @@ else:
         # If present in repo - install from apt
         if len(packages.stdout.readlines()) >= 3:
             print('Kernel found, installing from repo')
-            with subprocess.Popen(["apt", "install", f"*5.10.35.tfw-{current_patch_hash}"],
+            with subprocess.Popen(["apt", "install", f"*{KERNEL_VERSION}.tfw-{current_patch_hash}"],
                           stdout=subprocess.PIPE) as p:
                 for line in p.stdout:
                     print(line, end='\n')
@@ -72,8 +74,8 @@ else:
             
 
             # Step 1: Download sources
-            print('Step 1: Downloading 5.10.35 sources...')
-            url = 'http://tempesta-vm.cloud.cherryservers.net:8081/repository/maven-releases/tempesta/linux-kernel-source/5.10.35/linux-kernel-source-5.10.35.tar.gz'
+            print(f'Step 1: Downloading {KERNEL_VERSION} sources...')
+            url = f'http://tempesta-vm.cloud.cherryservers.net:8081/repository/maven-releases/tempesta/linux-kernel-source/{KERNEL_VERSION}/linux-kernel-source-{KERNEL_VERSION}.tar.gz'
             r = requests.get(url, allow_redirects=True)
             open('download.tar.gz', 'wb').write(r.content)
             print('Done')
@@ -87,36 +89,36 @@ else:
 
             # Step 3: Apply EXTRAVERSION and patch
             print('Step 3: Patch...')
-            with open('linux-5.10.35/Makefile') as f:
+            with open(f'linux-{KERNEL_VERSION}/Makefile') as f:
                     data = f.read()
                     data = data.replace('EXTRAVERSION =', f'EXTRAVERSION = .tfw-{current_patch_hash}')
                     data = data.replace('NAME = Dare mighty things', f'NAME = Some test string')
-            with open(r'linux-5.10.35/Makefile', 'w') as file:
+            with open(f'linux-{KERNEL_VERSION}/Makefile', 'w') as file:
                 file.write(data)
-            shutil.copyfile(f'{tempesta_path}/linux-5.10.35.patch', 'linux-5.10.35/linux-5.10.35.patch')
-            with subprocess.Popen(["patch -p1 < linux-5.10.35.patch"],
+            shutil.copyfile(f'{tempesta_path}/linux-{KERNEL_VERSION}.patch', f'linux-{KERNEL_VERSION}/linux-{KERNEL_VERSION}.patch')
+            with subprocess.Popen([f"patch -p1 < linux-{KERNEL_VERSION}.patch"],
                                   stdout=subprocess.PIPE,
                                   shell=True,
-                                  cwd=f"linux-5.10.35") as p:
+                                  cwd=f"linux-{KERNEL_VERSION}") as p:
                 output, err = p.communicate()
                 patch_out = output.decode("utf-8").strip()
 
             # Step 4: config
             if os.path.exists('.config'):
                 print('Step 4: Predefined .config found...')
-                subprocess.Popen(['cp', '.config', 'linux-5.10.35/config'], stdout=subprocess.PIPE)
+                subprocess.Popen(['cp', '.config', f'linux-{KERNEL_VERSION}/config'], stdout=subprocess.PIPE)
             else:
                 print('Step 4: Use OS .config...')
                 subprocess.Popen(['cp', f'/boot/config-{os.uname().release}', '.config'],
                              stdout=subprocess.PIPE,
-                             cwd=f"linux-5.10.35")
+                             cwd=f"linux-{KERNEL_VERSION}")
             
             # Step 5: Build kernel
             print('Step 5: Make deb-pkg...')
             with subprocess.Popen(["make", "deb-pkg"],
                                   stdout=subprocess.PIPE,
                                   bufsize=1, universal_newlines=True,
-                                  cwd=f"linux-5.10.35") as p:
+                                  cwd=f"linux-{KERNEL_VERSION}") as p:
                 for line in p.stdout:
                     print(line, end='') # process line here
 
@@ -143,7 +145,7 @@ else:
                 if row.find('submenu') != -1:
                     submenu_id = [s for s in row.split() if 'gnulinux' in s][0].strip("'")
                 elif row.find(f'.tfw-{current_patch_hash}') != -1 and row.find('menuentry') != -1:
-                        kernel_id = [s for s in row.split() if f'linux-5.10.35.tfw-{current_patch_hash}' in s][0].strip("'")
+                        kernel_id = [s for s in row.split() if f'linux-{KERNEL_VERSION}.tfw-{current_patch_hash}' in s][0].strip("'")
 
         with open('/etc/default/grub', 'r') as file:
             data = file.read()
