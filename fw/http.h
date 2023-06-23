2cb4a1f909ce0053786eae40cb2ac24a21abd0b5
Constantine <kt@tempesta-tech.com>
2023-06-23 16:26:38 +0300

    Rework HTTP1 response adjusting
    The main goal of this patch is get rid off skb fragmentation that
    cause errors during message adjusting. Because `TfwStr` stores pointer
    to skb that owns data we have problems when that data moves to another
    skb, it happens when we add data to highly fragmented skb and last
    fragments move to another skb, that breaks `skb` field of `TfwStr`
    which stores pointer to moved data.
    
    I have decided to mirgate to approach that same with used in HTTP2
    message transformation. Unlink skbs that contains headers, except the
    last one skb, this skb bocomes new response `head`, then copy
    necessery headers to `head` using `TfwPool`. During copy adjust
    headers.
    
    Response for PURGE request:
    
    Now we just copy response headers to new skb
    skipping response body if it exists, otherwise
    we just remove headers from current skb using
    `tfw_http_msg_cutoff_headers()` and use it as skb_head.
    
    This approach used because
    `tfw_http_msg_cutoff_headers()` already is complex
    adding more logic to this function overcomplicate
    its. Also this function used for both HTTP1 and HTTP2
    protocols if we add logic related to dropping the body
    of "PURGE responses" it will make an overhead for
    each HTTP2 response.

--- fw/http.h
+++ fw/http.h
@@ -488,8 +488,7 @@ struct tfw_http_resp_t {
 };
 
 /**
- * Represents the data that should be cleaned up after HTTP1 -> HTTP2 response
- * transformation.
+ * Represents the data that should be cleaned up after message transformation.
  *
  * @skb_head	- head of skb list that must be freed;
  * @pages	- pages that must be freed;
@@ -499,7 +498,7 @@ typedef struct {
 	struct sk_buff *skb_head;
 	struct page *pages[MAX_SKB_FRAGS];
 	unsigned char pages_sz;
-} TfwHttpRespCleanup;
+} TfwHttpMsgCleanup;
 
 #define TFW_HDR_MAP_INIT_CNT		32
 #define TFW_HDR_MAP_SZ(cnt)		(sizeof(TfwHttpHdrMap)		\
@@ -739,6 +738,7 @@ int tfw_http_expand_stale_warn(TfwHttpResp *resp);
 int tfw_http_expand_hdr_date(TfwHttpResp *resp);
 int tfw_http_expand_hbh(TfwHttpResp *resp, unsigned short status);
 int tfw_http_expand_hdr_via(TfwHttpResp *resp);
+int tfw_http_expand_hdr_server(TfwHttpResp *resp);
 void tfw_h2_resp_fwd(TfwHttpResp *resp);
 int tfw_h2_hdr_map(TfwHttpResp *resp, const TfwStr *hdr, unsigned int id);
 int tfw_h2_add_hdr_date(TfwHttpResp *resp, bool cache, unsigned int stream_id);
