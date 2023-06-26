8866dfa13c350ec390d9e588b7e97f02a3501d0a
EvgeniiMekhanik <em@tempesta-tech.com>
2023-06-26 14:52:20 +0300

    Fix according review

--- lib/errinj.c
+++ lib/errinj.c
@@ -1,13 +1,14 @@
 /**
- *		Tempesta FW
+ *              Tempesta FW
  *
  * Error injection library.
  *
- * Application protocol handler layers must implement zero data copy logic
- * on top on native Linux socket buffers. The helpers provide common and
- * convenient wrappers for skb processing.
+ * Copyright (C) 2023 Tempesta Technologies, Inc.
  *
- * Copyright (C) 2015-2023 Tempesta Technologies, Inc.
+ * Error injection library is used to check handling various
+ * situations that cannot be reproduced by traditional python
+ * tests. For example we may change return code of some Linux
+ * kernel functions and so on.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
@@ -30,75 +31,78 @@
 
 #include "errinj.h"
 
-#define ERRINJ_MEMBER(n, t, s) { /* .name = */ #n, /* .type = */ t, /* .state = */ s },
+#define ERRINJ_MEMBER(n, t, s) {					\
+	/* .name = */ #n,						\
+	/* .type = */ t,						\
+	/* .state = */ s						\
+},
 
 struct errinj errinjs[errinj_id_MAX] = {
-        ERRINJ_LIST(ERRINJ_MEMBER)
+	ERRINJ_LIST(ERRINJ_MEMBER)
 };
 EXPORT_SYMBOL(errinjs);
-
 
 int
 errinj_split_name_val(char *input, char **name, char **val)
 {
-        char *sep = strchr(input, '=');
-        if (!sep)
-                return -EINVAL;
+	char *sep = strchr(input, '=');
+	if (!sep)
+		return -EINVAL;
 
-        *sep = '\0';
-        *name = input;
-        *val = (sep + 1);
-        return 0;
+	*sep = '\0';
+	*name = input;
+	*val = (sep + 1);
+	return 0;
 }
 EXPORT_SYMBOL(errinj_split_name_val);
 
 struct errinj *
 errinj_by_name(const char *name)
 {
-        enum errinj_id i;
+	enum errinj_id i;
 
-        for (i = 0 ; i < errinj_id_MAX ; i++) {
-                if (!strcmp(errinjs[i].name, name))
-                        return &errinjs[i];
-        }
-        return NULL;
+	for (i = 0 ; i < errinj_id_MAX ; i++) {
+		if (!strcmp(errinjs[i].name, name))
+			return &errinjs[i];
+	}
+	return NULL;
 }
 EXPORT_SYMBOL(errinj_by_name);
 
 void
 errinj_to_str(const struct errinj *inj, char *buf, size_t buf_size)
 {
-        switch (inj->type) {
-        case ERRINJ_BOOL:
-                snprintf(buf, buf_size, "%s=%s",
-                         inj->name, inj->bparam ? "true" : "false");
-                break;
-        case ERRINJ_LONG:
-                snprintf(buf, buf_size, "\"%s\" = %ld",
-                         inj->name, inj->lparam);
-                break;
-        default:
-                BUG(); 
-        }
+	switch (inj->type) {
+	case ERRINJ_BOOL:
+		snprintf(buf, buf_size, "%s=%s",
+			 inj->name, inj->bparam ? "true" : "false");
+		break;
+	case ERRINJ_LONG:
+		snprintf(buf, buf_size, "\"%s\" = %ld",
+			 inj->name, inj->lparam);
+		break;
+	default:
+		BUG(); 
+	}
 }
 EXPORT_SYMBOL(errinj_to_str);
 
 int
 str_to_errinj(struct errinj *inj, const char *buf)
 {
-        int r = -EINVAL;
+	int r = -EINVAL;
 
-        switch (inj->type) {
-        case ERRINJ_BOOL:
-                r = kstrtobool(buf, &inj->bparam);
-                break;
-        case ERRINJ_LONG:
-                r = kstrtol(buf, 10, &inj->lparam);
-                break;
-        default:
-                BUG();
-        }
+	switch (inj->type) {
+	case ERRINJ_BOOL:
+		r = kstrtobool(buf, &inj->bparam);
+		break;
+	case ERRINJ_LONG:
+		r = kstrtol(buf, 10, &inj->lparam);
+		break;
+	default:
+		BUG();
+	}
 
-        return r;
+	return r;
 }
 EXPORT_SYMBOL(str_to_errinj);
