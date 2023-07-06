06c1a67eb6f5495af2c54de23c824824c9c825b3
pale-emperor <46090986+pale-emperor@users.noreply.github.com>
2023-07-06 20:45:45 +0300

    Update tempesta_installer.sh right way

--- pkg/scripts/tempesta_installer.sh
+++ pkg/scripts/tempesta_installer.sh
@@ -57,9 +57,9 @@ usage()
 # our needs.
 tfw_download()
 {
-        release_tag=`curl -s https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO_LINUX/tags | grep \"$DISTRO/ | cut -d '"' -f 4 | sort -V -r | head -n1`
+        release_tag=`curl -s https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO_TEMPESTA/tags | grep \"$DISTRO/ | cut -d '"' -f 4 | sort -V -r | head -n1`
 	if [[ ! "$release_tag" ]]; then
-		echo "Can't find latest release in repo: https://github.com/$GITHUB_USER/$GITHUB_REPO_LINUX"
+		echo "Can't find latest release in repo: https://github.com/$GITHUB_USER/$GITHUB_REPO_TEMPESTA"
 		#TODO: show next line only if received 403 status code.
 		echo "Or may be Github API rate limit exceeded."
 		exit 2
