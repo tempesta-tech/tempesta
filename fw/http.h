b58f0d12c6dc24049e26b25e967a94bc99b8d50c
Constantine <kt@tempesta-tech.com>
2023-08-09 14:54:58 +0300

    Rework HTTP1 request adjusting (Part 1/2)
    Same as response patch.
    
    `TfwHttpMsgCleanup *cleanup` added to TfwHttpReq to have ability
    to cleanup request on destruction. In future `old_head` must be
    replaced with `cleanup`.

--- fw/http.h
+++ fw/http.h
@@ -336,6 +336,19 @@ typedef struct {
 } TfwHttpCond;
 
 /**
+ * Represents the data that should be cleaned up after message transformation.
+ *
+ * @skb_head	- head of skb list that must be freed;
+ * @pages	- pages that must be freed;
+ * @pages_sz	- current number of @pages;
+ */
+typedef struct {
+	struct sk_buff *skb_head;
+	struct page *pages[MAX_SKB_FRAGS];
+	unsigned char pages_sz;
+} TfwHttpMsgCleanup;
+
+/**
  * HTTP Request.
  *
  * @vhost	- virtual host for the request;
@@ -345,6 +358,8 @@ typedef struct {
  *		  hop-by-hop peer (TfwConnection->peer) and end-to-end peer are
  *		  the same;
  * @old_head	- Original request head. Required for keep request data until
+ * 		  the response is sent to the client;
+ * @cleanup	- Original request data. Required for keep request data until
  * 		  the response is sent to the client;
  * @pit		- iterator for tracking transformed data allocation (applicable
  *		  for HTTP/2 mode only);
@@ -379,6 +394,7 @@ struct tfw_http_req_t {
 	TfwHttpSess		*sess;
 	TfwClient		*peer;
 	struct sk_buff		*old_head;
+	TfwHttpMsgCleanup	*cleanup;
 	TfwHttpCond		cond;
 	TfwMsgParseIter		pit;
 	TfwStr			userinfo;
@@ -486,19 +502,6 @@ struct tfw_http_resp_t {
 	TfwStr			cut;
 	int			trailers_len;
 };
-
-/**
- * Represents the data that should be cleaned up after message transformation.
- *
- * @skb_head	- head of skb list that must be freed;
- * @pages	- pages that must be freed;
- * @pages_sz	- current number of @pages;
- */
-typedef struct {
-	struct sk_buff *skb_head;
-	struct page *pages[MAX_SKB_FRAGS];
-	unsigned char pages_sz;
-} TfwHttpMsgCleanup;
 
 #define TFW_HDR_MAP_INIT_CNT		32
 #define TFW_HDR_MAP_SZ(cnt)		(sizeof(TfwHttpHdrMap)		\
@@ -733,7 +736,8 @@ void tfw_http_resp_fwd(TfwHttpResp *resp);
 void tfw_http_resp_build_error(TfwHttpReq *req);
 int tfw_cfgop_parse_http_status(const char *status, int *out);
 void tfw_http_hm_srv_send(TfwServer *srv, char *data, unsigned long len);
-int tfw_h1_set_loc_hdrs(TfwHttpMsg *hm, bool is_resp, bool from_cache);
+int tfw_h1_add_loc_hdrs(TfwHttpMsg *hm, const TfwHdrMods *h_mods,
+			bool from_cache);
 int tfw_http_expand_stale_warn(TfwHttpResp *resp);
 int tfw_http_expand_hdr_date(TfwHttpResp *resp);
 int tfw_http_expand_hbh(TfwHttpResp *resp, unsigned short status);
@@ -773,5 +777,33 @@ void tfw_http_extract_request_authority(TfwHttpReq *req);
 bool tfw_http_mark_is_in_whitlist(unsigned int mark);
 char *tfw_http_resp_status_line(int status, size_t *len);
 int tfw_http_on_send_resp(void *conn, struct sk_buff **skb_head);
+
+static inline const BasicStr *
+tfw_http_method_id2str(int id)
+{
+#define STR_METHOD(name) [TFW_HTTP_METH_ ## name] = { #name, sizeof(#name) - 1 }
+
+	static BasicStr http_methods[] = {
+		STR_METHOD(COPY),
+		STR_METHOD(DELETE),
+		STR_METHOD(GET),
+		STR_METHOD(HEAD),
+		STR_METHOD(LOCK),
+		STR_METHOD(MKCOL),
+		STR_METHOD(MOVE),
+		STR_METHOD(OPTIONS),
+		STR_METHOD(PATCH),
+		STR_METHOD(POST),
+		STR_METHOD(PROPFIND),
+		STR_METHOD(PROPPATCH),
+		STR_METHOD(PUT),
+		STR_METHOD(TRACE),
+		STR_METHOD(UNLOCK),
+		STR_METHOD(PURGE),
+	};
+
+#undef STR_METHOD
+	return &http_methods[id];
+}
 
 #endif /* __TFW_HTTP_H__ */
