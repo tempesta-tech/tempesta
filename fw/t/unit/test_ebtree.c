d19714b511b02bbc15513b3e241275377103d6a2
EvgeniiMekhanik <em@tempesta-tech.com>
2023-09-29 18:53:10 +0300

    Remove unnecessary functions from ebtree
    We dont need all functions from ported ebtree.

--- fw/t/unit/test_ebtree.c
+++ fw/t/unit/test_ebtree.c
@@ -4,10 +4,10 @@
 #define EB64_NODES_MAX 1000
 static struct eb64_node nodes[EB64_NODES_MAX];
 
-static long int
+static unsigned long
 find_min_key(struct eb64_node *nodes, int size)
 {
-	long int min = nodes[0].key;
+	unsigned long min = nodes[0].key;
 	unsigned int i;
 	
 	for (i = 1; i < size; i++) {
@@ -22,12 +22,12 @@ TEST(ebtree, extract_min)
 {
 	struct eb_root tree = EB_ROOT;
 	struct eb64_node *root;
-	long int min;
+	unsigned long min;
 	unsigned int i;
 
 	for (i = 0; i < EB64_NODES_MAX; i++) {
-		nodes[i].key = get_random_int();
-		eb64i_insert(&tree, &nodes[i]);
+		nodes[i].key = get_random_long();
+		eb64_insert(&tree, &nodes[i]);
 	}
 
 	for (i = 0; i < EB64_NODES_MAX; i++) {
@@ -39,8 +39,8 @@ TEST(ebtree, extract_min)
 		root = eb64_first(&tree);
 		EXPECT_EQ(root->key, min);
 		eb64_delete(root);
-		root->key = get_random_int();
-		eb64i_insert(&tree, root);
+		root->key = get_random_long();
+		eb64_insert(&tree, root);
 	}
 }
 
