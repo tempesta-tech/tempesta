af9cb0bd0c7111a905f5e87b9cc6cd2c30986eb3
Constantine <kt@tempesta-tech.com>
2024-01-04 21:00:33 +0200

    Update copyright year

--- fw/http.c
+++ fw/http.c
@@ -66,7 +66,7 @@
  * created HTTP/1.1-message.
  *
  * Copyright (C) 2014 NatSys Lab. (info@natsys-lab.com).
- * Copyright (C) 2015-2023 Tempesta Technologies, Inc.
+ * Copyright (C) 2015-2024 Tempesta Technologies, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
