49af5af5207bb2e22ece346b846023a219539d51
kingluo <home_king@163.com>
2024-07-04 14:58:44 +0800

    handle SKBFL_SHARED_FRAG in flags, not tx_flags

--- fw/http_msg.c
+++ fw/http_msg.c
@@ -1237,11 +1237,11 @@ tfw_http_msg_hdr_add(TfwHttpMsg *hm, const TfwStr *hdr)
  */
 int
 tfw_http_msg_setup(TfwHttpMsg *hm, TfwMsgIter *it, size_t data_len,
-		   unsigned int tx_flags)
+		   unsigned int flags)
 {
 	int r;
 
-	if ((r = tfw_msg_iter_setup(it, &hm->msg.skb_head, data_len, tx_flags)))
+	if ((r = tfw_msg_iter_setup(it, &hm->msg.skb_head, data_len, flags)))
 		return r;
 	T_DBG2("Set up HTTP message %pK with %lu bytes data\n", hm, data_len);
 
@@ -1620,7 +1620,7 @@ tfw_http_msg_linear_transform(TfwMsgIter *it)
 		if (!nskb)
 			return -ENOMEM;
 
-		skb_shinfo(nskb)->tx_flags = skb_shinfo(it->skb)->tx_flags;
+		skb_shinfo(nskb)->flags = skb_shinfo(it->skb)->flags;
 		ss_skb_insert_before(&it->skb_head, it->skb, nskb);
 		it->skb = nskb;
 		it->frag = -1;
@@ -1691,8 +1691,8 @@ __tfw_http_msg_expand_from_pool(TfwHttpResp *resp, const TfwStr *str,
 					}
 				}
 
-				skb_shinfo(nskb)->tx_flags =
-					skb_shinfo(it->skb)->tx_flags;
+				skb_shinfo(nskb)->flags =
+					skb_shinfo(it->skb)->flags;
 				ss_skb_insert_after(it->skb, nskb);
 				/*
 				 * If body is located in the zero fragment and
