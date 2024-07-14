09bd329d76bf16b11ada8a033b585e28f04d0d3d
kingluo <home_king@163.com>
2024-07-14 23:48:11 +0800

    Remove `kernel_fpu_begin()` added by mistake

--- fw/apm.c
+++ fw/apm.c
@@ -1018,8 +1018,6 @@ tfw_apm_prcntl_tmfn(struct timer_list *t)
 	TfwApmRBuf *rbuf = &data->rbuf;
 	TfwApmRBEnt *rbent = rbuf->rbent;
 
-	kernel_fpu_begin();
-
 	/*
 	 * Increment the counter and make the updates use the other array
 	 * of the two that are available. In the meanwhile, use the array
@@ -1058,8 +1056,6 @@ tfw_apm_hm_timer_cb(struct timer_list *t)
 	TfwServer *srv = hmctl->srv;
 	TfwApmHM *hm = READ_ONCE(hmctl->hm);
 	unsigned long now;
-
-	kernel_fpu_begin();
 
 	BUG_ON(!hm);
 	if (!atomic64_read(&hmctl->rcount))
