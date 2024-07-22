94f1587c6f37819d66d3094bd74b7e26b5296128
kingluo <home_king@163.com>
2024-07-22 15:36:39 +0800

    send postpoined frames only if no trailers or trailers sent

--- fw/http_frame.c
+++ fw/http_frame.c
@@ -2120,6 +2120,7 @@ do {									\
 			return r;
 		}
 
+		printk("---> HTTP2_MAKE_TRAILER_FRAMES\n");
 		T_FSM_JMP(HTTP2_SEND_FRAMES);
 	}
 
@@ -2132,6 +2133,7 @@ do {									\
 			return r;
 		}
 
+		printk("---> HTTP2_MAKE_TRAILER_CONTINUATION_FRAMES\n");
 		T_FSM_JMP(HTTP2_SEND_FRAMES);
 	}
 
@@ -2148,7 +2150,8 @@ do {									\
 			T_FSM_JMP(HTTP2_MAKE_CONTINUATION_FRAMES);
 		} else {
 			if (unlikely(stream->xmit.postponed) &&
-			    !stream->xmit.frame_length)
+			    !stream->xmit.frame_length &&
+			    !stream->xmit.t_len)
 				ss_skb_tcp_entail_list(sk,
 						       &stream->xmit.postponed);
 			if (stream->xmit.b_len) {
