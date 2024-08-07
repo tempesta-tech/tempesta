53c7b433552c4e6269b7ce96188c1c4635f2cb9e
kingluo <home_king@163.com>
2024-08-08 02:06:23 +0800

    fix tfw_sock_clnt_drop

--- fw/sock_clnt.c
+++ fw/sock_clnt.c
@@ -317,7 +317,8 @@ static void
 tfw_sock_clnt_drop(struct sock *sk)
 {
 	TfwConn *conn = sk->sk_user_data;
-	{
+
+	if (TFW_CONN_PROTO(conn) == TFW_FSM_H2) {
 		TfwH2Ctx *ctx = tfw_h2_context_safe(conn);
 		WARN_ONCE(ctx && atomic_read(&ctx->hpack.inuse),
 			  "---> free req when hpack is in use, hpack->state=%d",
