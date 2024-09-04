57a95d04c6560c58feffbcbb3adceaf34839c816
EvgeniiMekhanik <em@tempesta-tech.com>
2024-09-04 19:45:12 +0300

    Update copyright year

--- fw/tls.h
+++ fw/tls.h
@@ -1,7 +1,7 @@
 /**
  *		Tempesta FW
  *
- * Copyright (C) 2015-2022 Tempesta Technologies, Inc.
+ * Copyright (C) 2015-2024 Tempesta Technologies, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
