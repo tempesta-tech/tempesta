7a15ac1e6423cb5a0a9e1b59aabee20c2150cfcb
EvgeniiMekhanik <em@tempesta-tech.com>
2024-12-12 19:05:57 +0200

    Fix according review
    - Move all ja5 constants to separate header file.
    - Change order of fields according to task description.
    - Add unit tests.
    - Move all ja5h cookie calculations, to handle cases
      when there are several cookie values in one header.
    - Handle case when cookie or refer header restored
      from hpack table.

---
+++ lib/ja5.h
@@ -0,0 +1,76 @@
+/**
+ *		Tempesta FW
+ *
+ * Copyright (C) 2024 Tempesta Technologies, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+#ifndef __LIB_JA5_H__
+#define __LIB_JA5_H__
+
+/**
+ * Different constants for HTTP ja5 hash calculation.
+ */
+enum {
+	TFW_HTTP_JA5H_HTTP_REQ = 0,
+	TFW_HTTP_JA5H_HTTP2_REQ = 1,
+	TFW_JA5_HASH_CALC_PRIME = 11,
+	TFW_HTTP_JA5H_COOKIE_MAX = 31,
+	TFW_HTTP_JA5H_HEADERS_MAX = 63
+};
+
+/**
+ * Description of HTTP ja5 hash structure.
+ * @padding	- padding up to 3 bytes;
+ * @has_referer	- has Refer;
+ * @headers_num	- number of headers (all bits set for 63
+ *		  and more headers);
+ * @cookie_num 	- number of Cookie values (all bits set for 31
+ *		  and more cookies, within one or several headers);
+ * @method	- HTTP method (tfw_http_meth_t value)
+ * @version	- version (http1 or http2);
+ * @summ	- sum * 11 + header, where header is an 'id'
+ *		  if 'id' < TFW_HTTP_HDR_RAW or 4 or less bytes
+ *		  (depending on how many bytes available) value
+ *		  (header name or header name + value if name is
+ *		  less then 4 bytes);
+ */
+typedef struct {
+	unsigned int	padding:6;
+	unsigned int	has_referer:1;
+	unsigned int	headers_num:6;
+	unsigned int	cookie_num:5;
+	unsigned int	method:5;
+	unsigned int	version:1;
+	unsigned int	summ;
+} __packed HttpJa5h;
+
+#define HTTP_JA5H_CALC_NUM(val, max, num)				\
+	({								\
+		typeof(max) x = (val) < (max) ? (val) + (num) : (val);	\
+		x;							\
+	})
+
+#define HTTP_JA5H_REQ_CALC_NUM(req, name, max, num)			\
+	(req)->ja5h.name##_num =					\
+		HTTP_JA5H_CALC_NUM(((req)->ja5h.name##_num), max, num)
+
+#define COMPUTE_JA5_ACCHASH(hash, field)	\
+do {						\
+	(hash) *= TFW_JA5_HASH_CALC_PRIME;	\
+	(hash) += (field);			\
+} while (0)
+
+#endif /* __LIB_JA5_H__ */
