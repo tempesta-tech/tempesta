e5990e3b9d19d7535f80a83bf9a87e073c7d4739
Constantine <kt@tempesta-tech.com>
2025-01-22 14:50:17 +0200

    Safe request cleanup
    Free cleanup structure in safe way, to prevent
    use-after-free. We free clean structure immediately
    after error to get free memory to create the response.

--- fw/http.c
+++ fw/http.c
@@ -1789,7 +1789,7 @@ do {									\
 }
 
 static void
-__tfw_http_msg_cleanup(TfwHttpMsgCleanup *cleanup)
+__tfw_http_free_cleanup(TfwHttpMsgCleanup *cleanup)
 {
 	int i;
 	struct sk_buff *skb;
@@ -1799,6 +1799,15 @@ __tfw_http_msg_cleanup(TfwHttpMsgCleanup *cleanup)
 
 	for (i = 0; i < cleanup->pages_sz; i++)
 		put_page(cleanup->pages[i]);
+}
+
+static void
+__tfw_http_req_cleanup(TfwHttpReq *req)
+{
+	if (!req->cleanup)
+		return;
+	__tfw_http_free_cleanup(req->cleanup);
+	req->cleanup = NULL;
 }
 
 /**
@@ -2749,8 +2758,7 @@ tfw_http_req_destruct(void *msg)
 	if (req->stale_ce)
 		tfw_cache_put_entry(req->node, req->stale_ce);
 
-	if (req->cleanup)
-		__tfw_http_msg_cleanup(req->cleanup);
+	__tfw_http_req_cleanup(req);
 }
 
 /**
@@ -3804,7 +3812,9 @@ tfw_h1_adjust_req(TfwHttpReq *req)
 	return r;
 
 clean:
-	__tfw_http_msg_cleanup(req->cleanup);
+	T_DBG("%s: req [%p] adjusting has failed with code %i\n", __func__, req,
+	      r);
+	__tfw_http_req_cleanup(req);
 
 	return r;
 }
@@ -4403,7 +4413,7 @@ tfw_h2_adjust_req(TfwHttpReq *req)
 
 err:
 	ss_skb_queue_purge(&new_head);
-	__tfw_http_msg_cleanup(req->cleanup);
+	__tfw_http_req_cleanup(req);
 	T_DBG3("%s: req [%p] convertation to http1.1 has failed"
 	       " with result (%d)\n", __func__, req, r);
 	return -EINVAL;
@@ -4586,7 +4596,7 @@ tfw_http_adjust_resp(TfwHttpResp *resp)
 	r = tfw_http_msg_expand_from_pool(hm, &crlf);
 
 clean:
-	__tfw_http_msg_cleanup(&cleanup);
+	__tfw_http_free_cleanup(&cleanup);
 
 	return r;
 }
@@ -5768,11 +5778,11 @@ tfw_h2_resp_encode_headers(TfwHttpResp *resp)
 	       req, resp);
 	SS_SKB_QUEUE_DUMP(&resp->msg.skb_head);
 
-	__tfw_http_msg_cleanup(&cleanup);
+	__tfw_http_free_cleanup(&cleanup);
 	return 0;
 
 clean:
-	__tfw_http_msg_cleanup(&cleanup);
+	__tfw_http_free_cleanup(&cleanup);
 	return r;
 }
 
