cf676d9b9d8ff2a98a2f781c4bdf03d6a8f42146
Constantine <kt@tempesta-tech.com>
2025-01-22 15:17:27 +0200

    Update copyright year

--- fw/http_sess.c
+++ fw/http_sess.c
@@ -29,7 +29,7 @@
  * JS challenge client should execute it and send new request with
  * appropriate cookie just in time.
  *
- * Copyright (C) 2015-2024 Tempesta Technologies, Inc.
+ * Copyright (C) 2015-2025 Tempesta Technologies, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
