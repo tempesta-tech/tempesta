e720d7db25366e048dafe987489858a1435411e8
Constantine <kt@tempesta-tech.com>
2025-02-03 14:54:06 +0200

    Add missed semicolon

--- fw/cache.c
+++ fw/cache.c
@@ -3229,7 +3229,7 @@ tfw_cache_build_resp_stale(TfwHttpReq *req)
 
 	return resp;
 }
-ALLOW_ERROR_INJECTION(tfw_cache_build_resp_stale, NULL)
+ALLOW_ERROR_INJECTION(tfw_cache_build_resp_stale, NULL);
 
 /**
  * Release cache entry reference.
