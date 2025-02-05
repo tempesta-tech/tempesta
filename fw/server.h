12d9689cefba86184e53311675e3eba71bb79e59
Constantine <kt@tempesta-tech.com>
2025-02-05 17:38:59 +0200

    Flexible arrays and small fixes
     -tfw_ctlfn_state_io - Changed prototype to match new
      prototype of proc_handler.
     -tfw_sysctl_tbl removed empty struct.
     -Flexible arrays declared using `DECLARE_FLEX_ARRAY`.

--- fw/server.h
+++ fw/server.h
@@ -2,7 +2,7 @@
  *		Tempesta FW
  *
  * Copyright (C) 2014 NatSys Lab. (info@natsys-lab.com).
- * Copyright (C) 2015-2024 Tempesta Technologies, Inc.
+ * Copyright (C) 2015-2025 Tempesta Technologies, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
@@ -122,7 +122,7 @@ struct tfw_srv_group_t {
 	unsigned int		max_recns;
 	unsigned int		flags;
 	unsigned int		nlen;
-	char			name[0];
+	DECLARE_FLEX_ARRAY(char, name);
 };
 
 /**
