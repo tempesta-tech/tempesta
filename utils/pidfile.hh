485a6d5cc76237614afecceed39a50a88893c83f
MorganaFuture <andrewmochalskyi@gmail.com>
2025-05-26 09:49:50 +0000

    refactor: tfw_logger
    A dedicated TfwLoggerConfig class for logger settings
    JSON configuration file support via Boost's property_tree
    Two operation modes: daemon (background) and handle (foreground for debugging)
    Support for a logger_config attribute in the access_log directive
    Updates to Tempesta scripts for compatibility
    Backward compatibility with existing configuration
    Add tests

---
+++ utils/pidfile.hh
@@ -0,0 +1,60 @@
+/**
+ *		Tempesta FW
+ *
+ * Copyright (C) 2025 Tempesta Technologies, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#pragma once
+
+#include <string>
+
+/**
+ * Check if daemon is already running by examining PID file.
+ *
+ * @param fname Path to PID file
+ * @return 0 if no daemon is running, -1 on error
+ * @throws Exception if daemon is already running
+ */
+int
+pidfile_check(const std::string &fname);
+
+/**
+ * Create and lock PID file.
+ *
+ * @param fname Path to PID file
+ * @return File descriptor of locked PID file, -1 on error
+ */
+int
+pidfile_create(const std::string &fname);
+
+/**
+ * Remove PID file and release lock.
+ *
+ * @param fname Path to PID file
+ * @param fd File descriptor of PID file
+ */
+void
+pidfile_remove(const std::string &fname, int fd);
+
+/**
+ * Stop daemon by reading PID from file and sending SIGTERM.
+ *
+ * @param fname Path to PID file
+ * @throws Exception if PID file not found or daemon stop failed
+ */
+void
+pidfile_stop_daemon(const std::string &fname);
