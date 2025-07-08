a9621db98e4166def320cd0e9197fd960e85d8a7
EvgeniiMekhanik <em@tempesta-tech.com>
2025-07-08 13:51:38 +0300

    Drop connetion with TCP RST if client mem is exceeded
    If administrator specify `client_mem` and the memory
    used by all connection of current client exceeded this
    value Tempesta FW drops connection and block client
    by ip if `ip_block on;` is specified.

--- fw/t/unit/helpers.c
+++ fw/t/unit/helpers.c
@@ -33,7 +33,6 @@
  */
 #include "helpers.h"
 #include "http_msg.h"
-#include "helpers.h"
 
 #include "pool.c"
 #include "apm.h"
@@ -435,6 +434,12 @@ frang_req_is_whitelisted(TfwHttpReq *req)
 
 int
 frang_http_hdr_limit(TfwHttpReq *req, unsigned int new_hdr_len)
+{
+	return T_OK;
+}
+
+int
+frang_client_mem_limit(TfwCliConn *conn, bool block_if_exceeded)
 {
 	return T_OK;
 }
