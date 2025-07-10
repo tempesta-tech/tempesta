b5ef2f26517223773497dde6ec81bbcf7a708d60
Constantine <kt@tempesta-tech.com>
2025-07-10 15:15:43 +0300

    feat: Add RNG API to lib module
    The main difference from the kernel RBG Tempesta's RNG
    lib uses arch specific random generator primarly, only
    If arch RNG is not able to retunr to the caller enough
    bytes or arch RNG is not available software RNG will be
    used instead.
    
    All calls to the kernel API replaced by this API.

--- tls/tls_internal.h
+++ tls/tls_internal.h
@@ -27,10 +27,10 @@
 
 #include <linux/types.h>
 #include <asm/fpu/api.h>
-#include <asm/archrandom.h>
 
 #include "lib/fsm.h"
 #include "lib/str.h"
+#include "lib/random.h"
 
 #include "crypto.h"
 #include "ttls.h"
@@ -404,56 +404,10 @@ ttls_rnd(void *buf, size_t len)
 
 #else
 
-/*
- * This function borrowed from 5.10.35 kernel.
- *
- * This function will use the architecture-specific hardware random
- * number generator if it is available.  The arch-specific hw RNG will
- * almost certainly be faster than what we can do in software, but it
- * is impossible to verify that it is implemented securely (as
- * opposed, to, say, the AES encryption of a sequence number using a
- * key known by the NSA).  So it's useful if we need the speed, but
- * only if we're willing to trust the hardware manufacturer not to
- * have put in a back door.
- *
- * Return number of bytes filled in.
- */
-static inline int
-get_random_bytes_arch(void *buf, int nbytes)
-{
-	int left = nbytes;
-	char *p = buf;
-
-	while (left) {
-		unsigned long v;
-		int chunk = min_t(int, left, sizeof(unsigned long));
-
-		if (!arch_get_random_longs(&v, 1))
-			break;
-
-		memcpy(p, &v, chunk);
-		p += chunk;
-		left -= chunk;
-	}
-
-	return nbytes - left;
-}
-
-/*
- * CPUs since Intel Ice Lake are safe against SRBDS attack, so we're good
- * with the hardware random generator.
- *
- * The random number generator is extremely important for ECDSA, see
- * M.Macchetti, "A Novel Related Nonce Attack for ECDSA", 2023,
- * https://eprint.iacr.org/2023/305.pdf
- */
 static inline void
 ttls_rnd(void *buf, int len)
 {
-	int n = get_random_bytes_arch(buf, len);
-
-	if (unlikely(n < len))
-		get_random_bytes((char *)buf + n, len - n);
+	tfw_get_random_bytes(buf, len);
 }
 #endif
 
