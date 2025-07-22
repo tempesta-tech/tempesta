1a51b05cc46ab8247da55781c056f800fc9a30f2
symstu <stukalo.maksym@gmail.com>
2025-07-22 14:10:42 +0200

    fixed logger

--- scripts/ddos_mitigation/logger.py
+++ scripts/ddos_mitigation/logger.py
@@ -5,11 +5,9 @@ __copyright__ = "Copyright (C) 2023-2025 Tempesta Technologies, Inc."
 __license__ = "GPL2"
 
 
-logger = logging.getLogger()
+logger = logging.getLogger('ddos')
 logger.propagate = True
-logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
 handler.setFormatter(
     logging.Formatter("[%(asctime)s][%(name)s][%(levelname)s]: %(message)s")
 )
