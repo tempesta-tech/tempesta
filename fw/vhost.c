5400b5959b2b8878ab716bbdc94f63834974940d
Constantine <kt@tempesta-tech.com>
2026-01-07 15:12:59 +0200

    fix: change style

--- fw/vhost.c
+++ fw/vhost.c
@@ -1,7 +1,7 @@
 /**
  *		Tempesta FW
  *
- * Copyright (C) 2016-2025 Tempesta Technologies, Inc.
+ * Copyright (C) 2016-2026 Tempesta Technologies, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -1499,11 +1499,13 @@ tfw_location_init(TfwLocation *loc, tfw_match_t op, const char *arg,
 
 	memset(loc, 0, sizeof(TfwLocation));
 	if (op != TFW_HTTP_MATCH_O_REGEX) {
-		if ((argmem = tfw_kmalloc(len + 1, GFP_KERNEL)) == NULL)
+		argmem = tfw_kmalloc(len + 1, GFP_KERNEL);
+		if (!argmem)
 			return -ENOMEM;
 	}
 	else {/*If it is a regex we need only number of DB*/
-		if ((argmem = tfw_kmalloc(2 + 1, GFP_KERNEL)) == NULL)
+		argmem = tfw_kmalloc(sizeof(USHRT_MAX) + 1, GFP_KERNEL);
+		if (!argmem)
 			return -ENOMEM;
 	}
 
