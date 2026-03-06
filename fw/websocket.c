d2b8caf7b532504a446a1c7bcce9e94db7cbd352
EvgeniiMekhanik <em@tempesta-tech.com>
2026-03-06 18:13:53 +0200

    Update copyright year

--- fw/websocket.c
+++ fw/websocket.c
@@ -3,7 +3,7 @@
  *
  * Websocket proxy protocol implementation for Tempesta FW.
  *
- * Copyright (C) 2022-2025 Tempesta Technologies, Inc.
+ * Copyright (C) 2022-2026 Tempesta Technologies, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
