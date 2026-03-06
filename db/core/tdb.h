75ae9ab7bb3c4f351ea4a7152f8a77446d47f862
EvgeniiMekhanik <em@tempesta-tech.com>
2026-03-06 18:13:53 +0200

    Update copyright year

--- db/core/tdb.h
+++ db/core/tdb.h
@@ -4,7 +4,7 @@
  * Generic storage layer.
  *
  * Copyright (C) 2014 NatSys Lab. (info@natsys-lab.com).
- * Copyright (C) 2015-2025 Tempesta Technologies, INC.
+ * Copyright (C) 2015-2026 Tempesta Technologies, INC.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
