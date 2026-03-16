7438aa73ff0c643f4f0ea87fc9f72ee0ae3e6c8e
Constantine <kt@tempesta-tech.com>
2026-03-16 13:31:57 +0200

    refactor: Move skb related functions to ss_skb.h

--- fw/t/unit/test_http_msg.c
+++ fw/t/unit/test_http_msg.c
@@ -164,7 +164,7 @@ TEST(http_msg, cutoff_linear_headers_paged_body)
 		TFW_STR_STRING("paged_body")
 	};
 	TfwStr *head = &frags[0], *pgd = &frags[1];
-	TfwHttpMsgCleanup cleanup = {};
+	TfwSkbCleanup cleanup = {};
 	TfwMsgIter *it;
 	int i;
 
@@ -199,7 +199,7 @@ TEST(http_msg, cutoff_linear_headers_and_linear_body)
 		TFW_STR_STRING("paged_body2")
 	};
 	TfwStr *head = &frags[0], *pgd = &frags[1];
-	TfwHttpMsgCleanup cleanup = {};
+	TfwSkbCleanup cleanup = {};
 	TfwMsgIter *it;
 	int i;
 
@@ -231,7 +231,7 @@ TEST(http_msg, expand_from_pool_for_headers)
 	};
 	TfwStr *hdr = &frags[0], *head = &frags[0], *pgd = &frags[1];
 	TfwHttpMsg *msg = (TfwHttpMsg *)resp;
-	TfwHttpMsgCleanup cleanup = {};
+	TfwSkbCleanup cleanup = {};
 	TfwMsgIter *it;
 	int i;
 
@@ -286,7 +286,7 @@ TEST(http_msg, expand_from_pool_for_trailers)
 	};
 	TfwStr *trailer = &frags[0], *head = &frags[1], *pgd = &frags[2];
 	TfwHttpMsg *msg = (TfwHttpMsg *)resp;
-	TfwHttpMsgCleanup cleanup = {};
+	TfwSkbCleanup cleanup = {};
 	TfwMsgIter *it;
 	int i;
 
