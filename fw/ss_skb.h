acfe8415a7721ff1631b5c9f7c9bc4752ec7c23f
EvgeniiMekhanik <em@tempesta-tech.com>
2026-05-12 19:21:31 +0300

    Do not forward requests for already dropped client connections.
    - Rework `on_tcp_entail` callback invocation. Now it is called
      from `ss_skb_tcp_entail_list` not from `ss_skb_tcp_entail`.
      If this callback fails we drop all skbs from appropriate
      list.
    - Implement `tfw_http_req_on_tcp_entail` callback to check
      that client connection for sending request is not closed
      and drop skbs and request if connection is already closed.
    - Implement `ss_skb_copy_cb` function for correct copying
      of skb->cb (including all callbacks and destructors).
    
    Previously, requests that had already been placed into a server
    connection's `fwd_queue` were still forwarded to the server even
    after the originating client connection had been closed. Although
    the responses to such requests were no longer needed, the requests
    themselves were still fully transmitted to the backend.
    This patch changes this behavior by performing an additional client
    connection state check immediately before entailing a request's skbs
    to the server socket write queue for transmission. If the client
    connection has already been closed, the request is discarded together
    with all associated skbs instead of being sent to the server.
    The chosen approach has a theoretical disadvantage: if a large number
    of requests from a reset client connection coexist with many requests
    from active client connections and the TCP window is small, requests
    belonging to the inactive client connection may remain in the server
    connection queue for an extended period of time, consuming memory.
    An alternative approach would have been to drop the server connection
    and purge requests belonging to inactive client connections from the
    server send queue during reconnection. This approach was rejected
    because it would also impact active client connections by forcing their
    requests to be retransmitted.
    Since the server connection is also dropped upon receiving the first
    response byte for an inactive client connection (see bac89d1), the
    current implementation provides a practical and effective fix for
    issue #2284.
    
    Part-of: release-server-queued-requests
    Closes #2284

--- fw/ss_skb.h
+++ fw/ss_skb.h
@@ -29,7 +29,7 @@
 
 typedef int (*on_send_cb_t)(void *conn, struct sk_buff **skb_head,
 			    void *on_send_data);
-typedef void (*on_tcp_entail_t)(void *conn, struct sk_buff *skb_head);
+typedef int (*on_tcp_entail_t)(void *conn, struct sk_buff *skb_head);
 typedef void (*on_send_fail_cb_t)(void *conn, struct sk_buff *skb_head);
 typedef struct tfw_client_mem_t TfwClientMem;
 
@@ -54,17 +54,19 @@ typedef struct tfw_client_mem_t TfwClientMem;
 struct tfw_skb_cb {
 	TfwClientMem 		*cli_mem;
 	void 			(*destructor)(struct sk_buff *);
-	union {
-		on_send_cb_t	on_send;
-		on_tcp_entail_t on_tcp_entail;
-	};
-	union {
-		void		*on_send_data;
-		void		*on_tcp_entail_data;
-	};
 	long int		mem;
-	unsigned int 		stream_id;
-	bool			is_head;
+	struct_group (copy,
+		union {
+			on_send_cb_t	on_send;
+			on_tcp_entail_t on_tcp_entail;
+		};
+		union {
+			void		*on_send_data;
+			void		*on_tcp_entail_data;
+		};
+		unsigned int 		stream_id;
+		bool			is_head;
+	);
 };
 
 #define TFW_SKB_CB(skb) ((struct tfw_skb_cb *)&((skb)->cb[0]))
@@ -74,6 +76,7 @@ void ss_skb_set_owner(struct sk_buff *skb, void (*destructor)(struct sk_buff *),
 void ss_skb_adjust_client_mem(struct sk_buff *skb, int delta);
 void ss_skb_dflt_destructor(struct sk_buff *skb);
 void ss_skb_on_send_dflt(void *conn, struct sk_buff **skb_head);
+void ss_skb_copy_cb(struct sk_buff *to, struct sk_buff *from);
 
 static inline void
 __ss_skb_set_owner(struct sk_buff *skb, void (*destructor)(struct sk_buff *),
@@ -126,13 +129,14 @@ ss_skb_on_send(void *conn, struct sk_buff **skb_head)
 	return r;
 }
 
-static inline void
+static inline int
 ss_skb_on_tcp_entail(void *conn, struct sk_buff *skb_head)
 {
 	on_tcp_entail_t on_tcp_entail = TFW_SKB_CB(skb_head)->on_tcp_entail;
 
 	if (on_tcp_entail)
-		on_tcp_entail(conn, skb_head);
+		return on_tcp_entail(conn, skb_head);
+	return 0;
 }
 
 typedef int ss_skb_actor_t(void *conn, unsigned char *data, unsigned int len,
